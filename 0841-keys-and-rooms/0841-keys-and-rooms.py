125145da3b5f02cc560d1bfdd3311cfaf2e689e0
Loi Tran <datloiboi@gmail.com>
2022-12-30 11:01:38 -0500

    Time: 66 ms (94.33%), Space: 15 MB (12.46%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,25 +1,11 @@
-'''
-1. Constraints
-We receive a list, we return a boolean
-
-
-2. Diagram
-
-3. Pseudocode
-
-4. Code
-'''
-
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visit = set([0])
+        seen = set([0])
         
         def dfs(r):
-            for key in rooms[r]:
-                if key not in visit:
-                    visit.add(key)
-                    dfs(key)
-            
-        dfs(0)
-        
-        return len(visit) == len(rooms)
+            for k in rooms[r]:
+                if k not in seen:
+                    seen.add(k)
+                    dfs(k)
+        dfs(0) 
+        return len(seen) == len(rooms)

e9ae42ee6afd4524c1400c910bef2010befc8d5b
Loi Tran <datloiboi@gmail.com>
2022-12-30 09:46:52 -0500

    Time: 31 ms (98.97%), Space: 14.4 MB (77.91%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,20 +1,25 @@
-'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 
-
-Call dfs on each n, clone it, and then loop through it's neighbors.
-
-'''
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         seen = {}
+        
         def dfs(n):
             if n in seen:
                 return seen[n]
             copy = Node(n.val)
             seen[n] = copy
+            
             for nei in n.neighbors:
                 copy.neighbors.append(dfs(nei))
-            return copy
             
+            return copy
+        
         
         return dfs(node) if node else None

9d22a63c95ddb481a3f4b7c3a26f1202abd33796
Loi Tran <datloiboi@gmail.com>
2022-12-30 05:56:24 -0500

    Time: 2674 ms (85.41%), Space: 13.9 MB (68.38%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -2,31 +2,30 @@ class Solution:
     def largestVariance(self, s: str) -> int:
         chars = {}
         for c in s:
-            chars[c] = chars.get(c,0)+1
+            chars[c] = chars.get(c, 0)+1
         perms = itertools.permutations(chars, 2)
-
         def kadane(a,b):
-            res = count = 0
+            count = res = 0
+            isA = isB = False
             aCount = chars[a]
             bCount = chars[b]
-            isA = isB = False
             for c in s:
-                if not c == a and not c == b: continue
+                if not a == c and not b == c: continue
                 if count < 0 and aCount != 0 and bCount != 0:
                     isA = isB = False
                     count = 0
                 if c == a:
-                    aCount-=1
-                    isA = True
+                    aCount -=1
                     count += 1
+                    isA = True
                 if c == b:
                     bCount -=1
+                    count -= 1
                     isB = True
-                    count-=1
-                if isA and isB:
-                    res = max(res, count)
+                if isA and isB:    
+                    res = max(res, count)           
             return res
         res = 0
-        for a,b in set(perms): 
-            res = max(res, kadane(a,b))
+        for a,b in perms:
+            res = max(res, kadane(a, b))
         return res

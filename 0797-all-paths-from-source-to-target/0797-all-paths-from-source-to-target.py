f3978053c7351948cf06361cdf8aea3fdfaf82e5
Loi Tran <datloiboi@gmail.com>
2022-12-31 07:37:19 -0500

    Time: 105 ms (82.41%), Space: 15.6 MB (78.47%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,14 +1,14 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        res = []
-        end = len(graph) - 1
-        
-        def dfs(n, path, res):
-            if n == end:
-                res.append(path+[n])
-                return 
+        self.res = []
+        self.end = len(graph)-1
+        def dfs(n, path):
+            if n == self.end:
+                self.res.append(path+[n])
+            
             for nei in graph[n]:
-                dfs(nei, path+[n], res)
-        dfs(0, [], res)
-        return res
+                dfs(nei, path+[n])
+            
+        dfs(0, [])
         
+        return self.res

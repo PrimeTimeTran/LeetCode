4be4f515380a976b64460940493aba4da15735f1
Loi Tran <datloiboi@gmail.com>
2022-12-31 17:37:08 -0500

    Time: 773 ms (61.46%), Space: 20.2 MB (30.98%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,39 +1,19 @@
 '''
-1. Constraints 
-We receive an 2d array of [duration, date]
 
-2. Diagram
-
-courses = [[100,200],[1000,1250],[200,1300],[2000,3200]]
-output = 3
-
-
-1, 100, 101
-3, 1000, 1100
-2, 200, 1300
-4, 2000, 3300
-
-3. Pseudocode
-
-Sort courses on end date
-Iterate over courses and add their duration time to a start time.
-If the duration time exceeds the end time, remove the most recently added time.
-
+Iterate through courses
+Increment the start time by the duration of each course
+Add the course's duration to a maxheap
+If the new start time is greater than the end time, remove current duration from time/heap
 '''
-
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        time = 0
         pq = []
-        for dur, end in sorted(courses, key= lambda x: x[1]):
-            time += dur
+        start = 0
+        
+        for dur, end in sorted(courses, key = lambda x: x[1]):
+            start += dur
             heapq.heappush(pq, -dur)
-            while time > end:
-                time += heapq.heappop(pq)
-                
+            if start > end:
+                start += heapq.heappop(pq)
+        
         return len(pq)
-            
-        
-        
-        
-        

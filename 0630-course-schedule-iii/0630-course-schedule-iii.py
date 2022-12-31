0e58645e80d53abe871461225bdded6996815902
Loi Tran <datloiboi@gmail.com>
2022-12-31 11:32:09 -0500

    Time: 755 ms (76.55%), Space: 19.9 MB (82.20%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,12 +1,35 @@
-from heapq import heappush, heappushpop
+'''
+1. Constraints
+We receive a 2d list where the elements are [duration, endDay]
+
+We want to return an int, indicating maximum number of courses we can take.
+
+
+Diagram
+courses = [[100,200],[1000,1250],[200,1300],[2000,3200]]
+1, 100 <= 200
+2, 1100 <= 1250
+3, 1300 <= 1300
+4, 3300 <= 3200
+res = 3
+
+
+
+2. Pseudocode
+
+- Sort our courses lastDay
+- Iterate through courses and increment start time by course time. 0 + 100 + 1000 + 200 ... n-1
+
+
+'''
 
 class Solution:
-    def scheduleCourse(self, A: List[List[int]]) -> int:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
         pq = []
         start = 0
-        for t, end in sorted(A, key= lambda x: x[1]):
+        for t, end in sorted(courses, key = lambda x: x[1]):
             start += t
-            heappush(pq, -t)
-            if start > end:
+            heapq.heappush(pq, -t)
+            while start > end:
                 start += heapq.heappop(pq)
         return len(pq)

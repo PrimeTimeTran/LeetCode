0165965341b100ce65f165028a7e75724c55caf0
Loi Tran <datloiboi@gmail.com>
2022-12-31 16:50:01 -0500

    Time: 521 ms (71.68%), Space: 16.3 MB (51.67%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,20 +1,23 @@
+'''
+1. Constraints
+We receive an array of weighted edges. We also receive a start node, k and a variable n for number of nodes.
+
+We should return an integer
+
+2. Diagram
+
+3. Pseudocode
+
+Use a pq to track nodes we need to visit and the time so far.
+Iterate through all nodes until we've visited all nodes.
+If the number of nodes visited is the same as n return our time, otherwise return -1
+
+
+'''
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # seen, t = set(), 0
-        # g = collections.defaultdict(list)
-        # for u, v, w in times:
-        #     g[u].append((v, w))
-        # heap = [(0, k)]
-        # while heap:
-        #     t, u = heapq.heappop(heap)
-        #     if u in seen: continue
-        #     seen.add(u)
-        #     if len(seen) == n: break
-        #     for v, nt in g[u]:
-        #         heapq.heappush(heap, (t+nt, v))
-        # return t if len(seen)==n else -1
-        
-        seen, t = set(), 0
+        seen= set()
         g = defaultdict(list)
         
         for u,v,w in times:

b7ce3582161c0a0fa51f7af2acbe1e11d2a7230b
Loi Tran <datloiboi@gmail.com>
2022-12-31 10:21:01 -0500

    Time: 117 ms (71.74%), Space: 17.2 MB (18.06%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,19 +1,45 @@
+'''
+1. Constraints
+We're given n which is the numbeer of nodes and edges which represents their edges.
+
+We return an integer.
+
+
+2. Diagram
+
+3. Pseudocode
+
+Create graph using edges.
+
+Iterate over all nodes.
+    Process each node if node hasn't been processed. 
+        Process node's neighbors.
+    Increment count
+Return the count of times processes
+    
+
+'''
+
 class Solution:
     def countComponents(self, N: int, edges: List[List[int]]) -> int:
         g = {i:[] for i in range(N)}
+        
         for a,b in edges:
             g[a].append(b)
             g[b].append(a)
+        
+        count = 0
+        seen = set()
         def dfs(n):
             if n in seen:
                 return
             seen.add(n)
+            
             for nei in g[n]:
                 dfs(nei)
-        res = 0
-        seen = set()
+        
         for n in range(N):
-            if n not in seen:
+            if not n in seen:
                 dfs(n)
-                res += 1
-        return res
+                count+=1
+        return count

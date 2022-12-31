1ca1fb34d70ae54d5a7c097116095ba3b0fb5a82
Loi Tran <datloiboi@gmail.com>
2022-12-31 09:27:36 -0500

    Time: 711 ms (94.07%), Space: 20.2 MB (33.33%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,12 +1,24 @@
+'''
+courses = [[100,200],[1000,1250],[200,1300],[2000,3200]]
+
+100+1000= 1100
+1100 + 200 = 1300
+1300 + 2000 = 3300
+
+
+
+'''
+
 from heapq import heappush, heappushpop
 
 class Solution:
     def scheduleCourse(self, A: List[List[int]]) -> int:
         pq = []
         start = 0
-        for t, end in sorted(A, key = lambda x: x[1]):
+        for t, end in sorted(A, key= lambda x: x[1]):
             start += t
-            heapq.heappush(pq, -t)
+            heappush(pq, -t)
             while start > end:
                 start += heapq.heappop(pq)
+                
         return len(pq)

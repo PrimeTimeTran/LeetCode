2deab5717de3e03f01293a455119425311ab34a4
Loi Tran <datloiboi@gmail.com>
2022-12-31 15:46:43 -0500

    Time: 450 ms (97.74%), Space: 16.2 MB (55.86%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,10 +1,27 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # seen, t = set(), 0
+        # g = collections.defaultdict(list)
+        # for u, v, w in times:
+        #     g[u].append((v, w))
+        # heap = [(0, k)]
+        # while heap:
+        #     t, u = heapq.heappop(heap)
+        #     if u in seen: continue
+        #     seen.add(u)
+        #     if len(seen) == n: break
+        #     for v, nt in g[u]:
+        #         heapq.heappush(heap, (t+nt, v))
+        # return t if len(seen)==n else -1
+        
         seen, t = set(), 0
-        g = collections.defaultdict(list)
-        for u, v , w in times:
-            g[u].append((v, w))
+        g = defaultdict(list)
+        
+        for u,v,w in times:
+            g[u].append((v,w))
+            
         heap = [(0, k)]
+        
         while heap:
             t, u = heapq.heappop(heap)
             if u in seen: continue
@@ -12,19 +29,5 @@ class Solution:
             if len(seen) == n: break
             for v, nt in g[u]:
                 heapq.heappush(heap, (t+nt, v))
-        return t if len(seen)==n else -1
-        
-        
-        g = defaultdict(dict)
-        
-        for s,e,t in times:
-            g[s][e] = t
             
-        seen = set()
-        
-        q = deque([k])
-        
-            
-            
-            
-        return -1
+        return t if len(seen) == n else -1

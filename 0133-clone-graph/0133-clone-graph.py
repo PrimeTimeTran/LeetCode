e3c8375ed80edbff7bbf292d49fcfa351bab8298
Loi Tran <datloiboi@gmail.com>
2022-12-31 08:31:22 -0500

    Time: 42 ms (83.34%), Space: 14.3 MB (77.76%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,25 +1,23 @@
 """
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
+- Copy Node
+
+- Copy neighbors, append neighbors to copy
+
+- Return copy
+
 """
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         seen = {}
-        
         def dfs(n):
             if n in seen:
                 return seen[n]
             copy = Node(n.val)
             seen[n] = copy
-            
             for nei in n.neighbors:
                 copy.neighbors.append(dfs(nei))
-            
+                
             return copy
-        
         
         return dfs(node) if node else None

971e409ff00e6db616cb19f3eb1f564d8f17b3fa
Loi Tran <datloiboi@gmail.com>
2022-12-31 08:26:52 -0500

    Time: 108 ms (77.29%), Space: 15.7 MB (78.47%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,14 +1,33 @@
+'''
+1. Constraints 
+We're given a 2d array, and should return a 2d array which consists of paths from 0 to n-1
+
+2. Diagram
+
+0 -> 1 -> 3
+[0]
+    [0, 1]
+          [0, 1, 3]
+
+0 -> 2 -> 3
+[0]
+
+
+2. 
+
+
+
+'''
+
+
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         self.res = []
         self.end = len(graph)-1
-        def dfs(n, path):
-            if n == self.end:
+        def dfs(n,path):
+            if self.end == n:
                 self.res.append(path+[n])
-            
             for nei in graph[n]:
                 dfs(nei, path+[n])
-            
         dfs(0, [])
-        
         return self.res

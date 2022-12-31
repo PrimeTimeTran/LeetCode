cbffe7f23424f87d943668617ddb9d8477e376af
Loi Tran <datloiboi@gmail.com>
2022-12-31 14:33:32 -0500

    Time: 1562 ms (99.61%), Space: 75.7 MB (34.92%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -4,14 +4,15 @@ class Solution:
         
         for a,b,d in roads:
             g[a][b] = g[b][a] = d
-        res = float('infinity')
+            
         dq = deque([1])
-        vis = set()
+        visit = set()
+        res = float('inf')
         while dq:
             cur = dq.popleft()
             for des, d in g[cur].items():
-                if des not in vis:
-                    vis.add(des)
+                if des not in visit:
+                    visit.add(des)
                     dq.append(des)
                 res = min(res, d)
         return res

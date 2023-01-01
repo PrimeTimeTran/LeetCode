957665b2fe0b6855f6446d16f6dd178066bba7dd
Loi Tran <datloiboi@gmail.com>
2023-01-01 16:24:21 -0500

    Time: 87 ms (98.59%), Space: 17.5 MB (17.97%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,6 +1,6 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        g = {i: [] for i in range(n)}
+        g = {i:[] for i in range(n)}
         
         for a,b in edges:
             g[a].append(b)
@@ -12,9 +12,10 @@ class Solution:
             if n in seen:
                 return False
             seen.add(n)
+
             for nei in g[n]:
                 if nei == prev: continue
                 if not dfs(nei, n): return False
             return True
         
-        return dfs(0, -1) and len(seen) == n
+        return dfs(0,-1) and len(seen) == n

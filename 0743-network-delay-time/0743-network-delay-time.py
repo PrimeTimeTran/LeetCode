b53611f9a4595d2f430555b750ecc14c9b0bd2b4
Loi Tran <datloiboi@gmail.com>
2023-01-01 13:34:54 -0500

    Time: 473 ms (89.36%), Space: 16.1 MB (55.86%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -18,23 +18,15 @@ If the number of nodes visited is the same as n return our time, otherwise retur
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         g = defaultdict(list)
-        
         for u,v,w in times:
             g[u].append((v, w))
-        
         seen = set()
         heap = [(0, k)]
-
         while heap:
             t, u = heapq.heappop(heap)
-            # t, u = heap.pop(0)
             if u in seen: continue
             seen.add(u)
             if len(seen) == n: break
             for v, nt in g[u]:
                 heapq.heappush(heap, (t+nt, v))
-                # heap.append((t+nt, v))
-                
-        
-        
         return t if len(seen) == n else -1

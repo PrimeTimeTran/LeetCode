63bdcdf0ffd1c218a97818fa7a448339d7f3af9d
Loi Tran <datloiboi@gmail.com>
2023-01-01 11:32:18 -0500

    Time: 182 ms (100.00%), Space: 14.6 MB (100.00%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
@@ -1,12 +1,32 @@
+'''
+Iterate over each index of s
+Add it's value to a running sum.
+If the new value is greater than k, increment count, and reset sum to current digit
+
+
+
+'165462'
+
+
+ans = 4
+cur = 2
+2
+
+
+
+'''
+
 class Solution:
     def minimumPartition(self, s: str, k: int) -> int:
         cur, ans = 0, 1
+        
         for d in s:
             if int(d) > k: return -1
+            
             cur = 10*cur + int(d)
             
             if cur > k:
-                ans += 1
+                ans+=1
                 cur = int(d)
         return ans
-                
+            

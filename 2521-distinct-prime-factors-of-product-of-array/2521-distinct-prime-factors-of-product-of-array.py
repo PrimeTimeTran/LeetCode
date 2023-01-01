e09d968008039c0961bc577e63f59f6901bc27c4
Loi Tran <datloiboi@gmail.com>
2023-01-01 16:58:47 -0500

    Time: 1791 ms (28.57%), Space: 15.4 MB (85.71%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -1,33 +1,17 @@
-'''
-1. Constraints
-We receieve input array and must an integer.
-
-
-
-2. Pseudocode
-
-Process each number looking for it's prime factors.
-If the number contains a prime factor, add it to a set.
-
-
-
-'''
-
 class Solution:
     def distinctPrimeFactors(self, nums: List[int]) -> int:
-        s = set()
+        seen = set()
         def process(num):
             n = num
             for i in range(2, num+1):
                 if n % i == 0:
                     while n % i == 0:
                         n /= i
-                        s.add(i)
+                        seen.add(i)
                 if n == 1:
                     return
             
-        
         for n in nums:
             process(n)
             
-        return len(s)
+        return len(seen)

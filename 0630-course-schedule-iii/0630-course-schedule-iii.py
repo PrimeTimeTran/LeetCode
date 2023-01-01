eaacc2a80023933ca9b365196cdfb73ee1764fbc
Loi Tran <datloiboi@gmail.com>
2023-01-01 12:16:23 -0500

    Time: 704 ms (95.37%), Space: 20 MB (57.32%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,19 +1,10 @@
-'''
-
-Iterate through courses
-Increment the start time by the duration of each course
-Add the course's duration to a maxheap
-If the new start time is greater than the end time, remove current duration from time/heap
-'''
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        pq = []
-        start = 0
-        
-        for dur, end in sorted(courses, key = lambda x: x[1]):
+        start =  0
+        heap = []
+        for dur, end in sorted(courses, key=lambda x: x[1]):
             start += dur
-            heapq.heappush(pq, -dur)
+            heapq.heappush(heap, -dur)
             if start > end:
-                start += heapq.heappop(pq)
-        
-        return len(pq)
+                start += heapq.heappop(heap)
+        return len(heap)

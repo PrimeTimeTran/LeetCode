8f738c0cd20d43e47b70a58970f4194fb6b0609b
Loi Tran <datloiboi@gmail.com>
2023-01-01 11:49:23 -0500

    Time: 46 ms (100.00%), Space: 13.8 MB (100.00%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -1,23 +1,25 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
+        
         def isPrime(n):
             if n < 2: return False
-            for i in range(2, int(n ** 0.5) +1):
+            for i in range(2, int(n**0.5)+1):
                 if n % i == 0:
                     return False
             return True
-                
+        
         q = []
-        diff = float('infinity')
         pair = [-1,-1]
+        diff = float('infinity')
         
         for i in range(left, right+1):
             if isPrime(i):
                 q.append(i)
             while len(q) >= 2:
-                if abs(q[0] - q[1]) < diff:
-                    diff = abs(q[0] - q[1])
+                if abs(q[0]-q[1]) < diff:
+                    diff = abs(q[0]-q[1])
                     pair = [q[0], q[1]]
                     if diff <= 2: return pair
                 q.pop(0)
+        
         return pair

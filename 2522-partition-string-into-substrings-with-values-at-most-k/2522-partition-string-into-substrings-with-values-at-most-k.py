06af779c819ffabef471dba98fab4414e526c5df
Loi Tran <datloiboi@gmail.com>
2023-01-01 08:32:28 -0500

    Time: 211 ms (100.00%), Space: 14.7 MB (100.00%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
@@ -1,5 +1,15 @@
 class Solution:
     def minimumPartition(self, s: str, k: int) -> int:
+        curr, ans = 0, 1
+        for d in s:
+            if int(d) > k:
+                return -1
+            curr = 10 * curr + int(d)
+            if curr > k:
+                ans += 1
+                curr = int(d)
+        return ans
+
         count = num = 0
         for digit in s:
             if num > k: return -1

7201c5f170c819ae8f27af6ca3427792994652cc
Loi Tran <datloiboi@gmail.com>
2023-01-02 15:09:59 -0500

    Time: 3973 ms (51.80%), Space: 76.7 MB (31.14%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,24 +1,5 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        g = defaultdict(list)
-        for a,b, d in roads:
-            g[a].append((b,d))
-            g[b].append((a,d))
-        
-        self.res = float('inf')
-        seen = set()
-        
-        def dfs(n):
-            # if n in seen:
-            #     return 
-            seen.add(n)
-            for des, d in g[n]:
-                if des not in seen:
-                    dfs(des)
-                self.res = min(self.res, d)
-        dfs(1)
-        return self.res
-    
         g = defaultdict(dict)
         for a,b,d in roads:
             g[a][b] = g[b][a] = d

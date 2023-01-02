abb273de2e806a7379f6b65e9aed59bc17a15581
Loi Tran <datloiboi@gmail.com>
2023-01-02 06:04:56 -0500

    Time: 39 ms (71.61%), Space: 13.8 MB (96.54%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,7 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper(): return True
+        if word.islower(): return True
+        if word.istitle(): return True
+        return False
+    

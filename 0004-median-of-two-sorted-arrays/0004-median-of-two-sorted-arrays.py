9233510b1050c8a440508ff01df018d3765e1332
Loi Tran <datloiboi@gmail.com>
2023-01-03 18:16:44 -0500

    Time: 201 ms (42.48%), Space: 14.2 MB (23.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,10 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        final = heapq.merge(nums1,nums2)
-        final = list(final)
-        if len(final)%2 != 0:
-            return final[len(final)//2]
-        else:
-            left = int(len(final)/2) - 1
-            right = int(len(final)/2)
-            return (final[left]+final[right])/2
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+        
+        if len(B) < len(A):
+            A, B = B, A
+            
+            
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2
+            j = (half - i - 2)
+            
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+            
+            if Aleft <= Bright and Bleft <= Aright:
+                if  total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright,  Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1

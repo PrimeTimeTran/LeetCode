6bcc20d4843b2ec7c4cd5eaefea5022bc7a0e02e
Loi Tran <datloiboi@gmail.com>
2023-01-03 15:32:59 -0500

    Time: 793 ms (54.35%), Space: 15.9 MB (91.72%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,20 +1,35 @@
+'''
+
+3. Pseudocode
+
+- Create a graph using edges 
+- Traverse graph using bfs starting at node k. 
+- Check if the node has been seen, if so continue
+- Add node to seen nodes
+- Check if the length of seen nodes is equal to number of nodes, if so break
+- Add neighbors of node to queue adding current time taken
+- Return time if number of nodes seen equal to number of nodes, else return -1
+
+
+'''
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         g = defaultdict(list)
         
-        
-        for u,v,t in times:
-            g[u].append((v,t))
+        for u,v,w in times:
+            g[u].append((v,w))
             
         seen = set()
         q = [(0,k)]
         
-        while q:
-            t, u = heapq.heappop(q)
+        while q: 
+            time, u = heapq.heappop(q)
             if u in seen: continue
             seen.add(u)
             if len(seen) == n: break
             for v, nt in g[u]:
-                heapq.heappush(q,(nt+t, v))
+                heapq.heappush(q, (time+nt, v))
         
-        return t if len(seen) == n else -1
+        return time if len(seen) == n else -1
+        

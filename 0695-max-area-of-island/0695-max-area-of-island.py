cacd5398103aceac01eb1247ea93c1c4db7c3edf
Loi Tran <datloiboi@gmail.com>
2023-01-03 14:45:06 -0500

    Time: 202 ms (58.17%), Space: 17.9 MB (18.04%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -6,9 +6,7 @@ class Solution:
             out = r < 0 or c < 0 or r == m or c == n
             if out:
                 return 0
-            if grid[r][c] == 0:
-                return 0
-            if (r,c) in seen:
+            if (r,c) in seen or grid[r][c] == 0:
                 return 0
             seen.add((r,c))
             return 1+dfs(r+1,c)+dfs(r-1,c)+dfs(r,c+1)+dfs(r,c-1)

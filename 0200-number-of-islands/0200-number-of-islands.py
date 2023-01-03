a47a54ce0d5690df44b8ae70c3d2247baf408ab3
Loi Tran <datloiboi@gmail.com>
2023-01-03 10:09:57 -0500

    Time: 906 ms (31.70%), Space: 20.6 MB (37.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,40 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+        
+    def find(self, u):
+        if u != self.parent[u]:
+            self.parent[u] = self.find(self.parent[u])  # Path compression
+        return self.parent[u]
+    
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv: return False
+        if self.size[pu] < self.size[pv]:  # Merge pu to pv
+            self.size[pv] += self.size[pu]
+            self.parent[pu] = pv
+        else:
+            self.size[pu] += self.size[pv]
+            self.parent[pv] = pu
+        return True
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m,n = len(grid), len(grid[0])
+        DIR = [0, 1, 0, -1, 0]
+        m, n = len(grid), len(grid[0])
+        uf = UnionFind(m*n)
         
-        
-        seen = set()
-        def dfs(r,c):
-            out = r < 0 or c < 0 or r == m or c == n
-            if out:
-                return False
-            if grid[r][c] == "0":
-                return False
-            if (r,c) in seen:
-                return False
-            seen.add((r,c)) 
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-            return True
-        
-        res = 0
+        component = 0
         for r in range(m):
             for c in range(n):
-                if dfs(r,c):
-                    res +=1
-        return res
-                    
-                    
+                if grid[r][c] == "0": continue
+                component += 1
+                curId = r * n + c
+                for i in range(4):
+                    nr, nc = r + DIR[i], c + DIR[i+1]
+                    if nr < 0 or nr == m or nc < 0 or nc == n or grid[nr][nc] == "0": continue
+                    neiId = nr * n + nc
+                    if uf.union(curId, neiId):
+                        component -= 1
+        return component

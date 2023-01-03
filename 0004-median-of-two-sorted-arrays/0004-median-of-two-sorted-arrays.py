8cfa5d2926da3c6be92f1a0cc31d369ac47618a6
Loi Tran <datloiboi@gmail.com>
2023-01-03 18:29:47 -0500

    Time: 258 ms (10.33%), Space: 14.1 MB (96.99%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,8 +4,8 @@ class Solution:
         nums = list(nums)
         
         if len(nums) % 2 == 0:
-            l = len(nums)//2 - 1
-            r = len(nums)//2
+            l = len(nums) // 2 - 1
+            r = len(nums) // 2
             return (nums[l]+nums[r])/2 
         else:
             return nums[len(nums) //2]

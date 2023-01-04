d308a353271d5a3d62f194851ed59ff6c93323e8
Loi Tran <datloiboi@gmail.com>
2023-01-03 20:27:08 -0500

    Time: 2833 ms (12.47%), Space: 28.4 MB (44.10%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -3,15 +3,3 @@ class Solution:
         cnt = Counter(tasks).values()
         if 1 in cnt: return -1
         return sum([ceil(v/3) for v in cnt])
-    
-        def tot(n):
-            res = n%3
-            div = n//3
-            if res ==0: return div
-            else: return div +1
-
-        ans =0
-        for count in Counter(tasks).values():
-            if count ==1: return -1
-            else: ans += tot(count) 
-        return ans

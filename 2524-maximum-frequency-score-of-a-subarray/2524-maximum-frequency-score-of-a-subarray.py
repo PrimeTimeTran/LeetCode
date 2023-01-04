db7fde34175ad58453f41fb60bcea1feefc13671
Loi Tran <datloiboi@gmail.com>
2023-01-04 13:03:43 -0500

    Time: 3264 ms (100.00%), Space: 32.5 MB (100.00%) - LeetHub

--- 2524-maximum-frequency-score-of-a-subarray/2524-maximum-frequency-score-of-a-subarray.py
+++ 2524-maximum-frequency-score-of-a-subarray/2524-maximum-frequency-score-of-a-subarray.py
@@ -1,41 +1,32 @@
-'''
-Use sliding window of len k from left to right.
-Use a counter to track distinct values and their powers.
-Use cur and ans to track running totals to avoid TLE.
-
-'''
-
 class Solution:
     def maxFrequencyScore(self, nums: List[int], k: int) -> int:
-        mod = 10**9 + 7
+        mod = 10**9+7
         
         @cache
-        def powmod(b,e):
+        def modpow(b,e):
             return b**e % mod
         
         c = Counter(nums[:k])
         
         cur = 0
         for b,e in c.items():
-            cur = (cur + powmod(b,e)) % mod
+            cur = (cur+modpow(b,e)) % mod
+            
         ans = cur
         
         for i in range(1, len(nums)-k+1):
             L,R = nums[i-1], nums[i+k-1]
             if L == R: continue
-            cur -= powmod(L, c[L])
+            
+            cur -= modpow(L, c[L])
             if c[L] > 1:
-                cur += powmod(L, c[L] - 1)
-            
+                cur += modpow(L, c[L] - 1)
+                
             if c[R]:
-                cur -= powmod(R, c[R])
-            
-            cur +=  powmod(R, c[R]+1)
-
-            cur %= mod
+                cur -= modpow(R, c[R])
+            cur += modpow(R, c[R]+1)
+            cur = cur % mod
             ans = max(ans, cur)
-            
             c[L] -= 1
             c[R] += 1
-                
         return ans

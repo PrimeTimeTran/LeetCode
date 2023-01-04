ef7576f66ef04778361ac41e5133050acf447abb
Loi Tran <datloiboi@gmail.com>
2023-01-04 09:45:07 -0500

    Time: 96 ms (82.89%), Space: 13.8 MB (94.37%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -1,37 +1,32 @@
 '''
-
-Iterate range left to right
-Identify prime nums, add them to a queue.
-
-If the queue has 2 or more items, calculate min difference between elements
-
+Move through range left to right adding prime numbers to a list.
+If the number is prime and the list is greater than 2 items, compare to current minimum res.
+If smaller update res and reset pairs. Also check if minimum is smaller than 3, is so return cause twin primes
+are the smallest possible.
 
 '''
 
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        def isPrime(num):
-            if num < 2:
-                return False
-            for i in range(2, int(num**0.5)+1):
-                if num % i == 0:
+        def isPrime(n):
+            if n < 2: return False
+            for i in range(2, int(n**0.5)+1):
+                if n % i == 0:
                     return False
             return True
+            
+        q = []    
+        pairs = [-1,-1]
+        res = float('infinity')
         
-        pair = [-1,-1]
-        q = []
-        diff = float('inf')
         for i in range(left, right+1):
             if isPrime(i):
                 q.append(i)
-            while len(q) >= 2:
-                print(q)
-                if abs(q[0]-q[1]) < diff:
-                    diff = abs(q[0]-q[1])
-                    pair = [q[0],q[1]]
-                    if diff <= 2:
-                        return pair
+            if len(q) > 1:
+                if abs(q[0] - q[1]) < res:
+                    pairs = [q[0], q[1]]
+                    res = abs(q[0] - q[1])
+                    if res < 3:
+                        return pairs
                 q.pop(0)
-        return pair
-                
-                
+        return pairs

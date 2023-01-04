f392158ab5296fd830b54a16cf20fe08186ac2b9
Loi Tran <datloiboi@gmail.com>
2023-01-04 16:13:43 -0500

    Time: 1278 ms (34.60%), Space: 14.8 MB (81.97%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,30 +1,36 @@
+'''
+Create a 2d matrix using m and n as a cache for DP to avoid TLE.
+Iterate all cells with m and n. Check for new max length path using DFS passing dfs previous cell value.
+In conjunction with checking next cell out of bounds, check if next cell less than or equal to previous cell, if so return 0.
+
+Check for memoized value and return if found.
+'''
+
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m,n = len(matrix), len(matrix[0])
         dp = [[-1] * n for _ in range(m)]
-        
-        
-        def dfs(r,c,prev):
-            out = r < 0 or c < 0 or r == m or c == n or matrix[r][c] <= prev
+
+        def dfs(r,c,p):
+            out = r < 0 or c < 0 or r == m or c == n or matrix[r][c] <= p
             if out:
-                return 0 
+                return 0
             if dp[r][c] != -1:
                 return dp[r][c]
-            
-            p = matrix[r][c]
+            cur = matrix[r][c]
             
             t = max(
-                dfs(r+1,c,p),
-                dfs(r-1,c,p),
-                dfs(r,c+1,p),
-                dfs(r,c-1,p)
+                dfs(r+1,c,cur),
+                dfs(r-1,c,cur),
+                dfs(r,c+1,cur),
+                dfs(r,c-1,cur)
             )
-            
-            dp[r][c] = t + 1
+            dp[r][c] = 1 + t
             return dp[r][c]
-       
+        
         res = 0
         for r in range(m):
             for c in range(n):
-                res = max(res, dfs(r,c, -1))
+                res = max(res, dfs(r,c,-1))
+                
         return res

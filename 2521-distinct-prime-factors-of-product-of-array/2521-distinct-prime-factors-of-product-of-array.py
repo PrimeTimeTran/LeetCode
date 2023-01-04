d578c57b7b356b240266e3df4226aa64874ad74b
Loi Tran <datloiboi@gmail.com>
2023-01-03 21:44:36 -0500

    Time: 5081 ms (5.02%), Space: 15.3 MB (92.47%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -3,16 +3,15 @@ class Solution:
         seen = set()
         def process(num):
             n = num
-            for i in range(2, n+1):
+            for i in range(2, num+1):
                 if n % i == 0:
                     while n % i == 0:
                         n /= i
                         seen.add(i)
                 if n == 1:
                     return
-            
-            
+        
         for n in nums:
             process(n)
+        print(seen)
         return len(seen)
-            

c9779381a6255f12e6fdfd6f6f3ba2cc4a54c83a
Loi Tran <datloiboi@gmail.com>
2023-01-04 18:20:55 -0500

    Time: 72 ms (79.77%), Space: 15.1 MB (16.03%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -1,29 +1,30 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        wordDict = defaultdict(set)
+        g = defaultdict(set)
         for word in wordList:
             if word != beginWord:
                 for i in range(len(word)):
-                        wordDict[word[:i] + "*" + word[i+1:]].add(word)
-        queue = deque([beginWord])
+                    pat = word[:i] + "*" + word[i+1:]
+                    g[pat].add(word)
+        q = deque([beginWord])
         visited = {beginWord: 1}
         parent_list = defaultdict(set)
         ans_path = []
-        # print(wordDict)
-        
-        while queue:
-            word = queue.popleft()
+
+        while q:
+            word = q.popleft()
             if word == endWord:             
                 break
             for i in range(len(word)):
-                for next_word in wordDict[word[:i] + "*" + word[i+1:]]:
+                pat = word[:i] + "*" + word[i+1:]
+                for next_word in g[pat]:
                     if next_word not in visited:
                         visited[next_word] = visited[word] + 1
-                        queue.append(next_word)
+                        q.append(next_word)
                         parent_list[next_word].add(word)
                     elif visited[next_word] > visited[word]:
                         parent_list[next_word].add(word)
-        
+        print(visited)
         def dfs(word, path):
             if word == beginWord:
                 ans_path.append(path[::-1])

da0736a0b41b89a1727672e43bb9d9f16a388a34
Loi Tran <datloiboi@gmail.com>
2023-01-04 17:41:10 -0500

    Time: 259 ms (73.66%), Space: 17.3 MB (56.89%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -8,26 +8,27 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        g = collections.defaultdict(list)
+        
+        g = defaultdict(list)
         wordList.append(beginWord)
-
+        
         for w in wordList:
             for j in range(len(w)):
-                pat = w[:j] + '*' + w[j+1:]
+                pat = w[:j] + "*" + w[j+1:]
                 g[pat].append(w)
-        seen = set([beginWord])
         q = deque([beginWord])
+        seen = set(beginWord)
         res = 1
         while q:
             for _ in range(len(q)):
-                word = q.popleft()
-                if word == endWord:
+                w = q.popleft()
+                if w == endWord:
                     return res
-                for j in range(len(word)):
-                    pat = word[:j] + '*' + word[j+1:]
+                for j in range(len(w)):
+                    pat = w[:j] + "*" + w[j+1:]
                     for nei in g[pat]:
                         if nei not in seen:
                             seen.add(nei)
                             q.append(nei)
-            res+=1
+            res += 1
         return 0

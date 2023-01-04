687b88a3c7b4abf215fbfb176fb7d6e660963f8c
Loi Tran <datloiboi@gmail.com>
2023-01-03 22:26:52 -0500

    Time: 3477 ms (100.00%), Space: 32.3 MB (100.00%) - LeetHub

---
+++ 2524-maximum-frequency-score-of-a-subarray/2524-maximum-frequency-score-of-a-subarray.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxFrequencyScore(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        MOD = (10**9 + 7)
+        
+        @cache
+        def modpow(b, e):
+            return b ** e % MOD
+        
+        ct = Counter(nums[:k])
+        cur = 0
+        for v in ct:
+            cur = (cur + modpow(v, ct[v])) % MOD
+        ans = cur
+        for i in range(1, n - k + 1):
+            L = nums[i - 1]
+            R = nums[i + k - 1]
+            if L == R: 
+                continue
+            cur -= modpow(L, ct[L])
+            if ct[L] > 1:
+                cur += modpow(L, ct[L] - 1)
+            if ct[R]:
+                cur -= modpow(R, ct[R])
+            cur += modpow(R, ct[R] + 1)
+            cur %= MOD
+            ans = max(cur, ans)
+            ct[L] -= 1
+            ct[R] += 1
+        return ans
+    

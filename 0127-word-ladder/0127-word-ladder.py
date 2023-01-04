d295b7c837b93ea71d6ce9fb87ed43761939e751
Loi Tran <datloiboi@gmail.com>
2023-01-04 15:44:09 -0500

    Time: 268 ms (72.51%), Space: 17.5 MB (47.68%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,6 +1,7 @@
 '''
-Create graph matching each word pattern to it's viable patterns.
-Use BFS to search through all words for potential match to endWord.
+Validate input wordList. 
+Create graph of wildcarded words with potential matches
+Use BFS to search from start to end word. Iterate for every item in q and only add non seen words to q.
 '''
 
 class Solution:
@@ -14,20 +15,20 @@ class Solution:
             for j in range(len(w)):
                 pat = w[:j] + '*' + w[j+1:]
                 g[pat].append(w)
-        visit = set([beginWord])
+        seen = set([beginWord])
         q = deque([beginWord])
         res = 1
-
+        
         while q:
             for i in range(len(q)):
                 word = q.popleft()
                 if word == endWord:
-                    return res
+                    return res  
                 for j in range(len(word)):
                     pat = word[:j] + '*' + word[j+1:]
                     for nei in g[pat]:
-                        if nei not in visit:
-                            visit.add(nei)
+                        if nei not in seen:
+                            seen.add(nei)
                             q.append(nei)
             res+=1
         return 0

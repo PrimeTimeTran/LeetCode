033a4272b9eb490aa7bcc1f43b6a89e83f23097c
Loi Tran <datloiboi@gmail.com>
2023-01-03 19:24:47 -0500

    Time: 97 ms (57.11%), Space: 14.5 MB (52.95%) - LeetHub

---
+++ 0818-race-car/0818-race-car.py
@@ -0,0 +1,12 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        q = deque([[0,0,1]])
+        while q:
+            move, pos, speed = q.popleft()
+            if pos == target:
+                return move
+            q.append([move+1, pos+speed, speed*2])
+            if pos+speed > target and speed > 0 or pos+speed < target and speed < 0:
+                speed = -1 if speed > 0 else 1
+                q.append([move+1,pos,speed])
+            

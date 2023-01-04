f3b24c5e7a4716e0fa8e92fcbeb3073cf58a09dc
Loi Tran <datloiboi@gmail.com>
2023-01-03 20:22:44 -0500

    Time: 974 ms (94.43%), Space: 28.1 MB (96.23%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
         cnt = Counter(tasks).values()
-        if 1 in set(cnt): return -1
+        if 1 in cnt: return -1
         return sum([ceil(v/3) for v in cnt])
     
         def tot(n):

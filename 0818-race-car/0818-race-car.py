57f55b3ef7bcfcba35c68aeb0aafdf08094b5b3e
Loi Tran <datloiboi@gmail.com>
2023-01-04 10:08:27 -0500

    Time: 74 ms (69.70%), Space: 14.4 MB (75.90%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,12 +1,27 @@
+'''
+BFS:
+Using q track movements toward target using BFS. Track moves, pos, speed in q
+
+Add updated q item in each process. 
+Check if pos is target. Return moves in so.
+
+Lastly check if pos > than target and speed pos, or pos < target and speed negative. If so add new item to q.
+
+
+
+'''
 class Solution:
     def racecar(self, target: int) -> int:
-        q = deque([[0,0,1]])
+        q = deque([(0,0,1)])
+        
         while q:
-            move, pos, speed = q.popleft()
+            moves, pos, speed = q.popleft()
             if pos == target:
-                return move
-            q.append([move+1, pos+speed, speed*2])
+                return moves
+            
+            q.append((moves+1, pos+speed, speed*2))
+            
             if pos+speed > target and speed > 0 or pos+speed < target and speed < 0:
                 speed = -1 if speed > 0 else 1
-                q.append([move+1,pos,speed])
-            
+                q.append((moves+1, pos, speed))
+                

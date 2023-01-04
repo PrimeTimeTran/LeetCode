68e816fd8095fb9ae7aa0d9334e583f57c59e610
Loi Tran <datloiboi@gmail.com>
2023-01-04 09:36:31 -0500

    Time: 333 ms (23.13%), Space: 14.7 MB (67.38%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.py
@@ -1,6 +1,14 @@
+'''
+Iterate through each digit in s. Add it as one's place to cur number.
+
+Compare new cur with k, if larger then increment ans and reset cur to be digit
+
+Return ans
+'''
+
 class Solution:
     def minimumPartition(self, s: str, k: int) -> int:
-        cur, ans = 0, 1
+        cur,ans = 0, 1
         for d in s:
             if int(d) > k: return -1
             cur = 10*cur+int(d)
@@ -8,7 +16,3 @@ class Solution:
                 ans+=1
                 cur = int(d)
         return ans
-            
-            
-            
-            

ce336d8973a8530aa9b83d5a817540f83736268b
Loi Tran <datloiboi@gmail.com>
2023-01-04 09:54:15 -0500

    Time: 3201 ms (61.65%), Space: 74.5 MB (38.63%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,20 +1,32 @@
+'''
+DFS: 
+From node one traverse through neighbors using adj list comparing roads to find the minimum score road.
+Guard against cycles by checking if road seen with a set.
+
+'''
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        g = defaultdict(dict)
+        g = defaultdict(list)
         for a,b,d in roads:
-            g[a][b] = g[b][a] = d
+            g[a].append((b,d))
+            g[b].append((a,d))
+            
             
         seen = set()
         self.res = float('infinity')
+        
         def dfs(n):
             if n in seen:
                 return
             seen.add(n)
-            for nei, d in g[n].items():
+            for nei, d in g[n]:
                 if nei not in seen:
                     dfs(nei)
                 self.res = min(self.res, d)
         
         dfs(1)
         return self.res
+        
+        
         

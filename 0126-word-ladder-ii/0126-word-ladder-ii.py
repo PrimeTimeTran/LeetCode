9df049c616d5ca512bab1f38eff27b40417c0e37
Loi Tran <datloiboi@gmail.com>
2023-01-04 17:58:43 -0500

    Time: 160 ms (36.64%), Space: 14.5 MB (63.61%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -3,40 +3,30 @@ Guard invalid input
 '''
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        graph = defaultdict(list)
-        
-        def word2repres(word):
-            return [word[0 : i] + '?' + word[i + 1 ::] for i in range(len(word))]
-        
-        wordList = wordList + [beginWord]
-        for word in wordList:
-            for r in word2repres(word):
-                graph[r].append(word)
-        
-        queue = deque([beginWord])
-        visited = set([beginWord])
-        parents = defaultdict(list)
-        res = []
-        while queue:
-            layer = set()
-            for i in range(len(queue)):
-                word = queue.popleft()
-                for r in  word2repres(word):
-                    for neigh in graph[r]:
-                        if neigh not in visited:
-                            layer.add(neigh)
-                            parents[neigh].append(word)
-            queue.extend(layer)
-            visited.update(layer)
-        
-        def dfs(node, curr):
-            if node == beginWord:
-                res.append(curr[::-1])
-                return
-            for par in parents[node]:
-                curr.append(par)
-                dfs(par, curr)
-                curr.pop()
-        
-        dfs(endWord, [endWord])
-        return res
+        graph = collections.defaultdict(list)
+        words = set(wordList)
+        if endWord not in words: return []
+        words.discard(beginWord)
+        q = {beginWord}
+        while q:
+            nq = set()
+            for word in q:
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    for i in range(len(word)):
+                        nw = word[:i] + c + word[i+1:]
+                        if nw in words:
+                            graph[nw].append(word)
+                            nq.add(nw)
+            words -= set(graph.keys())
+            q = nq
+            
+        # use DFS to reconstruct the path from end to begin
+        def dfs(word):
+            if word == beginWord:
+                return [[beginWord]]
+            res = []
+            for w in graph[word]:
+                res += [k + [word] for k in dfs(w)]
+            return res
+			
+        return dfs(endWord)

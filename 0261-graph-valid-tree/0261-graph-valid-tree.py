486755288ccdf912a38a2ed2c1ccd15cd203a928
Loi Tran <datloiboi@gmail.com>
2023-01-04 11:53:16 -0500

    Time: 114 ms (63.26%), Space: 15.3 MB (91.30%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -13,6 +13,25 @@ Union find or DFS
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if n - 1 != len(edges):
+            return False
+        parent = {i:i for i in range(n)}
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]            
+        
+        for x,y in edges:
+            rx, ry = find(x), find(y)
+            if rx == ry:
+                return False
+            parent[rx] = ry
+        
+        return True
+        
+        
+        
         g = defaultdict(list)
         
         for a,b in edges:

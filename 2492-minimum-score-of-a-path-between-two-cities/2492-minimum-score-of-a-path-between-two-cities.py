06e8dfc943f0db6709a73424e0a4cf4eff0ba810
Loi Tran <datloiboi@gmail.com>
2023-01-05 17:59:25 -0500

    Time: 4361 ms (39.01%), Space: 67.7 MB (63.91%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,27 +1,20 @@
-'''
-Create a graph using roads. 
-DFS from node 1 looking for smallest score road.
-
-'''
-
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         g = defaultdict(list)
-        
         for a,b,d in roads:
             g[a].append((b,d))
             g[b].append((a,d))
+
+        q = deque([1])
         
-        self.res = inf
         seen = set()
-        def dfs(n):
-            if n in seen:
-                return
+        res = inf
+        while q:
+            n = q.popleft()
             for nei, d in g[n]:
                 if nei not in seen:
-                    seen.add(n)
-                    dfs(nei)
-                self.res = min(self.res, d)
-        
-        dfs(1)
-        return self.res
+                    seen.add(nei)
+                    q.append(nei)
+                res = min(res, d)
+                    
+        return res

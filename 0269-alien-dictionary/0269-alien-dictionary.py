b36ec2f4289c7401f25a93ddb4b93b725c5325e3
Loi Tran <datloiboi@gmail.com>
2023-01-05 15:39:12 -0500

    Time: 85 ms (5.44%), Space: 13.9 MB (61.40%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,14 +1,22 @@
+'''
+Create a graph using chars of word.
+Guard for long matching prefix words appearing before shorter.
+Find first diff char of w1 and w2. Add w2 to g of w1
+'''
+
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        g = { c: set() for w in words for c in w }
+        g = {c: [] for w in words for c in w}
+        # g = defaultdict(list)
+        
         for i in range(len(words)-1):
             w1,w2 = words[i], words[i+1]
             minlength = min(len(w1), len(w2))
             if len(w1) > len(w2) and w1[:minlength] == w2[:minlength]:
-                return ""
+                return ''
             for j in range(minlength):
                 if w1[j] != w2[j]:
-                    g[w1[j]].add(w2[j])
+                    g[w1[j]].append(w2[j])
                     break
         res = []
         seen = {}
@@ -22,7 +30,8 @@ class Solution:
             res.append(c)
             return True
         
+        
+        
         for c in g:
             if not dfs(c): return ""
-        
-        return "".join(res[::-1])             
+        return "".join(res[::-1])

c75842062911b0c9c8798c39121acc10b829a40e
Loi Tran <datloiboi@gmail.com>
2023-01-05 18:24:37 -0500

    Time: 260 ms (10.18%), Space: 17.7 MB (9.01%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,53 +1,20 @@
-'''
-len(edges) == n - 1
-No cycles
-
-
-
-Union find or DFS
-
-
-
-
-'''
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n - 1 != len(edges):
-            return False
-        parent = {i:i for i in range(n)}
-        
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]            
-        
-        for x,y in edges:
-            rx, ry = find(x), find(y)
-            if rx == ry:
-                return False
-            parent[rx] = ry
-        
-        return True
-        
-        
-        
         g = defaultdict(list)
         
         for a,b in edges:
             g[a].append(b)
             g[b].append(a)
-        
+            
+            
         seen = set()
-        def dfs(n, p):
+        def dfs(n, prev):
             if n in seen:
                 return False
             seen.add(n)
-            
             for nei in g[n]:
-                if nei == p: continue
+                if nei == prev: continue
                 if not dfs(nei, n): return False
-                    
             return True
         
-        return dfs(0,-1) and len(seen) == n
+        return dfs(0, -1) and len(seen) == n

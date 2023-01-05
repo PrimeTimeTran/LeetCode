bed0e6d726f22f349519604a6ed2e484de15718b
Loi Tran <datloiboi@gmail.com>
2023-01-05 17:26:19 -0500

    Time: 125 ms (64.81%), Space: 17.2 MB (25.95%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,25 +1,21 @@
-'''
-Define find. Define union. Union with edges and return len of set found items.
-
-'''
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        parent = [i for i in range(n)]
+        g = defaultdict(list)
+        for a,b in edges:
+            g[a].append(b)
+            g[b].append(a)
         
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
+        seen = set()
+        def dfs(n):
+            if n in seen:
+                return 
+            seen.add(n)
+            for nei in g[n]:
+                dfs(nei)
         
-        def union(x,y):
-            rx,ry = find(x), find(y)
-            if rx != ry:
-                parent[rx] = ry
-        
-        
-        for x,y in edges:
-            union(x,y)
-        
-        
-        return len({find(i) for i in range(n)})
+        res = 0
+        for n in range(n):
+            if n not in seen:
+                dfs(n)
+                res+=1
+        return res

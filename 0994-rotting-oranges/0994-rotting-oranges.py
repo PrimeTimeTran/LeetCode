166f5995c57ce42b82dfb2ab6ae82ade08f8e624
Loi Tran <datloiboi@gmail.com>
2023-01-06 16:44:27 -0500

    Time: 58 ms (76.34%), Space: 13.9 MB (47.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,26 +1,38 @@
+'''
+Use nested loops to count number of fresh oranges and add any rotten orange to q.
+While q and number of fresh > 0, process each q item.
+When processing adj cells check for out of bounds and not fresh orange, 1. If so continue.
+Change this new cell to be rotten, append it to the q, decrement fresh.
+
+Add to time after each for loop of queue.
+Return time if fresh is 0 else return -1
+
+'''
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        time, fresh = 0, 0
-        m, n = len(grid), len(grid[0])
+    def orangesRotting(self, g: List[List[int]]) -> int:
+        q, f = deque([]), 0
+        
+        m, n = len(g), len(g[0])
         for r in range(m):
             for c in range(n):
-                if grid[r][c] == 1:
-                    fresh += 1
-                if grid[r][c] == 2:
-                    q.append([r, c])
+                if g[r][c] == 2:
+                    q.append([r,c])
+                if g[r][c] == 1:
+                    f +=1
+        t = 0
         d = [0,1,0,-1,0]
-        while q and fresh > 0:
-            for i in range(len(q)):
+        while q and f > 0:
+            for _ in range(len(q)):
                 r, c = q.popleft()
                 for i in range(4):
-                    nr, nc = d[i] + r, d[i+1] + c
+                    nr, nc = d[i]+r,d[i+1]+c
+                    
                     out = nr < 0 or nc < 0 or nr == m or nc == n
-                    if out or grid[nr][nc] != 1:
+                    
+                    if out or g[nr][nc] != 1:
                         continue
-                    grid[nr][nc] = 2
-                    q.append([nr, nc])
-                    fresh -= 1
-            time += 1
-
-        return time if fresh == 0 else -1
+                    f -= 1
+                    g[nr][nc] = 2
+                    q.append([nr,nc])
+            t+=1
+        return t if f == 0 else -1

ac783deb1312007e6356abd926fbf2cdf1cff145
Loi Tran <datloiboi@gmail.com>
2023-01-05 20:33:42 -0500

    Time: 136 ms (90.84%), Space: 17.3 MB (58.88%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,47 +1,29 @@
 '''
-Create graph/adj list/hm of wildcarded words with matches
-Use BFS to search graph. Start from begin word in graph to neighbor's adding non seen words to q.
-For every word wildcard varient, increment res.
-
-
-{
-    *ot: [hot, dot, lot],
-    h*t: [hot],
-    ho*: [hot],
-    d*t: [dot],
-    do*: [dot, dog],
-    *og: [dog, cog, log],
-    d*g: [dog],
-    l*t: [lot],
-    lo*: [lot, log],
-    c*g: [cog],
-    co*: [cog],
-}
+Create g with wildcard keys containing their variants.
+BFS from start looking for word matching end.
 
 '''
-
 class Solution:
-    def ladderLength(self, start: str, end: str, words: List[str]) -> int:
+    def ladderLength(self, s: str, e: str, words: List[str]) -> int:
         g = defaultdict(list)
-
+        
         for w in words:
             for j in range(len(w)):
                 pat = w[:j] + '*' + w[j+1:]
                 g[pat].append(w)
-
         res = 1
-        q = deque([start])
-        seen = set(start)
-        while q:
+        seen = set()        
+        q = deque([s])
+        while q: 
             for _ in range(len(q)):
                 w = q.popleft()
-                if w == end:
-                    return res
+                if w == e: return res
                 for j in range(len(w)):
                     pat = w[:j] + '*' + w[j+1:]
                     for nei in g[pat]:
                         if nei not in seen:
                             seen.add(nei)
                             q.append(nei)
-            res += 1
+            
+            res+=1
         return 0

6264c0bd7189854c1d6273ad74c1331101fe699b
Loi Tran <datloiboi@gmail.com>
2023-01-06 15:54:33 -0500

    Time: 1236 ms (91.30%), Space: 72.3 MB (26.74%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,27 +1,21 @@
-'''
-DFS
-Use undirected graph compared to given graph/adj list to identify how many roads we're missing.
-'''
-
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        g = defaultdict(list)        
-        
+        g = defaultdict(list)
         for a,b in connections:
             g[a].append(b)
             g[b].append(a)
             
         seen = set()
+        seen.add(0)
         self.res = 0
-        roads = {(a,b) for a, b in connections}
+        roads = {(a,b) for a,b in connections}
         def dfs(n):
             for nei in g[n]:
                 if nei in seen: continue
-                if (nei, n) not in roads:
-                    self.res +=1
+                if (nei,n) not in roads:
+                    self.res += 1
                 seen.add(nei)
                 dfs(nei)
-        seen.add(0)
-        dfs(0)
         
+        dfs(0)
         return self.res

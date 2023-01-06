66ed805f194bfee629ebb586078bd7bcc606a5ee
Loi Tran <datloiboi@gmail.com>
2023-01-06 18:05:49 -0500

    Time: 3374 ms (74.02%), Space: 112.1 MB (7.85%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,6 +1,7 @@
 '''
 Loop cells and DFS on unseen land in grid2.
-In DFS return true if bounds or water. If cell of grid1 water, recurse but still flag as invalid.
+In DFS return true if out of bounds, cell of grid 2 is water, or cell has been seen. 
+By default res is truue, but if grid1 of cell is water, set to false; recurse but still flag as invalid.
 '''
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
@@ -22,9 +23,9 @@ class Solution:
             res = dfs(r,c-1) and res
             return res
         
-        self.res = 0
+        res = 0
         for r in range(m):
             for c in range(n):
                 if grid2[r][c] and (r,c) not in seen and dfs(r,c):
-                    self.res +=1
-        return self.res
+                    res +=1
+        return res

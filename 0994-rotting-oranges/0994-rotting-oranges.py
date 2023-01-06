942124564152366b1fd2f8e2d424eb38a142da22
Loi Tran <datloiboi@gmail.com>
2023-01-06 16:16:45 -0500

    Time: 49 ms (95.27%), Space: 13.8 MB (91.80%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,16 +1,14 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        time, fresh = 0, 0
         q = deque()
-
+        time, fresh = 0, 0
+        m, n = len(grid), len(grid[0])
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
                     q.append([r, c])
-
         d = [0,1,0,-1,0]
         while q and fresh > 0:
             for i in range(len(q)):

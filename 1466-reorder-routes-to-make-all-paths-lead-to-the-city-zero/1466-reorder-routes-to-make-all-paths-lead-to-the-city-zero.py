2c55abe987bb4440bdf74c4373b7f9ca762fe785
Loi Tran <datloiboi@gmail.com>
2023-01-06 15:49:10 -0500

    Time: 1176 ms (96.61%), Space: 71.8 MB (42.76%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,28 +1,27 @@
 '''
-Create a undirected acyclic graph as "happy case".
-Traverse happy case graph and if it isn't found in "reality" graph set, increment res.
+DFS
+Use undirected graph compared to given graph/adj list to identify how many roads we're missing.
 '''
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        neighbors = defaultdict(list)
-
-        for a, b in connections:
-            neighbors[a].append(b)
-            neighbors[b].append(a)
-
+        g = defaultdict(list)        
+        
+        for a,b in connections:
+            g[a].append(b)
+            g[b].append(a)
+            
         seen = set()
         self.res = 0
-        roads = {(a, b) for a, b in connections}
-        def dfs(c):
-            for n in neighbors[c]:
-                if n in seen:
-                    continue
-                if (n, c) not in roads:
-                    self.res += 1
-                seen.add(n)
-                dfs(n)
-
+        roads = {(a,b) for a, b in connections}
+        def dfs(n):
+            for nei in g[n]:
+                if nei in seen: continue
+                if (nei, n) not in roads:
+                    self.res +=1
+                seen.add(nei)
+                dfs(nei)
         seen.add(0)
         dfs(0)
+        
         return self.res

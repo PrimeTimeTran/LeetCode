4386bec33469f9bc8169b382b10c2f310286a985
Loi Tran <datloiboi@gmail.com>
2023-01-06 13:08:33 -0500

    Time: 5986 ms (44.51%), Space: 111.9 MB (8.96%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,27 +1,28 @@
+'''
+DFS Traversal if unseen land cells of grid2.
+
+'''
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        seen = set()
         m, n = len(grid1), len(grid1[0])
-
-        def dfs(r, c):
-            out = (
-                r < 0 or c < 0 or r == m or c == n or grid2[r][c] == 0 or (r, c) in seen
-            )
+        seen = set()
+        def dfs(r,c):
+            out = r < 0 or c < 0 or r == m or c == n or grid2[r][c] == 0 or (r,c) in seen
             if out:
                 return True
-            seen.add((r, c))
+            seen.add((r,c))
             res = True
             if grid1[r][c] == 0:
                 res = False
-            res = dfs(r + 1, c) and res
-            res = dfs(r - 1, c) and res
-            res = dfs(r, c + 1) and res
-            res = dfs(r, c - 1) and res
+            
+            res = dfs(r+1,c) and res
+            res = dfs(r-1,c) and res
+            res = dfs(r,c+1) and res
+            res = dfs(r,c-1) and res
             return res
-
-        count = 0
+        ct = 0
         for r in range(m):
             for c in range(n):
-                if grid2[r][c] and (r, c) not in seen and dfs(r, c):
-                    count += 1
-        return count
+                if grid2[r][c] and (r,c) not in seen and dfs(r,c):
+                    ct+=1
+        return ct

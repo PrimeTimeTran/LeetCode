0bb80e3ad5b7a4581bf45cbd01c932d9c2026603
Loi Tran <datloiboi@gmail.com>
2023-01-06 15:10:05 -0500

    Time: 748 ms (59.82%), Space: 26.3 MB (16.52%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,20 +1,25 @@
+'''
+Traverse using nested loops calling DFS on unseen land cells.
+In DFS, guard for our, water, seen.
+Return 1 + recursive call to neighbors.
+'''
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        m,n = len(grid), len(grid[0])
-        
+    def islandPerimeter(self, g: List[List[int]]) -> int:
+        m, n = len(g), len(g[0])
         seen = set()
         def dfs(r,c):
             out = r < 0 or c < 0 or r == m or c == n
-            if out or grid[r][c] == 0:
-                return 1
-            if (r,c) in seen:
-                return 0 
+            if out or g[r][c] == 0: return 1
+            if (r,c) in seen: return 0
             seen.add((r,c))
-            
-            return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
+            res = 0
+            d = [0,1,0,-1,0]
+            for i in range(4):
+                nr, nc = r + d[i], c + d[i+1]
+                res += dfs(nr, nc)
+            return res
         
         for r in range(m):
             for c in range(n):
-                if grid[r][c]:
+                if g[r][c]:
                     return dfs(r,c)
-                

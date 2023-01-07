9cbfbedec5d611ff953e967fce5542a083a6643e
Loi Tran <datloiboi@gmail.com>
2023-01-07 16:34:36 -0500

    Time: 158 ms (70.99%), Space: 16.2 MB (32.60%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,36 +1,36 @@
-'''
-With nested loops transform perimeter island cells to T guarding for cycles. 
-With nested loops transform O cells to X cells.
-With nested loops transform T cells to O cells.
-'''
-
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
-        m, n = len(board), len(board[0])
-        
-        def capture(r,c):
-            out = r < 0 or c < 0 or r == m or c == n
-            if out or board[r][c] != 'O':
+    def solve(self, b: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m,n = len(b), len(b[0])
+        seen = set()
+        def dfs(r,c):
+            out = r < 0 or c < 0 or r == m or c == n or (r,c) in seen 
+            if out:
                 return
-            board[r][c] = 'T'
-
-            capture(r+1,c)
-            capture(r-1,c)
-            capture(r,c+1)
-            capture(r,c-1)
-
+            if b[r][c] == 'X':
+                return
+            seen.add((r,c))
+            b[r][c] = 'T'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            
+            
+            
         for r in range(m):
             for c in range(n):
-                perimeter = (r in [0, m-1] or c in [0, n-1])
-                if board[r][c] == 'O' and perimeter:
-                    capture(r,c)                   
-        
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
+                if b[r][c] == 'O' and (r in [0, m-1] or c in [0, n - 1]):
+                    dfs(r,c)
                     
         for r in range(m):
             for c in range(n):
-                if board[r][c] == 'T':
-                    board[r][c] = 'O'
+                if b[r][c] == 'O':
+                    b[r][c] = 'X'
+                    
+        for r in range(m):
+            for c in range(n):
+                if b[r][c] == 'T':
+                    b[r][c] = 'O'

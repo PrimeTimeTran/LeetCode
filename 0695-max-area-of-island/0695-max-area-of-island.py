217023df691eefa32c10525a72155def6d42fac8
Loi Tran <datloiboi@gmail.com>
2023-01-07 16:01:18 -0500

    Time: 149 ms (78.83%), Space: 17.8 MB (26.29%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,8 +1,3 @@
-'''
-Loop grid performing DFS on any land found. 
-DFS marks all land cells neighboring as seen and returns the sum of recursive calls.
-'''
-
 class Solution:
     def maxAreaOfIsland(self, g: List[List[int]]) -> int:
         m,n = len(g), len(g[0])
@@ -11,13 +6,15 @@ class Solution:
         def dfs(r,c):
             out = r < 0 or c < 0 or r == m or c == n
             if out: return 0
-            if g[r][c] == 0 or (r,c) in seen:
+            if (r,c) in seen or g[r][c] == 0:
                 return 0
+            
             seen.add((r,c))
             return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
         
-        res = 0
+        self.res = 0
         for r in range(m):
             for c in range(n):
-                res = max(dfs(r,c), res)
-        return res
+                if g[r][c]:
+                    self.res = max(self.res, dfs(r,c))
+        return self.res

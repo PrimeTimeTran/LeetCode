98449ec239717a10c014d30a8ff044afee2fef12
Loi Tran <datloiboi@gmail.com>
2023-01-07 16:04:42 -0500

    Time: 36 ms (88.73%), Space: 13.9 MB (34.88%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        ord = {c:i for i, c in enumerate(order)}
+        o = {c: i for i, c in enumerate(order)}
         
         for i in range(len(words)-1):
             w1,w2 = words[i], words[i+1]
@@ -9,9 +9,7 @@ class Solution:
                 if j == len(w2):
                     return False
                 if w1[j] != w2[j]:
-                    if ord[w1[j]] > ord[w2[j]]:
+                    if o[w1[j]] > o[w2[j]]:
                         return False
                     break
-                
-        
         return True

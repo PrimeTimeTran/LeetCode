6b7e5f8f398bd2af28ef786b9b9526e9dd2bf25a
Loi Tran <datloiboi@gmail.com>
2023-01-06 23:20:02 -0500

    Time: 99 ms (87.14%), Space: 13.9 MB (81.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,13 +1,13 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        m,n = len(board), len(board[0])
+    def isValidSudoku(self, b: List[List[str]]) -> bool:
+        m,n = len(b), len(b[0])
         rowSets = defaultdict(set)
         colSets = defaultdict(set)
         squareSets = defaultdict(set)
         
         for r in range(m):
             for c in range(n):
-                num = board[r][c]
+                num = b[r][c]
                 if num == '.':
                     continue
                 if num in rowSets[r] or num in colSets[c] or num in squareSets[(r//3, c//3)]:

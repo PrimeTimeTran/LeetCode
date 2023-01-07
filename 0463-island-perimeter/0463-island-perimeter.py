6d69eae83d1a0c0e279e163f1ada17c95c8effab
Loi Tran <datloiboi@gmail.com>
2023-01-07 15:57:48 -0500

    Time: 642 ms (72.19%), Space: 28.9 MB (10.64%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,17 +1,17 @@
 class Solution:
-    def islandPerimeter(self, g: List[List[int]]) -> int:
+    def islandPerimeter(self,g: List[List[int]]) -> int:
         m,n=len(g), len(g[0])
         
         seen = set()
         def dfs(r,c):
-            out =  r < 0 or c < 0 or r == m or c == n
-            if out or g[r][c] == 0: return 1
-            
-            if (r,c) in seen: return 0
-            
+            out = r < 0 or c < 0 or r == m or c == n 
+            if out or g[r][c] == 0:
+                return 1
+            if (r,c) in seen:
+                return 0
             seen.add((r,c))
             
-            return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
+            return dfs(r+1,c) +dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
         
         for r in range(m):
             for c in range(n):

550ffc39595016e9caf5cecc1b748727f447aac8
Loi Tran <datloiboi@gmail.com>
2023-01-08 13:11:12 -0500

    Time: 109 ms (90.56%), Space: 15.5 MB (19.39%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,19 +1,27 @@
-from heapq import heappop, heappush
+'''
+Dijkstra
+Create graph with flights and BFS with PQ. Check for cycles and update res on each item.
+
+'''
+
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [inf] * n
-        prices[src] = 0
+        g = defaultdict(list)
+        for u,v,w in flights:
+            g[u].append((v, w))
+            
+        # price, src, steps remeaning
+        pq, seen = [(0, src, k + 1)],  {}
+        while pq: 
+            price, u, s = heapq.heappop(pq)
+            if u == dst:
+                return price
+            if u in seen and seen[u] >= s: continue
+            seen[u] = s
+            
+            if s:
+                for v, w in g[u]:
+                    heapq.heappush(pq, [price+w, v, s-1])
+        return -1
+                    
         
-        for _ in range(k+1):
-            tmp = prices.copy()
-            
-            for s, d, p in flights:
-                if prices[s] == inf: continue
-                
-                if prices[s]+p < tmp[d]:
-                    tmp[d] = prices[s]+p
-            
-            
-            prices = tmp
-            
-        return -1 if prices[dst] == inf else prices[dst]

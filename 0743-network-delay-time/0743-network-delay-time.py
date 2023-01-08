ea402b5dde925d5a227cab5fb4faef51d9cc2222
Loi Tran <datloiboi@gmail.com>
2023-01-08 12:27:38 -0500

    Time: 452 ms (96.31%), Space: 16.8 MB (12.99%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,21 +1,25 @@
+'''
+Use BFS to implement dikjstra's algorithm.
+Take the shortest path possible first from each node working through neighbors watching for cycles.
 
+'''
 
+from heapq import heappush, heappop
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         g = defaultdict(list)
+        
         for u,v,w in times:
-            g[u].append((v,w))
-            
-            
-        seen=set()
-        q = [(0,k)]
+            g[u].append([v,w])
+        seen = set()
+        q = [[0,k]]
         while q:
-            t, u = heapq.heappop(q)
-            if u in seen:
-                continue
+            t, u = heappop(q)
+            if u in seen: continue
             seen.add(u)
             if len(seen) == n: break
             for v, nt in g[u]:
-                heapq.heappush(q, (nt+t, v))
+                heappush(q, [t+nt, v])
+            
         
         return t if len(seen) == n else -1

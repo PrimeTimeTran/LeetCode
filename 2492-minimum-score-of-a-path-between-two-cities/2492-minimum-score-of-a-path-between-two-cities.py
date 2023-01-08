77ac8a9cbef7a5406769cb4eaa1a42a2e2626548
Loi Tran <datloiboi@gmail.com>
2023-01-08 17:29:13 -0500

    Time: 1714 ms (94.07%), Space: 68.5 MB (60.43%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,9 +1,14 @@
 '''
-Because we can visit vertices and edges multiple times we simply search for the min road
-in the path connected to 1. We're guaranteed to have one path from 1 to n.
+Because we can visit cities and roads multiple times we search for the min road
+any path connected to 1. We're guaranteed to have one path from 1 to n.
 
-DFS / BFS
-Use G traversal updating min on every edge.
+DFS
+Use G traversal updating min on every edge. Guard for cycles and move from node to neighbor
+using the adj list.
+
+BFS
+Use G traversal updating min on every edge. Guard cycles
+
 '''
 
 class Solution:
@@ -14,18 +19,27 @@ class Solution:
             g[u].append((v,w))
             g[v].append((u,w))
         
-        self.res, seen = inf, set()
-        def dfs(n):
-            if n in seen:
-                return
-            seen.add(n)
-            for nei, w in g[n]:
-                if nei not in seen:
-                    
-                    dfs(nei)
-                    
-                self.res = min(self.res, w)
-        dfs(1)
+        # self.res, seen = inf, set()
+        # def dfs(n):
+        #     if n in seen:
+        #         return
+        #     seen.add(n)
+        #     for nei, w in g[n]:
+        #         if nei not in seen:
+        #             dfs(nei)
+        #         self.res = min(self.res, w)
+        # dfs(1)
+        q = deque([1])
+        seen = set()
+        self.res = inf
+        while q:
+            u = q.popleft()
+            for v, d in g[u]:
+                if v not in seen:
+                    seen.add(v)
+                    q.append(v)
+                self.res = min(self.res, d)
+        
         return self.res
         
         

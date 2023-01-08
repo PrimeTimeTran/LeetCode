a51b2e5a9960ee1b7ce541fff039fbf1a5f27eb7
Loi Tran <datloiboi@gmail.com>
2023-01-08 12:21:42 -0500

    Time: 187 ms (71.23%), Space: 15.1 MB (73.89%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,21 +1,19 @@
 from heapq import heappop, heappush
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [inf]*n
+        prices = [inf] * n
         prices[src] = 0
-        for i in range(k+1):
+        
+        for _ in range(k+1):
             tmp = prices.copy()
             
             for s, d, p in flights:
-                if prices[s] == inf:
-                    continue
+                if prices[s] == inf: continue
+                
                 if prices[s]+p < tmp[d]:
                     tmp[d] = prices[s]+p
+            
+            
             prices = tmp
-                
+            
         return -1 if prices[dst] == inf else prices[dst]
-                
-                    
-
-# Bellman Ford
-# this time o(e*k), normally o(e*v)

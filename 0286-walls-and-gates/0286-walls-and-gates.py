a0df3241f465801d778eb351a18ba57d45ec8b9f
Loi Tran <datloiboi@gmail.com>
2023-01-07 19:54:32 -0500

    Time: 479 ms (58.87%), Space: 16.8 MB (91.96%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -6,16 +6,16 @@ class Solution:
         for r in range(m):
             for c in range(n):
                 if g[r][c] == 0:
-                    queue = deque([])
-                    queue.append((r+1, c, 1)); queue.append((r-1, c, 1))
-                    queue.append((r, c+1, 1)); queue.append((r, c-1, 1))
+                    q = deque([])
+                    q.append((r+1, c, 1)); q.append((r-1, c, 1))
+                    q.append((r, c+1, 1)); q.append((r, c-1, 1))
                     seen = set()
-                    while queue:
-                        x, y, val = queue.popleft()
+                    while q:
+                        x, y, val = q.popleft()
                         out  = x < 0 or x >= m or y < 0 or y >= n
                         if out or g[x][y] in [0, -1] or (x, y) in seen:
                             continue
                         seen.add((x, y))
                         g[x][y] = min(g[x][y], val)
-                        queue.append((x+1, y, val+1)); queue.append((x-1, y, val+1))
-                        queue.append((x, y+1, val+1)); queue.append((x, y-1, val+1))
+                        q.append((x+1, y, val+1)); q.append((x-1, y, val+1))
+                        q.append((x, y+1, val+1)); q.append((x, y-1, val+1))

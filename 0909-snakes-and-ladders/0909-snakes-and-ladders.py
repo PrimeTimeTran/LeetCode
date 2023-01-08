40641f48d69a652cdd54599f43396599ac084358
Loi Tran <datloiboi@gmail.com>
2023-01-07 20:04:51 -0500

    Time: 117 ms (90.37%), Space: 13.9 MB (94.40%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,27 +1,29 @@
 class Solution:
-  def snakesAndLadders(self, board: List[List[int]]) -> int:
-    length = len(board)
-    board.reverse()
-    def intToPos(square):
-      r = (square - 1 ) // length
-      c = (square - 1) % length
-      if r % 2:
-        c = length - 1 - c
-      return [r,c]
-    q = deque()
-    q.append([1,0])
-    visit = set()
-    while q:
-      square, moves = q.popleft()
-      
-      for i in range(1, 7):
-        nxtSquare = square+i
-        r,c = intToPos(nxtSquare)        
-        if board[r][c] != -1:
-          nxtSquare = board[r][c]
-        if nxtSquare == length*length:
-          return moves + 1
-        if  nxtSquare not in visit:
-          visit.add(nxtSquare)
-          q.append([nxtSquare, moves+1])
-    return -1
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        length = len(board)
+        board.reverse()
+
+        def intToPos(square):
+            r = (square - 1) // length
+            c = (square - 1) % length
+            if r % 2:
+                c = length - 1 - c
+            return [r, c]
+
+        q = deque()
+        q.append([1, 0])
+        visit = set()
+        while q:
+            square, moves = q.popleft()
+
+            for i in range(1, 7):
+                nxtSquare = square + i
+                r, c = intToPos(nxtSquare)
+                if board[r][c] != -1:
+                    nxtSquare = board[r][c]
+                if nxtSquare == length * length:
+                    return moves + 1
+                if nxtSquare not in visit:
+                    visit.add(nxtSquare)
+                    q.append([nxtSquare, moves + 1])
+        return -1

d9fb8459d286b32cf9fe252c20a0c1db094dfa1d
Loi Tran <datloiboi@gmail.com>
2023-01-08 16:25:54 -0500

    Time: 881 ms (49.66%), Space: 16 MB (91.28%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,23 +1,9 @@
-'''
-BFS G using PQ. Push time forward from each node. Guard for cycles and if seen node matches n, break.
-'''
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        g = defaultdict(list)
-        
-        for u,v,w in times:
-            g[u].append((v,w))
-            
-        pq, seen = [[0,k]], set()
-        
-        while pq:
-            time, u = heapq.heappop(pq)
-            # time, u = pq.pop()
-            if u in seen: continue
-            seen.add(u)
-            if len(seen) == n: return time
-            for v, nt in g[u]:
-                heapq.heappush(pq, [time+nt, v])
-                # pq.append([time+nt, v])
-        return -1
-        return time if len(seen) == n else -1
+        dist = [inf] *n
+        dist[k-1] = 0
+        for _ in range(n-1):
+            for u, v, w in times:
+                if dist[u-1] + w < dist[v-1]:
+                    dist[v-1] = dist[u-1] + w
+        return max(dist) if max(dist) < float("inf") else -1

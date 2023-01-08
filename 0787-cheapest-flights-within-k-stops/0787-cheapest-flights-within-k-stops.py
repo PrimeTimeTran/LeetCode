58be2cddafbffb7d8a0ffc67fbd3bb0be64d1f9a
Loi Tran <datloiboi@gmail.com>
2023-01-08 02:13:20 -0500

    Time: 109 ms (90.56%), Space: 15.3 MB (55.10%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,19 +1,38 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [inf]*n
-        prices[src] = 0
+        g = defaultdict(list)
+        for u, v, w in flights:
+            g[u].append([v, w])
 
-        for i in range(k+1):
-            tmpPrices = prices.copy()
+        pq, seen = [(0, src, k + 1)], dict()
+        while pq:
+            d, u, s = heapq.heappop(pq)
+            if u == dst:
+                return d
 
-            for s, d, p in flights:
-                if prices[s] == inf:
-                    continue
-                if prices[s]+p < tmpPrices[d]:
-                    tmpPrices[d] = prices[s] + p
-            prices = tmpPrices
+            if u in seen and seen[u] >= s:
+                continue
+            seen[u] = s
 
-        return -1 if prices[dst] == inf else prices[dst]
+            if s:
+                for v, w in g[u]:
+                    heapq.heappush(pq, (d + w, v, s - 1))
+        return -1
+
+#         prices = [inf]*n
+#         prices[src] = 0
+
+#         for i in range(k+1):
+#             tmpPrices = prices.copy()
+
+#             for s, d, p in flights:
+#                 if prices[s] == inf:
+#                     continue
+#                 if prices[s]+p < tmpPrices[d]:
+#                     tmpPrices[d] = prices[s] + p
+#             prices = tmpPrices
+
+#         return -1 if prices[dst] == inf else prices[dst]
 
 
 # Bellman Ford

ef3a972c5fed443422d7e24d7ffaadf85ab48fe6
Loi Tran <datloiboi@gmail.com>
2023-01-08 21:58:26 -0500

    Time: 643 ms (87.56%), Space: 15.2 MB (92.35%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -4,7 +4,7 @@ BFS through all combinations stopping on deadends. Add neighbors to Q.
 Find neighbors by traversing code index's and adding both -1 and 1, 
 yielding the codes with new val injected.
 
-Increment res once for each layer
+Increment res once for each layer. The first layer being 8
 
 '''
 
@@ -15,8 +15,7 @@ class Solution:
                 x = int(code[i])
                 for diff in [1,-1]:
                     y = (x+diff+10) % 10
-                    yield code[:i]+str(y)+code[i+1:]
-            
+                    yield code[:i] + str(y) + code[i+1:]
         deadSet = set(deadends)
         if "0000" in deadSet: return -1
         q = deque(["0000"])
@@ -24,12 +23,14 @@ class Solution:
         while q:
             for _ in range(len(q)):
                 cur = q.popleft()
-                if cur == target: return steps
+                if cur == target:
+                    return steps
                 for nei in neighbors(cur):
                     if nei in deadSet: continue
                     deadSet.add(nei)
                     q.append(nei)
 
-            steps+=1
+            steps += 1
+            
 
         return -1

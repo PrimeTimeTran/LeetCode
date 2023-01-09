01ecba3ae531d70b4ecc4c3646899ad7abbd3abf
Loi Tran <datloiboi@gmail.com>
2023-01-09 16:21:05 -0500

    Time: 99 ms (65.23%), Space: 14.7 MB (34.90%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,6 +1,9 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         g = defaultdict(list)
+        print(tickets)
+        print(sorted(tickets))
+        print(sorted(tickets)[::-1])
         for a, b in sorted(tickets)[::-1]:
             g[a].append(b)
         

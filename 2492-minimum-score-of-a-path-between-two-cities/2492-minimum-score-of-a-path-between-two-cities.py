89cd720c4696b93f8ed8161274868434ff4b94a3
Loi Tran <datloiboi@gmail.com>
2023-01-09 14:10:07 -0500

    Time: 1851 ms (87.33%), Space: 74 MB (42.96%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -3,43 +3,33 @@ Because we can visit cities and roads multiple times we search for the min road
 any path connected to 1. We're guaranteed to have one path from 1 to n.
 
 DFS
-Use G traversal updating min on every edge. Guard for cycles and move from node to neighbor
+Use G traversal updating min every item. Guard for cycles and move from node to neighbor
 using the adj list.
 
-BFS
-Use G traversal updating min on every edge. Guard cycles
+min 5
+cur 3
 
+
+BFS
+Use G traversal updating min on every edge. Guard cycles by only adding unseen nodes to q after marking complete.
 '''
 
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         g = defaultdict(list)
         
-        for u,v,w in roads:
+        for u, v, w in roads:
             g[u].append((v,w))
             g[v].append((u,w))
-        
-        # self.res, seen = inf, set()
-        # def dfs(n):
-        #     if n in seen:
-        #         return
-        #     seen.add(n)
-        #     for nei, w in g[n]:
-        #         if nei not in seen:
-        #             dfs(nei)
-        #         self.res = min(self.res, w)
-        # dfs(1)
-        q = deque([1])
-        seen = set()
-        self.res = inf
-        while q:
-            u = q.popleft()
-            for v, d in g[u]:
+            
+        seen, self.res = set(), inf
+        def dfs(n):
+            if n in seen:
+                return
+            seen.add(n)
+            for v, dis in g[n]:
                 if v not in seen:
-                    seen.add(v)
-                    q.append(v)
-                self.res = min(self.res, d)
-        
+                    dfs(v)
+                self.res = min(self.res, dis)
+        dfs(1)
         return self.res
-        
-        

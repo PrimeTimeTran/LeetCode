d43acded45dc030d544053f01eb303893b7910f4
Loi Tran <datloiboi@gmail.com>
2023-01-09 14:43:09 -0500

    Time: 484 ms (78.53%), Space: 16.1 MB (57.50%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,9 +1,16 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        dist = [inf] *n
-        dist[k-1] = 0
-        for _ in range(n-1):
-            for u, v, w in times:
-                if dist[u-1] + w < dist[v-1]:
-                    dist[v-1] = dist[u-1] + w
-        return max(dist) if max(dist) < inf else -1
+        g = defaultdict(list)
+        for u,v,w in times:
+            g[u].append((v,w))
+            
+        seen, q = set(), [[0,k]]
+        
+        while q:
+            t, u = heapq.heappop(q)
+            if u in seen: continue
+            seen.add(u)
+            if len(seen) == n: return t
+            for v, nt in g[u]:
+                heapq.heappush(q, (nt+t, v))
+        return -1

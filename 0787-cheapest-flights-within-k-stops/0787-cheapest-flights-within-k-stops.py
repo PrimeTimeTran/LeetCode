5f978cf077b312f25223ad54e1a4cbe902b97cce
Loi Tran <datloiboi@gmail.com>
2023-01-10 13:36:47 -0500

    Time: 108 ms (91.08%), Space: 15.5 MB (26.32%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,25 +1,20 @@
-'''
-BFS G with PQ with acc cost, src, stops.
-Check if u is destination and return cost if true. If we cross k, continue; updating seen of vertix with new stops value.
-Add neighbrs to PQ if k not yet crossed.
-
-'''
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         g = defaultdict(list)
+        
         for u,v,w in flights:
             g[u].append((v,w))
+            
         
-        # acc, src, moves
-        pq = [[0,src,k+1]]
+        q = [[0, src, k+1]]
         seen = {}
-        while pq:
-            cost, u, moves = heapq.heappop(pq)
+        
+        while q:
+            cost,u,stops = heapq.heappop(q)
             if u == dst: return cost
-            if u in seen and seen[u] >= moves: continue
-            seen[u] = moves - 1
-            if moves:
+            if u in seen and seen[u] >= stops : continue
+            seen[u] = stops - 1
+            if stops: 
                 for v, w in g[u]:
-                    heapq.heappush(pq, [cost+w, v,moves-1])
+                    heapq.heappush(q, (cost+w, v, stops-1))
         return -1

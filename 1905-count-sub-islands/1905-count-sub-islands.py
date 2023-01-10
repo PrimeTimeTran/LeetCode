3a4cc8457ce7a18bcf2caed7708a29c0d201f6e2
Loi Tran <datloiboi@gmail.com>
2023-01-10 17:03:08 -0500

    Time: 3436 ms (68.28%), Space: 111.9 MB (9.64%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,27 +1,28 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        m,n=len(grid2), len(grid2[0])
+        m,n = len(grid1), len(grid1[0])
         seen = set()
-        
         def dfs(r,c):
             out = r < 0 or c < 0 or r == m or c == n or (r,c) in seen or grid2[r][c] == 0
             if out:
                 return True
             seen.add((r,c))
-            
             res = True
             if grid1[r][c] == 0:
                 res = False
-            
             res = dfs(r+1,c) and res
             res = dfs(r-1,c) and res
             res = dfs(r,c+1) and res
             res = dfs(r,c-1) and res
             return res
             
+        
         res = 0
         for r in range(m):
             for c in range(n):
-                if grid2[r][c] and (r,c) not in seen and dfs(r,c):
-                    res+=1
+                if grid2[r][c] and (r,c) not in seen and dfs(r,c): 
+                    res +=1
+                    
         return res
+                    
+                

6d22ef602bb916c739a1a90f8715d352961efb55
Loi Tran <datloiboi@gmail.com>
2023-01-10 14:57:59 -0500

    Time: 81 ms (91.50%), Space: 14.4 MB (69.96%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,13 +1,33 @@
 class Solution:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        g = defaultdict(list)
-        for a, b in sorted(tickets)[::-1]:
-            g[a].append(b)
+#     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+#         g = defaultdict(list)
+#         for a, b in sorted(tickets)[::-1]:
+#             g[a].append(b)
         
-        route = []
-        def visit(airport):
-            while g[airport]:
-                visit(g[airport].pop())
-            route.append(airport)
-        visit('JFK')
-        return route[::-1]
+#         route = []
+#         def visit(airport):
+#             while g[airport]:
+#                 visit(g[airport].pop())
+#             route.append(airport)
+#         visit('JFK')
+#         return route[::-1]
+
+    def _DFS(self, graph, node):
+        neighbors = graph[node]
+        while neighbors:
+            nei = neighbors.pop(0)
+            self._DFS(graph, nei)
+        self.itinerary.append(node)
+    
+    def _makeGraph(self, edges):
+        graph = defaultdict(list)
+        for e in edges:
+            graph[e[0]].append(e[1])
+        return graph
+
+    def findItinerary(self, tickets):
+        tickets.sort(key= lambda x: x[1])
+        graph = self._makeGraph(tickets)
+        self.itinerary = []
+        self._DFS(graph, "JFK")
+        return self.itinerary[::-1]

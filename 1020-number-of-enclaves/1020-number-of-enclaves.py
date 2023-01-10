7f378e3185f04333624eb75a743fece66040bf80
Loi Tran <datloiboi@gmail.com>
2023-01-10 16:54:45 -0500

    Time: 988 ms (57.82%), Space: 104.7 MB (7.61%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,7 +1,6 @@
 class Solution:
     def numEnclaves(self, g: List[List[int]]) -> int:
         m,n = len(g), len(g[0])
-        
         seen = set()
         def dfs(r,c):
             out = r < 0 or c < 0 or r == m or c == n
@@ -9,13 +8,7 @@ class Solution:
                 return 0
             seen.add((r,c))
             g[r][c] = 0
-            
-            
-            
-            
-
             return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
-        
         res = 0
         for r in range(m):
             for c in range(n):

94f585a393000f5767091c8eef170516e2b39b03
Loi Tran <datloiboi@gmail.com>
2023-01-10 14:24:41 -0500

    Time: 187 ms (69.99%), Space: 15.1 MB (73.79%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,34 +1,32 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         prices = [inf]*n
-#         prices[src] = 0
-        
-#         for i in range(k+1):
-#             tmpPrices = prices.copy()
+        prices = [inf]*n
+        prices[src] = 0
+        for i in range(k+1):
+            tmp = prices.copy()
             
-#             for s,d,p in flights:
-#                 if prices[s] == inf:
-#                     continue
-#                 if prices[s]+p < tmpPrices[d]:
-#                     tmpPrices[d] = prices[s] + p
-#             prices = tmpPrices
-        
-#         return -1 if prices[dst] == inf else prices[dst]
+            for u,v,w in flights:
+                if prices[u] == inf:
+                    continue
+                if prices[u]+w < tmp[v]:
+                    tmp[v] = prices[u]+w
+            prices = tmp
+        return -1 if prices[dst] == inf else prices[dst]
     
-        g = defaultdict(list)
+#         g = defaultdict(list)
         
-        for u,v,w in flights:
-            g[u].append((v,w))
+#         for u,v,w in flights:
+#             g[u].append((v,w))
             
-        q = [[0, src, k+1]]
-        seen = {}
+#         q = [[0, src, k+1]]
+#         seen = {}
         
-        while q:
-            cost,u,stops = heapq.heappop(q)
-            if u == dst: return cost
-            if u in seen and seen[u] >= stops : continue
-            seen[u] = stops - 1
-            if stops: 
-                for v, w in g[u]:
-                    heapq.heappush(q, (cost+w, v, stops-1))
-        return -1
+#         while q:
+#             cost,u,stops = heapq.heappop(q)
+#             if u == dst: return cost
+#             if u in seen and seen[u] >= stops : continue
+#             seen[u] = stops - 1
+#             if stops: 
+#                 for v, w in g[u]:
+#                     heapq.heappush(q, (cost+w, v, stops-1))
+#         return -1

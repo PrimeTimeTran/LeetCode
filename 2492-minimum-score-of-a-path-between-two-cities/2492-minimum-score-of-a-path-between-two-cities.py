37beb749b421965a90f0d6c8a5beb3bd312be87a
Loi Tran <datloiboi@gmail.com>
2023-01-10 12:55:52 -0500

    Time: 1839 ms (87.56%), Space: 73.6 MB (44.23%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,34 +1,19 @@
-'''
-Because we can visit cities and roads multiple times we search for the min road
-any path connected to 1. We're guaranteed to have one path from 1 to n.
-
-DFS
-Use G traversal updating min every item. Guard for cycles and move from node to neighbor
-using the adj list.
-
-min 5
-cur 3
-
-
-BFS
-Use G traversal updating min on every edge. Guard cycles by only adding unseen nodes to q after marking complete.
-'''
-
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         g = defaultdict(list)
         
-        for u, v, w in roads:
+        for u,v,w in roads:
             g[u].append((v,w))
             g[v].append((u,w))
             
-        seen, self.res = set(), inf
-        def dfs(n):
-            if n in seen: return
-            seen.add(n)
-            for v, dis in g[n]:
+        self.res, seen = inf, set()
+        def dfs(u):
+            if u in seen: return
+            seen.add(u)
+            for v, w in g[u]:
                 if v not in seen:
                     dfs(v)
-                self.res = min(self.res, dis)
+                self.res = min(self.res, w)
+                    
         dfs(1)
         return self.res

85faad866ee53584207f533ceeda3f75abe63fd9
Loi Tran <datloiboi@gmail.com>
2023-01-10 17:59:40 -0500

    Time: 89 ms (58.18%), Space: 14 MB (89.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,11 +5,10 @@ class Solution:
         seen = set((sr,sc))
         old = image[sr][sc]
         image[sr][sc] = color
+        d = [0,1,0,-1,0]
         
         while q:
             r, c = q.popleft()
-            
-            d = [0,1,0,-1,0]
             for i in range(4):
                 nr,nc = r+d[i], c+d[i+1]
                 out = nr < 0 or nc < 0 or nr == m or nc == n or (nr,nc) in seen

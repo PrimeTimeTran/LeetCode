13f71e3deca723cb50ed88c8455b1ffc20dffcbf
Loi Tran <datloiboi@gmail.com>
2023-01-10 17:07:21 -0500

    Time: 44 ms (78.17%), Space: 14.3 MB (45.21%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,7 +1,5 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         for i, j in zip(s, t):
-            if s.find(i) == t.find(j):
-                continue
-            else:return False
+            if not s.find(i) == t.find(j): return False
         return True

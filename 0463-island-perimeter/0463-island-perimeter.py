4aa870bfb8c2773a9d47b071c26f9545a035b833
Loi Tran <datloiboi@gmail.com>
2023-01-11 00:25:43 -0500

    Time: 619 ms (73.45%), Space: 28.9 MB (9.15%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,19 +1,26 @@
+'''
+
+Find land using nested loops. When found sum the perimeter, water/border.
+Return the sum of those through recursion.
+'''
+
 class Solution:
-    def islandPerimeter(self,g: List[List[int]]) -> int:
-        m,n=len(g), len(g[0])
+    def islandPerimeter(self, g: List[List[int]]) -> int:
+        m,n = len(g),len(g[0])
         
         seen = set()
         def dfs(r,c):
-            out = r < 0 or c < 0 or r == m or c == n 
-            if out or g[r][c] == 0:
+            o = r < 0 or c < 0 or r == m or c == n or g[r][c] == 0
+            if o:
                 return 1
             if (r,c) in seen:
                 return 0
             seen.add((r,c))
+            return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
             
-            return dfs(r+1,c) +dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
         
         for r in range(m):
             for c in range(n):
                 if g[r][c]:
                     return dfs(r,c)
+                        

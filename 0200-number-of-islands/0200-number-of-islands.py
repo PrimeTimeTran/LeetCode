a12d75c216bb0e3c63e48876002640a96bdf616a
Loi Tran <datloiboi@gmail.com>
2023-01-11 16:50:36 -0500

    Time: 333 ms (74.59%), Space: 25.6 MB (9.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, g: List[List[str]]) -> int:
         m,n = len(g), len(g[0])
+        
         seen = set()
         def dfs(r,c):
             o = r < 0 or c < 0 or r == m or c == n
-            if o or (r,c) in seen or g[r][c] == '0':
-                return
+            if o:
+                return False
+            if (r,c) in seen or g[r][c] == "0":
+                return False 
             seen.add((r,c))
+            
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
             return True
             
-        
         res = 0
         for r in range(m):
             for c in range(n):
                 if dfs(r,c):
                     res+=1
         return res
-        
-        

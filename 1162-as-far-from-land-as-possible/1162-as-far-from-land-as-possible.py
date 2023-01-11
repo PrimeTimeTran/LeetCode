45682d303ded637d263113866f8b1d45e1c29408
Loi Tran <datloiboi@gmail.com>
2023-01-10 20:23:39 -0500

    Time: 525 ms (95.16%), Space: 14.6 MB (70.37%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -15,13 +15,3 @@ class Solution:
                         grid[xi][yj] = 1
             level += 1
         return level-1
-    
-        n, res = len(grid), 0
-        land = {(i, j) for i, j in product(range(n), range(n)) if grid[i][j]}
-        water = {(i, j) for i, j in product(range(n), range(n)) if not grid[i][j]}
-        while water:
-            if not land: return -1
-            land = {(x, y) for i, j in land for x, y in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)) if (x, y) in water}
-            water -= land
-            res += 1
-        return res or -1

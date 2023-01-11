3adf0f443f139acb06ef217e9835046cbe3fcaff
Loi Tran <datloiboi@gmail.com>
2023-01-11 00:10:45 -0500

    Time: 36 ms (85.39%), Space: 13.9 MB (94.07%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,10 +1,21 @@
 '''
-Topological Sort
-Create graph using chars of word of words.
-Iterate words 2 at a time, guarding for matching prefixes having a longer word before a shorter. Return "" if true.
-Loop through chars of w1 looking for first differing char. The first differing char add it to word one's list in the graph.
-Topsort of graph with loop detection.
-Return the reverse joined res because to create lexicographically increasing order string.
+1.
+Given a list of words and should return a string if valid lexicographicallyy sorted string can be found.
+
+2.
+
+t > f 
+w > e > r
+
+3.
+- Create adj list using chars of words.
+- Iterate thru pairs of words finding greatest largest difference.
+- Using G perform a topilogical sort and return reversed version.
+
+
+4.
+
+
 '''
 
 class Solution:
@@ -12,30 +23,29 @@ class Solution:
         g = {c: [] for w in words for c in w}
         
         for i in range(len(words)-1):
-            w1, w2 = words[i], words[i+1]
-            
-            minlength = min(len(w1), len(w2))
-            
-            # Check prefix appears after                    
-            if len(w1) > len(w2) and w1[:minlength] == w2[:minlength]:
+            w1,w2 = words[i], words[i+1]
+            minlen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minlen] == w2[:minlen]:
                 return ""
-            
-            for j in range(minlength):
+            for j in range(minlen):
                 if w1[j] != w2[j]:
                     g[w1[j]].append(w2[j])
                     break
-        res = []
-        seen = {}
+        
+        res, seen = [], {}
         def dfs(c):
             if c in seen:
                 return seen[c]
+            
             seen[c] = False
             for nei in g[c]:
                 if not dfs(nei): return False
             seen[c] = True
             res.append(c)
             return True
+        
         for c in g:
             if not dfs(c): return ""
         
-        return "".join(res[::-1])
+        
+        return ''.join(res[::-1])

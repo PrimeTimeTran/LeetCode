0b3bca68618c9a796c5ed6cc6b431d4d543e0a12
Loi Tran <datloiboi@gmail.com>
2023-01-10 22:11:35 -0500

    Time: 0 ms (100.00%), Space: 38.8 MB (93.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         int f0 = 1, f1 = 1; 
-        for (int i = 1; i <= n-1; ++i) {
+        for (int i = 1; i < n; ++i) {
             int ff = f0; 
             f0 = f1; 
             f1 = ff + f1; 

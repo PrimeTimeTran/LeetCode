1e89b81aea5349675370920c36f481a5b449e65e
Loi Tran <datloiboi@gmail.com>
2023-01-11 00:26:14 -0500

    Time: 607 ms (75.49%), Space: 28.9 MB (9.15%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -11,16 +11,12 @@ class Solution:
         seen = set()
         def dfs(r,c):
             o = r < 0 or c < 0 or r == m or c == n or g[r][c] == 0
-            if o:
-                return 1
-            if (r,c) in seen:
-                return 0
+            if o: return 1
+            if (r,c) in seen: return 0
             seen.add((r,c))
             return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
-            
         
         for r in range(m):
             for c in range(n):
                 if g[r][c]:
                     return dfs(r,c)
-                        

ca987e4017a945fd783820c949d06a1fc95d138e
Loi Tran <datloiboi@gmail.com>
2023-01-11 18:22:15 -0500

    Time: 292 ms (79.91%), Space: 15.7 MB (54.82%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,43 +1,40 @@
 '''
-Using two sets, mark cells which can be reached by pac and atl oceans by traversing coastlines.
-Using a loop check if the cell can be reached by both pac and atl. If so add it to the res.
+Find cells reachable by each ocean working outside in.
+If a cell is reachable by both oceans add to res.
 '''
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pac,atl = set(), set()
         m,n = len(heights), len(heights[0])
         
-        
-        def dfs(seen, r, c, prev):
-            out = r < 0 or c < 0 or r == m or c == n
-            if out:
-                return 
-            if prev > heights[r][c]:
+        def dfs(seen, r, c, p):
+            o = r < 0 or c < 0 or r == m or c == n
+            if o or p > heights[r][c] or (r,c) in seen:
                 return
-            if (r,c) in seen:
-                return
+            
             seen.add((r,c))
             cur = heights[r][c]
-            dfs(seen, r+1,c,cur)
-            dfs(seen, r-1,c,cur)
-            dfs(seen, r,c+1,cur)
-            dfs(seen, r,c-1,cur)
+            dfs(seen, r+1, c, cur)
+            dfs(seen, r-1, c, cur)
+            dfs(seen, r, c+1, cur)
+            dfs(seen, r, c-1, cur)
             
         
-        
-        pac, atl = set(), set()
         for r in range(m):
-            dfs(pac,r,0,heights[r][0])    
-            dfs(atl,r,n-1,heights[r][n-1])    
-        
+            dfs(atl, r, n-1, heights[r][n-1])    
+            dfs(pac, r, 0, heights[r][0])        
+            
         for c in range(n):
-            dfs(pac,0,c,heights[0][c])    
-            dfs(atl,m-1,c,heights[m-1][c])    
+            dfs(atl, m-1, c, heights[m-1][c])        
+            dfs(pac, 0, c, heights[0][c])    
         
         res = []
         for r in range(m):
             for c in range(n):
                 if (r,c) in atl and (r,c) in pac:
-                    res.append((r,c))
+                    res.append([r,c])
         
         return res
+        
+        

32975f77bcbc45fd0759b6763ae276c628896283
Loi Tran <datloiboi@gmail.com>
2023-01-10 23:45:51 -0500

    Time: 42 ms (78.39%), Space: 14.5 MB (25.73%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -4,11 +4,6 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-        
-        
-        
-Perform DFS on node copying the node and it's neighbors.
-Check for cycle using set and return the copy.
 """
 
 class Solution:
@@ -22,5 +17,4 @@ class Solution:
             for nei in n.neighbors:
                 copy.neighbors.append(dfs(nei))
             return copy
-        
         return dfs(node) if node else None

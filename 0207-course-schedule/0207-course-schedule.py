b231dc93065da3ab4faa5ef2d1e7377dec094a22
Loi Tran <datloiboi@gmail.com>
2023-01-10 23:50:33 -0500

    Time: 103 ms (82.89%), Space: 17.4 MB (14.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,7 +1,20 @@
+'''
+Create graph using edges. Loop over courses for classes we cannot finish using a DFS.
+DFS checks class has no prerequisite, hasn't been seen, and adds if True to both. 
+Next it recursively DFS on it's prerequisites. If recurse True up, it sets prerequisites to []
+and returns true.
+
+
+
+
+
+
+'''
 class Solution:
     def canFinish(self, N: int, P: List[List[int]]) -> bool:
         g = defaultdict(list)
-        for a,b in P:
+
+        for a, b in P:
             g[a].append(b)
         
         seen = set()
@@ -10,13 +23,15 @@ class Solution:
                 return True
             if n in seen:
                 return False
+            
             seen.add(n)
-            for nei in g[n]:
-                if not dfs(nei): return False
+            for pre in g[n]:
+                if not dfs(pre): return False
+                
             g[n] = []
             return True
+        
         for c in range(N):
             if not dfs(c): return False
-        
         
         return True

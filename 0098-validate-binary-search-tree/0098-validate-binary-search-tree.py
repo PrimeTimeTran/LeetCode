96d0aba93157925e456108e14c00a16b94a45a44
Loi Tran <datloiboi@gmail.com>
2023-01-11 22:49:37 -0500

    Time: 46 ms (83.93%), Space: 16.9 MB (24.44%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,21 +1,17 @@
 class Solution:
     def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
+        def valid(root):
+            if not root:
+                return (True, float('-inf'), float('inf'))
+
+            left_res = valid(root.left)
+            right_res = valid(root.right)
+
+            if not left_res[0] or not right_res[0] or root.val <= left_res[1] or root.val >= right_res[2]:
+                return (False, 0, 0)
+
+            return (True, max(root.val, right_res[1]), min(root.val, left_res[2]) )
         self.prev = None
-        return self.validBSTPostOrderHelper(root)[0]
+        return valid(root)[0]
 
     # The return result stores [True/False, max_so_far, min_so_far]
-    def validBSTPostOrderHelper(self, root):
-        if not root:
-            return (True, float('-inf'), float('inf'))
-
-        left_res = self.validBSTPostOrderHelper(root.left)
-        right_res = self.validBSTPostOrderHelper(root.right)
-
-        if not left_res[0] or not right_res[0] or root.val <= left_res[1] or root.val >= right_res[2]:
-            return (False, 0, 0)
-
-        return (True, max(root.val, right_res[1]), min(root.val, left_res[2]) )

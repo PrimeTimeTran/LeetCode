739f8ec4d08a1672d5c7ddb5c3e33a0fff6b1322
Loi Tran <datloiboi@gmail.com>
2023-01-11 23:36:43 -0500

    Time: 48 ms (78.59%), Space: 16.4 MB (77.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -2,15 +2,9 @@ class Solution:
     def isValidBST(self, root):
         def valid(n, left, right):
             if not n: return True
-
-            leftValid = valid(n.left, left, n.val)
-            rightValid = valid(n.right, n.val, right)
             
-            if not leftValid or not rightValid:
-                return False
-
             if n.val > left and n.val < right:
-                return True 
+                return valid(n.left, left, n.val) and valid(n.right, n.val, right)
             return False
-        
+                
         return valid(root, -inf, inf)

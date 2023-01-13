8c12b9d06b9b58bd59094bbdf836973f94c43260
Loi Tran <datloiboi@gmail.com>
2023-01-13 02:05:36 -0500

    Time: 106 ms (59.90%), Space: 15.7 MB (43.36%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,7 +1,6 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res = []
-        seen = set()
         def dfs(n, cur):
             if n == len(graph)-1:
                 res.append(cur+[n])

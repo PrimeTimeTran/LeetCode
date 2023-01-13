f483bf4cf903551c52cdac5dc32646fad898fa77
Loi Tran <datloiboi@gmail.com>
2023-01-13 14:10:18 -0500

    Time: 2544 ms (62.96%), Space: 157.9 MB (31.38%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,14 +1,14 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        children = [[] for i in range(len(s))]
+        g = defaultdict(list)
         for i,j in enumerate(parent):
             if j >= 0:
-                children[j].append(i)
+                g[j].append(i)
         
         res = [0]
         def dfs(i):
             candi = [0]
-            for j in children[i]:
+            for j in g[i]:
                 cur = dfs(j)
                 if s[i] != s[j]:
                     candi.append(cur)

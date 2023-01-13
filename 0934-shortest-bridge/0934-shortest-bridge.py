30e6555ecd541091994856d74b60d49582fa9621
Loi Tran <datloiboi@gmail.com>
2023-01-13 01:25:54 -0500

    Time: 385 ms (89.39%), Space: 19.8 MB (5.37%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -9,28 +9,28 @@ class Solution:
             
             for i in range(4):
                 nr, nc = r+DIR[i], c+DIR[i+1]
-                if not (0 <= nr < m and 0 <= nc < n) or (nr, nc) in seen or grid[r][c] != 1:
+                inbounds = 0 <= nr < m and 0 <= nc < n
+                if not inbounds or (nr, nc) in seen or grid[r][c] != 1:
                     continue
                 dfs(nr, nc)
         
         r, c = next((r, c) for r in range(m) for c in range(n) if grid[r][c] == 1)
         dfs(r, c)
         
-        q = deque(seen)
         level = 1
+        q = deque(seen)
         while q:
             for _ in range(len(q)):
                 r, c = q.popleft()
                 for i in range(4):
                     nr, nc = r+DIR[i], c+DIR[i+1]
-                    if (nr, nc) in seen or not (0 <= nr < m and 0 <= nc < n):
+                    inbounds = 0 <= nr < m and 0 <= nc < n
+                    if (nr, nc) in seen or not inbounds:
                         continue
                     if grid[nr][nc] == 1:
                         return level
                     
                     seen.add((nr, nc))
                     q.append((nr, nc))
-        
             level += 1
-            
         return -1

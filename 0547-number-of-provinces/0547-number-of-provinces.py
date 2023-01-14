cbd6632475044480c065346c4213690baf0763de
Loi Tran <datloiboi@gmail.com>
2023-01-14 12:41:21 -0500

    Time: 177 ms (99.29%), Space: 14.9 MB (11.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,20 +1,16 @@
-'''
-Loop through items in list and dfs marking them as seen.
-Increment res after each dfs, counting number of islands.
-'''
-
 class Solution:
     def findCircleNum(self, A: List[List[int]]) -> int:
-        res = 0
-        N = len(A)
+        g = defaultdict(list)
         seen = set()
-        def dfs(n):
-            for i, nei in enumerate(A[n]):
+        n = len(A)
+        def dfs(j):
+            for i, nei in enumerate(A[j]):
                 if nei and i not in seen:
                     seen.add(i)
                     dfs(i)
-        
-        for i in range(N):
+            
+        res = 0
+        for i in range(n):
             if i not in seen:
                 dfs(i)
                 res+=1

a394d7a585d24e243d035b3d6734d5f7441a7ecf
Loi Tran <datloiboi@gmail.com>
2023-01-14 17:03:15 -0500

    Time: 31 ms (90.52%), Space: 13.9 MB (62.07%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -6,5 +6,5 @@ class Solution:
                 hash.remove(c)
             else:
                 hash.add(c)
-        # len(hash) is the number of the odd letters
-        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
+        
+        return len(s) - len(hash)+1 if len(hash) > 0 else len(s)

97e2b7c0b50ced8a84471323e4462efe9571936c
Loi Tran <datloiboi@gmail.com>
2023-01-14 14:14:33 -0500

    Time: 46 ms (73.56%), Space: 14 MB (56.90%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -6,7 +6,6 @@ class Solution:
             if x != UF[x]:
                 UF[x] = find(UF[x])
             return UF[x]
-        
         def union(x,y):
             rootX = find(x)
             rootY = find(y)
@@ -14,17 +13,9 @@ class Solution:
                 UF[rootX] = rootY
             else:
                 UF[rootY] = rootX
-        
-        # Union the two equivalent characters
-        # at the same position from s1 and s2 into the same group.
         for i in range(len(s1)):
             union(s1[i],s2[i])
-        
-        # Simply find the root of the group a character belongs to
-        # Note that if c is not in any group, 
-        # we have UF.setdefault(x,x) in def find(x) to take care of it
         res = []
         for c in baseStr:
             res.append(find(c))
-            
         return ''.join(res)

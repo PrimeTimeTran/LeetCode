a83111bb5221e58862025f6fe29360a300a85305
Loi Tran <datloiboi@gmail.com>
2023-01-14 18:02:09 -0500

    Time: 32 ms (77.87%), Space: 13.8 MB (97.11%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,6 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        return bisect_left(range(n), True, 1, key=isBadVersion)

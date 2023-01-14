354ee414b7ddb7086f2e611872d63e5111bbd0d2
Loi Tran <datloiboi@gmail.com>
2023-01-13 19:17:40 -0500

    Time: 2614 ms (60.20%), Space: 158 MB (31.13%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,23 +1,26 @@
 '''
-Create graph usingg 
+- Create graph using an adj list
+- Understand a path a tree/graph
 '''
 
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        g = defaultdict(list)
-        for i,j in enumerate(parent):
-            if j >= 0:
-                g[j].append(i)
+        g = defaultdict(list)       
         
-        res = [0]
+        #  Build the graph
+        for i, n in enumerate(parent):
+            if i > 0:
+                g[n].append(i)
+
+        self.res = 0
         def dfs(i):
             cur = [0]
             for j in g[i]:
                 c = dfs(j)
-                if s[j] != s[i]:
+                if s[i] != s[j]:
                     cur.append(c)
             cur = nlargest(2, cur)
-            res[0] = max(res[0], sum(cur)+1) 
+            self.res = max(self.res, sum(cur)+1)
             return max(cur)+1
-        dfs(0)
-        return res[0]
+        dfs(0)        
+        return self.res

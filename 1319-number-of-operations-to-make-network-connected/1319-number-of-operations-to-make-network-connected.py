c36f2777bfc99ca8de2fd1fbbc4f761f70df66ae
Loi Tran <datloiboi@gmail.com>
2023-01-14 16:32:13 -0500

    Time: 514 ms (84.90%), Space: 49.1 MB (14.80%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,16 +1,20 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1: return -1
-        G = [set() for i in range(n)]
-        for i, j in connections:
-            G[i].add(j)
-            G[j].add(i)
+        g = defaultdict(list)
+        for a, b in connections:
+            g[a].append(b)
+            g[b].append(a)
+        
         seen = set()
-
-        def dfs(i):
-            if i in seen: return 0
-            seen.add(i)
-            for j in G[i]: dfs(j)
+        def dfs(n):
+            if n in seen:
+                return 0
+            seen.add(n)
+            for nei in g[n]:
+                dfs(nei)
             return 1
-
+        
         return sum(dfs(i) for i in range(n)) - 1
+        
+        

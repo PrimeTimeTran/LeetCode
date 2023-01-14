a5dbaf1f545024ea8ce067438deb01361f514768
Loi Tran <datloiboi@gmail.com>
2023-01-13 21:48:32 -0500

    Time: 179 ms (98.70%), Space: 14.9 MB (11.34%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findCircleNum(self, A: List[List[int]]) -> int:
+        res = 0
+        N = len(A)
+        seen = set()
+        def dfs(n):
+            for i, nei in enumerate(A[n]):
+                if nei and i not in seen:
+                    seen.add(i)
+                    dfs(i)
+            
+        for n in range(N):
+            if n not in seen:
+                dfs(n)
+                res+=1
+        return res

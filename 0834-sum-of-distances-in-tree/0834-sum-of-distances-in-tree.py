8670fd39ef20c0850fcdd257e623f4f58c0bd6f4
Loi Tran <datloiboi@gmail.com>
2023-01-14 14:00:32 -0500

    Time: 968 ms (90.82%), Space: 62.1 MB (84.04%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -1,6 +1,6 @@
 '''
 DFS 
-DFS twice with post and pre order traversal.
+DFS twice with post and pre order traversal. Similar too graph valid tree in that we must send in -1 to guard cycles.
 Create a count list which contains count of all nodes in subtree.
 Create res whcih contains distance.
 '''
@@ -12,18 +12,18 @@ class Solution:
         for a, b in edges:
             g[a].append(b); g[b].append(a)
 
-        def dfs(root, pre):
-            for i in g[root]:
-                if i != pre:
-                    dfs(i, root)
-                    count[root] += count[i]
-                    res[root] += res[i] + count[i]
+        def dfs(n, p):
+            for i in g[n]:
+                if i != p:
+                    dfs(i, n)
+                    count[n] += count[i]
+                    res[n] += res[i] + count[i]
         
-        def dfs2(root, pre):
-            for i in g[root]:
-                if i != pre:
-                    res[i] = res[root] - count[i] + N - count[i]
-                    dfs2(i, root)
+        def dfs2(n, p):
+            for i in g[n]:
+                if i != p:
+                    res[i] = res[n] - count[i] + N - count[i]
+                    dfs2(i, n)
         dfs(0, -1)
         dfs2(0, -1)
         print(count,res)

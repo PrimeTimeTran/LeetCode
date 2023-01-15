7741aee749e0103244f9ae673d52b488a7f0fbcd
Loi Tran <datloiboi@gmail.com>
2023-01-15 17:09:19 -0500

    Time: 1506 ms (60.62%), Space: 42.6 MB (77.15%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,14 +1,15 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        q = deque([(headID, 0)])
         g = defaultdict(list)
+        for i, u in enumerate(manager):
+            g[u].append(i)
+        
+        q = deque([(headID, 0)])
         res = 0
-        for i, v in enumerate(manager):
-            g[v].append(i)
-            
         while q:
             u, time = q.popleft()
-            res = max(res, time)
+            res = max(time, res)
             for v in g[u]:
-                q.append((v, time + informTime[u]))
+                q.append((v, time+informTime[u]))
         return res
+            

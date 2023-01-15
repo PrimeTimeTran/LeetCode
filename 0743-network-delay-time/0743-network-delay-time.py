0df9182e9cf2949d0f74d1b9aeb6050e8fdf9d3d
Loi Tran <datloiboi@gmail.com>
2023-01-15 18:10:39 -0500

    Time: 469 ms (84.94%), Space: 16.1 MB (71.14%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,7 +1,6 @@
 '''
 Create G and BFS with vertex and times. Create Q and append prev node and accumulated time thus far.
 Use set to identify nm of seen nodes and break if found. Use heapq to go shortest time first, Dikjstra
-
 '''
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
@@ -13,11 +12,11 @@ class Solution:
         seen = set()
         
         while q:
-            t, u = heapq.heappop(q)
+            t, u = heappop(q)
             if u in seen: continue
             seen.add(u)
             if len(seen) == n: return t
             for v, w in g[u]:
-                heapq.heappush(q, (w+t, v))
+                heappush(q, (w+t, v))
         
         return -1

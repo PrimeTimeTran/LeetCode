85fcb92e4eda43c7110f2a44ffed40de27abafd2
Loi Tran <datloiboi@gmail.com>
2023-01-14 19:28:06 -0500

    Time: 47 ms (78.88%), Space: 16.5 MB (76.62%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,33 +1,17 @@
-'''
-Check left node smaller and right node larger of every parent in tree.
-Preorder: Action, Left, Right
-Inorder: Left, Action, Right
-Postorder: Left, Right, Action
-'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isValidBST(self, root):
-        # PRE
-#         def valid(n, left, right):
-#             if not n: return True
-            
-#             if n.val > left and n.val < right:
-#                 return valid(n.left, left, n.val) and valid(n.right, n.val, right)
-#             return False
-                
-#         return valid(root, -inf, inf)
-
-        # POST
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
         def valid(n, left, right):
-            if not n: return True
-
-            leftValid = valid(n.left, left, n.val)
-            rightValid = valid(n.right, n.val, right)
+            if not n:
+                return True
             
-            if not leftValid or not rightValid:
-                return False
-
             if n.val > left and n.val < right:
-                return True 
+                return valid(n.left, left, n.val) and valid(n.right, n.val, right)
             return False
-        
+            
         return valid(root, -inf, inf)

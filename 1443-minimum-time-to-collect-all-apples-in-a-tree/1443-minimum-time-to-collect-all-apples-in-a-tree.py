c658220b586ade364c5a7c76874c75d0dea4cf1f
Loi Tran <datloiboi@gmail.com>
2023-01-15 18:37:19 -0500

    Time: 700 ms (52.44%), Space: 55.1 MB (17.18%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,16 +1,19 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
         g = defaultdict(list)
-        
         for a, b in edges:
             g[a].append(b); g[b].append(a)
-            
+        
         seen = set()
-        def dfs(n):
-            if n in seen: return 0
+        def dfs(n):        
+            if n in seen:
+                return 0 
             seen.add(n)
-            secs = sum(dfs(nei) for nei in g[n])
+            secs = 0
+            for nei in g[n]:
+                secs += dfs(nei)
             if secs > 0:
                 return secs + 2
             return 2 if hasApple[n] else 0
-        return max(dfs(0) - 2, 0)
+        
+        return max(dfs(0)-2,0)

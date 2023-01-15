42699280585b73070ebbb120cf24ea8da77d14d7
Loi Tran <datloiboi@gmail.com>
2023-01-15 18:18:54 -0500

    Time: 1818 ms (87.50%), Space: 73.9 MB (42.96%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,21 +1,23 @@
+'''
+Create a G using roads. DFS G checking for min road score on each item.
+'''
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         g = defaultdict(list)
+        for a, b, d in roads:
+            g[a].append((b,d)); g[b].append((a,d))
         
-        for u,v,w in roads:
-            g[u].append((v,w))
-            g[v].append((u,w))
-            
-        seen, q = set(), deque([1])
-        res = inf
-        while q:
-            u = q.popleft()
-            if u in seen: continue
-            seen.add(u)
-            for v, w in g[u]:
-                if v not in seen:
-                    q.append(v)
-                res = min(res, w)
-                
-        return res
-            
+        seen = set()
+        self.res = inf    
+        def dfs(n):
+            if n in seen:
+                return
+            seen.add(n)
+            for nei,d in g[n]:
+                if nei not in seen:
+                    dfs(nei)
+                self.res = min(self.res, d)
+        
+        dfs(1)
+        return self.res

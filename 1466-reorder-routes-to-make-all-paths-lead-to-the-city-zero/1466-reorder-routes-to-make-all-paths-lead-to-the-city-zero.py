f4bcc7d12b25201958400d15d06bb9e9941abdb6
Loi Tran <datloiboi@gmail.com>
2023-01-16 03:31:19 -0500

    Time: 1366 ms (62.14%), Space: 71.7 MB (43.71%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,22 +1,26 @@
+'''
+DFS
+Create optimistic G usng connections. Create set, roads, of actual roads using connections.
+DFS from 0 node checking if neighbor/city has road to n in roads. If not, increment res. 
+Add city to seen to prevent loops and dfs it. Guard cycle by continuing on seen cities.
+'''
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         g = defaultdict(list)
+        
         for a,b in connections:
-            g[a].append(b)
-            g[b].append(a)
+            g[a].append(b); g[b].append(a)
         
         self.res = 0
-        roads = {(a,b) for a,b in connections}
         seen = set([0])
+        roads = {(a,b) for a,b in connections}
+        
         def dfs(n):
             for c in g[n]:
-                if c in seen:
-                    continue
+                if c in seen: continue
                 if (c,n) not in roads:
-                    self.res+=1
+                    self.res +=1
                 seen.add(c)
                 dfs(c)
-                
-        
-        dfs(0)
+        dfs(0)        
         return self.res

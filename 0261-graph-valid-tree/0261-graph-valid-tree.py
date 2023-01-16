479361ebc678a126dfd60fdb18abea70ffc2440a
Loi Tran <datloiboi@gmail.com>
2023-01-16 00:15:55 -0500

    Time: 91 ms (94.38%), Space: 15.4 MB (72.78%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -3,12 +3,15 @@ DFS
 DFS from 0 node passing in previous node in each recursive call.
 Guard cycles with a set and continue of nei == p. If DFS isn't successful, return false.
 Return true if the loop completes with no return.
+
+UF
+
 '''
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if n - 1 != len(edges):
             return False
-        parent = {i:i for i in range(n)}
+        parent = [i for i in range(n)]
         
         def find(x):
             if parent[x] != x:

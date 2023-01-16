3c6245c3ba8dfa6ed5967c587c7b75622ca5213f
Loi Tran <datloiboi@gmail.com>
2023-01-16 13:08:10 -0500

    Time: 111 ms (84.46%), Space: 15.5 MB (26.07%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -6,21 +6,21 @@ If the number of stops in the hashmap is greater than stops, continue; skipping 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         g = defaultdict(list)
-        
         for u,v,w in flights:
             g[u].append((v,w))
-        
+            
         pq = [[0,src,k+1]]
         seen = {}
+        
         while pq:
-            p,u,stops = heappop(pq)
-            if u == dst: return p
+            c, u, stops = heappop(pq)
+            if u == dst: return c
             if u in seen and seen[u] >= stops: continue
-            seen[u] = stops-1
+            seen[u] = stops - 1
             if stops:
                 for v, w in g[u]:
-                    heappush(pq, (w+p, v, stops-1))
+                    heappush(pq, [c+w, v, stops-1])
         return -1
             
-            
-            
+             
+        

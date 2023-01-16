0460ca0675422fa3128a6d38f3f7515cdb40ebc3
Loi Tran <datloiboi@gmail.com>
2023-01-15 21:17:33 -0500

    Time: 330 ms (71.42%), Space: 25.7 MB (8.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,24 @@
+'''
+DFS 
+DFS cells finding 1s. Mark adj 1 cells as seen and increment count.
+'''
 class Solution:
     def numIslands(self, g: List[List[str]]) -> int:
         m,n = len(g), len(g[0])
-        
         seen = set()
         def dfs(r,c):
             o = r < 0 or c < 0 or r == m or c == n
-            if o:
-                return False
-            if (r,c) in seen or g[r][c] == "0":
-                return False 
+            if o or (r,c) in seen or g[r][c] == "0":
+                return
             seen.add((r,c))
-            
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
-            return True
-            
         res = 0
         for r in range(m):
             for c in range(n):
-                if dfs(r,c):
+                if g[r][c] == "1" and (r,c) not in seen:
+                    dfs(r,c)
                     res+=1
         return res

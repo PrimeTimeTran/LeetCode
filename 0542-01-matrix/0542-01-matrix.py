8b3d4e006993081acb66848dce7c93bd973af7e4
Loi Tran <datloiboi@gmail.com>
2023-01-16 14:08:35 -0500

    Time: 740 ms (66.55%), Space: 16.8 MB (88.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,22 +1,26 @@
+'''
+BFS
+Iterate cells adding 0 cells to Q and changing others to -1. BFS Q ignorng out of bounds, processed cells. 
+
+'''
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
         DIR = [0, 1, 0, -1, 0]
-
         q = deque([])
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 0:
-                    q.append((r, c))
+                    q.append((r,c))
                 else:
-                    mat[r][c] = -1  # Marked as not processed yet!
-
+                    mat[r][c] = -1
+        seen = set()        
         while q:
             r, c = q.popleft()
             for i in range(4):
-                nr, nc = r + DIR[i], c + DIR[i + 1]
-                out = nr < 0 or nr == m or nc < 0 or nc == n
+                nr, nc = r+DIR[i], c+DIR[i+1]
+                out = nr < 0 or nc < 0 or nr == m or nc == n
                 if out or mat[nr][nc] != -1: continue
-                mat[nr][nc] = mat[r][c] + 1
-                q.append((nr, nc))
+                mat[nr][nc] = 1 + mat[r][c]
+                q.append((nr,nc))
         return mat

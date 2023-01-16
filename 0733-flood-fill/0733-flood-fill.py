922fd171d7ac26276535d66c99fa394da04f4838
Loi Tran <datloiboi@gmail.com>
2023-01-16 01:20:10 -0500

    Time: 74 ms (92.59%), Space: 14.1 MB (36.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -14,8 +14,8 @@ class Solution:
             r, c = q.popleft()
             for i in range(4):
                 nr,nc = r+d[i], c+d[i+1]
-                out = nr < 0 or nc < 0 or nr == m or nc == n or (nr,nc) in seen
-                if out: continue
+                out = nr < 0 or nc < 0 or nr == m or nc == n
+                if out or (nr,nc) in seen: continue
                 seen.add((nr,nc))
                 if image[nr][nc] == old:
                     image[nr][nc] = color

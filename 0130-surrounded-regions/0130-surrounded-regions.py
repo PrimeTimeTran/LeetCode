8d40d0719fc8e3babefca9035900e0a436ecd446
Loi Tran <datloiboi@gmail.com>
2023-01-15 22:06:06 -0500

    Time: 137 ms (91.24%), Space: 16 MB (38.54%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,10 +1,20 @@
+'''
+Iterate mat DFSing on border O cells. In DFS change O cell and neighbor O cells to T
+Iterate mat and turn all O cell to X.
+Iterate mat and turn all T cell to O.
+'''
+
 class Solution:
     def solve(self, b: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         m,n = len(b), len(b[0])
+        
         seen = set()
         def dfs(r,c):
-            out = r < 0 or c < 0 or r == m or c == n or (r,c) in seen 
-            if out or b[r][c] == 'X':
+            o = r < 0 or c < 0 or r == m or c == n or (r,c) in seen
+            if o or b[r][c] == 'X':
                 return
             seen.add((r,c))
             b[r][c] = 'T'
@@ -12,12 +22,12 @@ class Solution:
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
-            
+        
         for r in range(m):
             for c in range(n):
-                if b[r][c] == 'O' and (r in [0, m-1] or c in [0, n - 1]):
+                if b[r][c] == 'O' and (r in [0, m-1] or c in [0, n-1]):
                     dfs(r,c)
-                    
+        
         for r in range(m):
             for c in range(n):
                 if b[r][c] == 'O':
@@ -27,3 +37,4 @@ class Solution:
             for c in range(n):
                 if b[r][c] == 'T':
                     b[r][c] = 'O'
+                    

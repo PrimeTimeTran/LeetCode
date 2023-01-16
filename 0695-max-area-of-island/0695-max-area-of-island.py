fbe92b5ebd133623c9353bb7d04e29d19d0b26e4
Loi Tran <datloiboi@gmail.com>
2023-01-15 21:20:57 -0500

    Time: 147 ms (73.60%), Space: 17.9 MB (9.75%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,20 +1,16 @@
 class Solution:
     def maxAreaOfIsland(self, g: List[List[int]]) -> int:
         m,n = len(g), len(g[0])
-        
         seen = set()
         def dfs(r,c):
-            out = r < 0 or c < 0 or r == m or c == n
-            if out: return 0
-            if (r,c) in seen or g[r][c] == 0:
+            o = r < 0 or c < 0 or r == m or c == n
+            if o or (r,c) in seen or g[r][c] == 0:
                 return 0
-            
             seen.add((r,c))
             return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
-        
-        self.res = 0
+        res = 0
         for r in range(m):
             for c in range(n):
-                if g[r][c]:
-                    self.res = max(self.res, dfs(r,c))
-        return self.res
+                if g[r][c] and (r,c) not in seen:
+                    res = max(res, dfs(r,c))
+        return res

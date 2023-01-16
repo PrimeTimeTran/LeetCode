b8d9f5cb46abeaed01e84f0105aa185efd20efa2
Loi Tran <datloiboi@gmail.com>
2023-01-16 18:32:00 -0500

    Time: 659 ms (80.01%), Space: 17.2 MB (57.38%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -23,8 +23,7 @@ class Solution:
             for i in range(4):
                 nr,nc = r+d[i],c+d[i+1]
                 o = nr < 0 or nc < 0 or nr == m or nc == n or mat[nr][nc] != -1
-                if o or (nr,nc) in seen: continue
-                # seen.add((nr,nc))
+                if o: continue
                 mat[nr][nc] = mat[r][c]+1
                 q.append((nr,nc))
         return mat

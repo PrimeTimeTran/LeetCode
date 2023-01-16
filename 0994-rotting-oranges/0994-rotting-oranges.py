0d94de0ccc66eed4e7db0402f5a488016a2c5cf9
Loi Tran <datloiboi@gmail.com>
2023-01-16 14:22:21 -0500

    Time: 48 ms (94.63%), Space: 13.9 MB (90.15%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,28 +1,27 @@
 class Solution:
     def orangesRotting(self, g: List[List[int]]) -> int:
-        m,n = len(g), len(g[0]) 
-        f, q = 0, deque()
-        
+        m,n = len(g), len(g[0])
+        q = deque([])
+        fresh = 0
         for r in range(m):
             for c in range(n):
-                if g[r][c] == 1:
-                    f+=1
                 if g[r][c] == 2:
-                    q.append([r,c])
-        
-        time = 0
-        while q and f > 0:
-            d = [0,1,0,-1,0]
+                    q.append((r,c))
+                elif g[r][c] == 1:
+                    fresh+=1
+                    
+        res = 0
+        d = [0,-1,0,1,0]
+        while q and fresh > 0:
             for _ in range(len(q)):
                 r, c = q.popleft()
                 for i in range(4):
                     nr,nc = r+d[i],c+d[i+1]
-                    out = nr < 0 or nc < 0 or nr == m or nc == n
-                    if out or g[nr][nc] != 1:
+                    o = nr < 0 or nc < 0 or nr == m or nc == n
+                    if o or g[nr][nc] != 1:
                         continue
                     g[nr][nc] = 2
-                    q.append([nr,nc])
-                    f-=1
-            time+=1
-        
-        return time if f == 0 else -1
+                    q.append((nr,nc))
+                    fresh -= 1
+            res+=1
+        return res if fresh == 0 else -1

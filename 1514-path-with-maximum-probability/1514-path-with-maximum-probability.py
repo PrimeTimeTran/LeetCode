26f82976d174becf51599b243600db63ed41f62e
Loi Tran <datloiboi@gmail.com>
2023-01-16 01:09:47 -0500

    Time: 653 ms (98.97%), Space: 27.4 MB (22.19%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,17 +1,22 @@
+'''
+Create G and BFS with PQ on maximized probability and node.
+Begin with -1 to find max probability as we add to PQ with multiplication. 
+Guard cycles using set.
+'''
+
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        g= defaultdict(list)
-        for i, (u, v) in enumerate(edges):
-            g[u].append((v,succProb[i]))
-            g[v].append((u,succProb[i]))
-
-        pq = [(-1, start)]
+        g = defaultdict(list)
+        for i, (u,v) in zip(succProb, edges):
+            g[u].append((v,i)); g[v].append((u,i))
+            
+        pq = [[-1,start]]
         seen = set()
-        while pq: 
+        while pq:
             p, u = heappop(pq)
             if u == end: return -p
             seen.add(u)
             for v, w in g[u]:
-                if v in seen: continue 
-                heappush(pq, (p * w, v))
+                if v in seen: continue
+                heappush(pq, ((p*w), v))    
         return 0

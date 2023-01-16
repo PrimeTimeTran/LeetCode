4eae627467e6fc71419ae6814e698f8406a7bd9c
Loi Tran <datloiboi@gmail.com>
2023-01-16 00:51:06 -0500

    Time: 1498 ms (61.02%), Space: 51.8 MB (27.76%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,4 +1,7 @@
 '''
+DFS
+Create G and DFS from headID. Return DFS call to headID and then return local max in recursive DFS
+
 BFS 
 Create G and then BFS with accumulated time and manager node. Use manager node + time on each append to calculate
 how long it takes to tell this employee. Update global max with each item.
@@ -6,8 +9,8 @@ how long it takes to tell this employee. Update global max with each item.
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         g = defaultdict(list)
-        for i, m in enumerate(manager):
-            g[m].append(i)
+        for v, u in enumerate(manager):
+            g[u].append(v)
 
         def dfs(u):
             return max([dfs(v) for v in g[u]] or [0]) + informTime[u]

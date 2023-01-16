de8e53e191b7adce5b193f997d57f8d1ad13fde5
Loi Tran <datloiboi@gmail.com>
2023-01-16 18:31:46 -0500

    Time: 737 ms (66.23%), Space: 17.6 MB (29.97%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,26 +1,30 @@
 '''
 BFS
-Iterate cells adding 0 cells to Q and changing others to -1. BFS Q ignorng out of bounds, processed cells. 
+Iterate cells adding 0 cells to Q and changing others to -1. BFS Q ignorng out of bounds and cells which aren't -1.
+For each cell item, increment cell by 1 + current cell value befoore appending new coords.
 
 '''
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        DIR = [0, 1, 0, -1, 0]
+        m,n = len(mat), len(mat[0])
         q = deque([])
+        
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 0:
                     q.append((r,c))
                 else:
                     mat[r][c] = -1
-        seen = set()        
+        
+        d = [0,-1,0,1,0]
+        seen = set()
         while q:
             r, c = q.popleft()
             for i in range(4):
-                nr, nc = r+DIR[i], c+DIR[i+1]
-                out = nr < 0 or nc < 0 or nr == m or nc == n
-                if out or mat[nr][nc] != -1: continue
-                mat[nr][nc] = 1 + mat[r][c]
+                nr,nc = r+d[i],c+d[i+1]
+                o = nr < 0 or nc < 0 or nr == m or nc == n or mat[nr][nc] != -1
+                if o or (nr,nc) in seen: continue
+                seen.add((nr,nc))
+                mat[nr][nc] = mat[r][c]+1
                 q.append((nr,nc))
         return mat

a0b412ec8894aa29c133ecd557340288e2c352c3
Loi Tran <datloiboi@gmail.com>
2023-01-16 02:44:01 -0500

    Time: 108 ms (88.73%), Space: 15.5 MB (18.73%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,32 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        prices = [inf]*n
-        prices[src] = 0
-        for i in range(k+1):
-            tmp = prices.copy()
-            
-            for u,v,w in flights:
-                if prices[u] == inf:
-                    continue
-                if prices[u]+w < tmp[v]:
-                    tmp[v] = prices[u]+w
-            prices = tmp
-        return -1 if prices[dst] == inf else prices[dst]
-    
-#         g = defaultdict(list)
+        g = defaultdict(list)
         
-#         for u,v,w in flights:
-#             g[u].append((v,w))
-            
-#         q = [[0, src, k+1]]
-#         seen = {}
+        for u,v,w in flights:
+            g[u].append((v,w))
         
-#         while q:
-#             cost,u,stops = heapq.heappop(q)
-#             if u == dst: return cost
-#             if u in seen and seen[u] >= stops : continue
-#             seen[u] = stops - 1
-#             if stops: 
-#                 for v, w in g[u]:
-#                     heapq.heappush(q, (cost+w, v, stops-1))
-#         return -1
+        pq = [[0,src,k+1]]
+        seen = {}
+        while pq:
+            p,u,stops = heappop(pq)
+            
+            if u == dst: return p
+            if u in seen and seen[u] >= stops: continue
+            seen[u] = stops-1
+            if stops:
+                for v, w in g[u]:
+                    heappush(pq, (w+p, v, stops-1))
+        return -1
+            
+            
+            

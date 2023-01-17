38d27582f96a9af703e4cae52b0ebd23e876775d
Loi Tran <datloiboi@gmail.com>
2023-01-16 20:07:12 -0500

    Time: 517 ms (47.87%), Space: 16.7 MB (92.27%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -9,18 +9,20 @@ class Solution:
             q = deque([])
             for i in range(4):
                 nr,nc = r+d[i],c+d[i+1]
-                q.append((nr,nc,1))
+                q.append((nr,nc, 1))
             seen = set()
-            while q:
-                r,c,v = q.popleft()
-                o = r < 0 or c < 0 or r == m or c == n
-                if o or (r,c) in seen or g[r][c] in [-1,0]:
+            while q: 
+                r,c, v = q.popleft()
+                o = r < 0 or c < 0 or r == m or c == n 
+                if o or g[r][c] in [-1,0] or (r,c) in seen:
                     continue
                 seen.add((r,c))
                 g[r][c] = min(g[r][c], v)
                 for i in range(4):
                     nr,nc = r+d[i],c+d[i+1]
-                    q.append((nr,nc,1+v))
+                    q.append((nr,nc, v+1))
+                    
+                
         for r in range(m):
             for c in range(n):
                 if g[r][c] == 0:

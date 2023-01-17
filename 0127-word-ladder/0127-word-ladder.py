d6ea4b484e5f14bea18e8e9f01e078cd587d708e
Loi Tran <datloiboi@gmail.com>
2023-01-17 14:24:38 -0500

    Time: 132 ms (91.49%), Space: 17.5 MB (47.27%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,34 +1,25 @@
-'''
-Create wildcard keys with matching potential values as adj list.
-Use BFS to with nested for loop to work though layer, count number of layers.
-
-'''
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         g = defaultdict(list)
         
         for w in wordList:
             for i in range(len(w)):
-                pat = w[:i] + '*' + w[i+1:]
+                pat = w[:i]+'*'+w[i+1:]
                 g[pat].append(w)
         
-        t = 1
         q = deque([beginWord])
-        seen = set([beginWord])
+        steps = 1
+        seen = set()
         while q:
             for _ in range(len(q)):
                 w = q.popleft()
-                if w == endWord: 
-                    return t
+                if w == endWord:
+                    return steps
                 for i in range(len(w)):
-                    pat = w[:i] + '*' + w[i+1:]
+                    pat = w[:i]+'*'+w[i+1:]
                     for nei in g[pat]:
                         if nei not in seen:
                             seen.add(nei)
                             q.append(nei)
-                
-            t+=1
-        
-        
-        return 0 
+            steps+=1
+        return 0

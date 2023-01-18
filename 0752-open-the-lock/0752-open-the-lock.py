18f3dfec689ad58a4edb46f0ee591f3018e9ff5e
Loi Tran <datloiboi@gmail.com>
2023-01-17 19:36:45 -0500

    Time: 614 ms (86.30%), Space: 15.2 MB (81.85%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -10,21 +10,22 @@ class Solution:
         def neighbors(code):
             for i in range(4):
                 x = int(code[i])
-                for dir in [-1,1]:
-                    y = (dir+x+10) % 10
-                    yield code[:i]+ str(y)+code[i+1:]
-        deadSet = set(deadends)
-        if "0000" in deadSet: return -1
-        q = deque(["0000"])
-        steps = 0
+                for diff in [-1,1]:
+                    y = (diff+x+10) % 10
+                    yield code[:i]+str(y)+code[i+1:]
+        dead = set(deadends)
+        if "0000" in dead: return -1
+        
+        q = deque(['0000'])
+
+        moves = 0
         while q:
             for _ in range(len(q)):
                 code = q.popleft()
-                if code == target: return steps
+                if code == target: return moves
                 for nei in neighbors(code):
-                    if nei not in deadSet:
-                        deadSet.add(nei)
+                    if nei not in dead:
+                        dead.add(nei)
                         q.append(nei)
-            steps+=1
+            moves+=1                
         return -1
-    

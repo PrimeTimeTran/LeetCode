dad72c3a4489f89de6d920d435b20a4dbb322635
Loi Tran <datloiboi@gmail.com>
2023-01-18 15:36:56 -0500

    Time: 50 ms (90.19%), Space: 14.9 MB (27.99%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -3,9 +3,8 @@ BFS
 BFS with G of wildcard keys and word values.
 Guard for cycles using HM which has word key and layer distance. Add word if unseen or greater than current word.
 
-DFS from endWord to beginWord using parent HM adding all words in the path. When beginword
+DFS from endWord to beginWord with parent HM adding all words in the path. When beginword
 found return the reversed path.
-
 '''
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
@@ -14,30 +13,32 @@ class Solution:
             for j in range(len(w)):
                 pat = w[:j] + "*" + w[j+1:]
                 g[pat].append(w)
-
+        
+        seen = { beginWord: 1 }
         q = deque([beginWord])
-        seen = {beginWord: 1}
         parent = defaultdict(set)
-
+        
         while q:
-            word = q.popleft()
-            if word == endWord:
-                break
-            for i in range(len(word)):
-                pat = word[:i] + "*" + word[i+1:]
-                for nei in g[pat]:
-                    if nei not in seen:
-                        seen[nei] = seen[word]+1
-                        parent[nei].add(word)
-                        q.append(nei)
-                    elif seen[nei] > seen[word]:
-                        parent[nei].add(word)
-        ans = []
+            for _ in range(len(q)):
+                w = q.popleft()    
+                if w == endWord: break
+                for j in range(len(w)):
+                    pat = w[:j] + "*" + w[j+1:]
+                    for nei in g[pat]:
+                        if nei not in seen:
+                            seen[nei] = seen[w]+1
+                            parent[nei].add(w)
+                            q.append(nei)
+                        elif seen[nei] > seen[w]:
+                            parent[nei].add(w)
+        res = []
         def dfs(w, p):
             if w == beginWord:
-                ans.append(p[::-1])
+                res.append(p[::-1])
             else:
                 for nei in parent[w]:
                     dfs(nei, p+[nei])
         dfs(endWord, [endWord])
-        return ans
+        return res
+                
+        

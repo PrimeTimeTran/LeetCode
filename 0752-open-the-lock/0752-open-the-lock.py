c60c3a4d2c54a7add8efbcc60856ffcde05d7df0
Loi Tran <datloiboi@gmail.com>
2023-01-18 14:56:49 -0500

    Time: 647 ms (81.32%), Space: 15.4 MB (52.52%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -11,20 +11,21 @@ class Solution:
             for i in range(4):
                 x = int(code[i])
                 for diff in [-1,1]:
-                    y = (diff+x+10) % 10
+                    y = (x+diff+10) % 10
                     yield code[:i]+str(y)+code[i+1:]
+        
         dead = set(deadends)
-        if "0000" in dead: return -1
-        q = deque(['0000'])
-
-        moves = 0
+        if '0000' in dead: return -1
+        steps, q = 0, deque(['0000'])
+        
         while q:
             for _ in range(len(q)):
                 code = q.popleft()
-                if code == target: return moves
+                if code == target: return steps
                 for nei in neighbors(code):
                     if nei not in dead:
                         dead.add(nei)
                         q.append(nei)
-            moves+=1                
+            steps+=1
         return -1
+        

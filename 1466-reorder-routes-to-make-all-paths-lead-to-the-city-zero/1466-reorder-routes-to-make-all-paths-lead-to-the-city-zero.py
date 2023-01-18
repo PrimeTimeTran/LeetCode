2cc0e3b5229148473aa84bf72f759d3d90d4a9c6
Loi Tran <datloiboi@gmail.com>
2023-01-18 15:47:00 -0500

    Time: 1301 ms (70.73%), Space: 71.7 MB (42.94%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -7,19 +7,19 @@ Add city to seen to prevent loops and dfs it. Guard cycle by continuing on seen 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         g = defaultdict(list)
-        for a,b in connections:
-            g[a].append(b)
-            g[b].append(a)
-            
-        roads = {(a,b) for a,b in connections}
+        for a, b in connections:
+            g[a].append(b);g[b].append(a)
+        
+        roads = {(a,b) for a, b in connections}
         self.res = 0
         seen = set([0])
         def dfs(n):
             for nei in g[n]:
                 if nei in seen: continue
-                if (nei,n) not in roads:
-                    self.res += 1
+                if (nei, n) not in roads:
+                    self.res+=1
                 seen.add(nei)
                 dfs(nei)
+        
         dfs(0)
         return self.res

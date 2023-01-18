3c10ad1882cb877a863e02a380eea2d7d291c64e
Loi Tran <datloiboi@gmail.com>
2023-01-18 15:26:13 -0500

    Time: 51 ms (79.45%), Space: 14.5 MB (61.93%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,17 +1,14 @@
 class Solution:
     def racecar(self, target: int) -> int:
         # pos, speed, moves
-        q = deque([[0, 1, 0]])
+        q = deque([[0,1,0]])
         
         
-        while q: 
-            m, s, p  = q.popleft()
-            
+        while q:
+            p, s, m = q.popleft()
             if p == target: return m
             
-            q.append([m+1, s*2, p+s])
-            
-            passed = p+s > target and s > 0 or p+s < target and s < 0
-            if passed:
+            q.append([p+s,s*2,m+1])
+            if p+s > target and s > 0 or p+s < target and s < 0:
                 s = -1 if s > 0 else 1
-                q.append([m+1, s, p])
+                q.append([p,s,m+1])

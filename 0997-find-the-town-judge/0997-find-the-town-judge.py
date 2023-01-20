227afb2b562a1d60d7b0fba927bec480b91f3906
Loi Tran <datloiboi@gmail.com>
2023-01-20 01:17:44 -0500

    Time: 755 ms (82.40%), Space: 18.8 MB (91.01%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,14 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        count = [0] * (n + 1)
-        for i, j in trust:
-            count[i] -= 1
-            count[j] += 1
+        c = [0]*(n+1)
+        for a,b in trust:
+            c[a]-=1
+            c[b]+=1
             
-        print(count)
-        for i in range(1, n + 1):
-            if count[i] == n - 1:
+        for i in range(1, n+1):
+            if n-1 == c[i]:
                 return i
         return -1
-        
-        

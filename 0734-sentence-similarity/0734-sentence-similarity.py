b56bc2d0c3552c1306fdf0663bf1b20e7ee2b32b
Loi Tran <datloiboi@gmail.com>
2023-02-01 03:48:20 -0500

    Time: 48 ms (75.67%), Space: 14.1 MB (7.33%) - LeetHub

---
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def areSentencesSimilar(self, s1: List[str], s2: List[str], pairs: List[List[str]]) -> bool:
+        if len(s1) != len(s2): return False
+        
+        for w1, w2 in zip(s1,s2):
+            if w1 == w2: continue
+            val = any(w1 in pair and w2 in pair for pair in pairs)
+            if not val:
+                return False
+        return True

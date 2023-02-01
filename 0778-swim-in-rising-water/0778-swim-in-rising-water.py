a03418ce2dc03263cac592fa3fe9b1508201dadd
Loi Tran <datloiboi@gmail.com>
2023-02-01 02:05:16 -0500

    Time: 103 ms (88.77%), Space: 14.5 MB (77.18%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,31 +1,12 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-#         m, n = len(grid), len(grid[0])
-#         def valid(i, j):
-#             return 0 <= i < m and 0 <= j < n
-        
-#         def get_neis(i, j):
-#             return [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]
-        
-#         heap = []
-#         heapq.heappush(heap, (grid[0][0], 0, 0))
-#         seen = {(0, 0): grid[0][0]}
-#         while len(heap):
-#             val, i, j = heapq.heappop(heap)
-#             if i == m-1 and j == n-1:
-#                 return val
-#             for x, y in get_neis(i, j):
-#                 if valid(x, y) and max(grid[x][y], val) < seen.get((x,y), float('inf')):
-#                     seen[(x,y)] = max(grid[x][y], val)
-#                     heapq.heappush(heap, (seen[(x,y)], x, y))
-                    
         N, pq, seen, res = len(grid), [(grid[0][0], 0, 0)], set([(0, 0)]), 0
         while True:
-            T, x, y = heapq.heappop(pq)
+            T, r, c = heappop(pq)
             res = max(res, T)
-            if x == y == N - 1:
+            if r == c == N - 1:
                 return res
-            for i, j in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
-                if 0 <= i < N and 0 <= j < N and (i, j) not in seen:
-                    seen.add((i, j))
-                    heapq.heappush(pq, (grid[i][j], i, j))
+            for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
+                if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in seen:
+                    seen.add((nr, nc))
+                    heappush(pq, (grid[nr][nc], nr, nc))

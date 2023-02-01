cf4fe40caf7aeddd423a4a5cb59cb662c8ccf31f
Loi Tran <datloiboi@gmail.com>
2023-02-01 03:48:29 -0500

    Time: 39 ms (98.67%), Space: 13.9 MB (45.67%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -5,6 +5,5 @@ class Solution:
         for w1, w2 in zip(s1,s2):
             if w1 == w2: continue
             val = any(w1 in pair and w2 in pair for pair in pairs)
-            if not val:
-                return False
+            if not val: return False
         return True

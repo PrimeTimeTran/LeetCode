aa85679ad9e23896dff29384e981db86777bf387
Loi Tran <datloiboi@gmail.com>
2023-02-01 03:08:51 -0500

    Time: 100 ms (92.74%), Space: 14.6 MB (51.06%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,13 +1,14 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        N, pq, seen, res = len(grid), [(grid[0][0], 0, 0)], set([(0, 0)]), 0
-        while True:
-            T, r, c = heappop(pq)
-            res = max(res, T)
-            if r == c == N - 1:
-                return res
-            for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
-                inbounds = 0 <= nr < N and 0 <= nc < N
-                if inbounds and (nr, nc) not in seen:
-                    seen.add((nr, nc))
-                    heappush(pq, (grid[nr][nc], nr, nc))
+        n = len(grid)
+        res = 0 
+        pq = [[grid[0][0], 0, 0]]
+        seen = set((0,0))
+        while pq:
+            V, r, c = heappop(pq)
+            res = max(res, V)
+            if r == c == n-1: return res
+            for nr, nc in [[r+1,c],[r,c+1], [r-1,c], [r,c-1]]:
+                if (nr,nc) not in seen and (0 <= nr < n and 0 <= nc < n):
+                    seen.add((nr,nc))
+                    heappush(pq, [grid[nr][nc], nr, nc])

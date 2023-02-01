a587e68a0dbff60dfcda34770793a8ddd8761d23
Loi Tran <datloiboi@gmail.com>
2023-02-01 01:52:12 -0500

    Time: 145 ms (49.45%), Space: 14.4 MB (89.95%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,7 +1,7 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        def is_valid(i, j):
+        def valid(i, j):
             return 0 <= i < m and 0 <= j < n
         
         def get_neis(i, j):
@@ -9,12 +9,12 @@ class Solution:
         
         heap = []
         heapq.heappush(heap, (grid[0][0], 0, 0))
-        visited = {(0, 0): grid[0][0]}
+        seen = {(0, 0): grid[0][0]}
         while len(heap):
             val, i, j = heapq.heappop(heap)
             if i == m-1 and j == n-1:
                 return val
             for x, y in get_neis(i, j):
-                if is_valid(x, y) and max(grid[x][y], val) < visited.get((x,y), float('inf')):
-                    visited[(x,y)] = max(grid[x][y], val)
-                    heapq.heappush(heap, (visited[(x,y)], x, y))
+                if valid(x, y) and max(grid[x][y], val) < seen.get((x,y), float('inf')):
+                    seen[(x,y)] = max(grid[x][y], val)
+                    heapq.heappush(heap, (seen[(x,y)], x, y))

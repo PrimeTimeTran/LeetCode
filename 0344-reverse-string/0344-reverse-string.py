83ff039208d4c7ccac313bf9995344b0ae8c39b9
Loi Tran <datloiboi@gmail.com>
2023-02-02 07:16:09 -0500

    Time: 219 ms (37.58%), Space: 18.4 MB (81.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         s.reverse()
+        
         # s[:] = s[::-1]
+        
         # def helper(l, r):
         #     if l < r:
         #         s[l], s[r] = s[r], s[l]

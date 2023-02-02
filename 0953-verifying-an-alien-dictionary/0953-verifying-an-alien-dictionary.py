1990c06fab9cc6f99f2334c848888e3507ef1499
Loi Tran <datloiboi@gmail.com>
2023-02-02 02:01:40 -0500

    Time: 35 ms (87.31%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,15 +1,32 @@
+'''
+1. Constraints
+Receive list of words and string reprensenting the alphabet
+Return a boolean
+
+2. Diagram
+
+
+
+3. Pseudocode
+Create g of key letter and value index from order
+
+
+'''
+
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        o = {c: i for i, c in enumerate(order)}
+        g = {}
+        for i, c in enumerate(order):
+            g[c] = i
         
         for i in range(len(words)-1):
-            w1,w2 = words[i], words[i+1]
+            w1, w2 = words[i], words[i+1]
             
             for j in range(len(w1)):
                 if j == len(w2):
                     return False
                 if w1[j] != w2[j]:
-                    if o[w1[j]] > o[w2[j]]:
+                    if g[w1[j]] > g[w2[j]]:
                         return False
                     break
         return True

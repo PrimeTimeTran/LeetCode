f37df6fe7327cf6c6bc66a89b3ede9acfbcf8de7
Loi Tran <datloiboi@gmail.com>
2023-02-02 02:02:10 -0500

    Time: 40 ms (63.42%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -21,12 +21,10 @@ class Solution:
         
         for i in range(len(words)-1):
             w1, w2 = words[i], words[i+1]
-            
             for j in range(len(w1)):
                 if j == len(w2):
                     return False
                 if w1[j] != w2[j]:
-                    if g[w1[j]] > g[w2[j]]:
-                        return False
+                    if g[w1[j]] > g[w2[j]]: return False
                     break
         return True

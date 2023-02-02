bc22aa4b922a775e7f0316488a78c197514f6a55
Loi Tran <datloiboi@gmail.com>
2023-02-02 07:15:53 -0500

    Time: 208 ms (59.75%), Space: 18.6 MB (11.35%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,7 +1,8 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        def helper(l, r):
-            if l < r:
-                s[l], s[r] = s[r], s[l]
-                helper(l + 1, r - 1)
-        helper(0, len(s) - 1)
+        s[:] = s[::-1]
+        # def helper(l, r):
+        #     if l < r:
+        #         s[l], s[r] = s[r], s[l]
+        #         helper(l + 1, r - 1)
+        # helper(0, len(s) - 1)

691ae46dccfbaf8d09fbf4ed0fb0934f45daf96a
Loi Tran <datloiboi@gmail.com>
2023-02-02 06:26:33 -0500

    Time: 37 ms (59.66%), Space: 13.8 MB (96.81%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,9 @@
-'''
-Count number of odd characters and subtract that count from length of the string + 1
-
-'''
-
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hash = set()
+        h = set()
         for c in s:
-            if c in hash:
-                hash.remove(c)
+            if c in h:
+                h.remove(c)
             else:
-                hash.add(c)
-        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
+                h.add(c)
+        return len(s) - len(h)+1 if len(h) > 0 else len(s)

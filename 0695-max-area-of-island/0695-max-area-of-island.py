2afb52f3b6eec53a48cfa469f40642c807a7f7b0
Loi Tran <datloiboi@gmail.com>
2023-02-03 02:15:44 -0500

    Time: 144 ms (68.80%), Space: 17.9 MB (17.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -8,12 +8,10 @@ class Solution:
                 seen.add((r,c))
                 return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
             return 0
-                
-        
+
         res = 0
         for r in range(m):
             for c in range(n):
                 if g[r][c]:
                     res = max(res, dfs(r,c))
-                    
         return res

89ac90dd238789dfe8f51186389fd7097a33b3c4
Loi Tran <datloiboi@gmail.com>
2023-02-03 02:02:38 -0500

    Time: 81 ms (63.65%), Space: 14.1 MB (62.42%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,23 +1,22 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        d = [0,1,0,-1,0]
+    def floodFill(self, image: List[List[int]], r: int, c: int, color: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
         
-        m,n = len(image), len(image[0])
+        old = image[r][c]
+        image[r][c] = color
         
-        old = image[sr][sc]
-        image[sr][sc] = color
+        d = [0,-1,0,1,0]
         
-        q = deque([[sr,sc]])
-        seen = set((sr,sc))
-        
-        while q:
+        q = deque([[r,c]])
+        seen = set()
+        while q: 
             r, c = q.popleft()
+            
             for i in range(4):
-                nr,nc = r+d[i], c+d[i+1]
+                nr, nc = d[i]+r, d[i+1]+c
                 inbounds = 0 <= nr < m and 0 <= nc < n
-                if inbounds and (nr,nc) not in seen:
+                if inbounds and (nr,nc) not in seen and image[nr][nc] == old:
                     seen.add((nr,nc))
-                    if image[nr][nc] == old:
-                        image[nr][nc] = color
-                        q.append([nr,nc])
+                    image[nr][nc] = color
+                    q.append((nr,nc))
         return image

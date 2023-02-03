02b940f6f492fc937bebf07a7dc0f1139dbc50e6
Loi Tran <datloiboi@gmail.com>
2023-02-03 02:03:33 -0500

    Time: 78 ms (76.12%), Space: 14 MB (62.42%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,17 +1,13 @@
 class Solution:
     def floodFill(self, image: List[List[int]], r: int, c: int, color: int) -> List[List[int]]:
-        m, n = len(image), len(image[0])
-        
+        seen = set()
+        d = [0,-1,0,1,0]
         old = image[r][c]
         image[r][c] = color
-        
-        d = [0,-1,0,1,0]
-        
+        m, n = len(image), len(image[0])
         q = deque([[r,c]])
-        seen = set()
         while q: 
             r, c = q.popleft()
-            
             for i in range(4):
                 nr, nc = d[i]+r, d[i+1]+c
                 inbounds = 0 <= nr < m and 0 <= nc < n

693b9b380010d11881f5ab3cadeec52477024f3f
Loi Tran <datloiboi@gmail.com>
2023-02-04 11:24:36 -0500

    Time: 1250 ms (50.00%), Space: 16.4 MB (37.50%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -3,13 +3,11 @@ class Solution:
         banned = set(banned)
         nums = [i for i in range(1, n+1) if i not in banned]
         cum = 0
-        c=0
+        count=0
         for i in nums:
             if i + cum <= k:
                 cum+=i
-                c+=1
+                count+=1
             else:
-                return c
-        nums = list(accumulate(map(int, nums)))
-        add = 1 if k in nums else 0
-        return bisect_left(nums, k) + add
+                return count
+        return count

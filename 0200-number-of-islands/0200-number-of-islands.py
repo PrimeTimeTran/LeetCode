fd2b0853fc515f5d9986742d307e97cf487a4bd4
Loi Tran <datloiboi@gmail.com>
2023-02-04 02:08:05 -0500

    Time: 338 ms (56.81%), Space: 25.7 MB (6.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
-'''
-DFS 
-DFS cells finding 1s. Mark adj 1 cells as seen and increment count.
-'''
 class Solution:
-    def numIslands(self, g: List[List[str]]) -> int:
-        m,n = len(g), len(g[0])
-        seen = set()
-        def dfs(r,c):
-            o = r < 0 or c < 0 or r == m or c == n
-            if o or (r,c) in seen or g[r][c] == "0":
-                return
-            seen.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m,n = len(grid), len(grid[0])
+        
         res = 0
+        seen = set()
+        
+        def dfs(r,c):
+            inbounds = 0 <= r < m and 0 <= c < n
+            if inbounds and grid[r][c] == "1" and (r,c) not in seen:
+                seen.add((r,c))
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c+1)
+                dfs(r,c-1)
+                return True
+            return False
+                
         for r in range(m):
             for c in range(n):
-                if g[r][c] == "1" and (r,c) not in seen:
-                    dfs(r,c)
+                if dfs(r,c):
                     res+=1
         return res
+        
+        

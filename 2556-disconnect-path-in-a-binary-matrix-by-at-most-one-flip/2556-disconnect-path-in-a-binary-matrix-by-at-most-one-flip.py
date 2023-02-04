6f0194a0df35242693eff96fe349358a1c62b638
Loi Tran <datloiboi@gmail.com>
2023-02-04 11:26:14 -0500

    Time: 885 ms (43.75%), Space: 21.7 MB (37.50%) - LeetHub

--- 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
@@ -1,20 +1,52 @@
 class Solution:
     def isPossibleToCutPath(self, grid: List[List[int]]) -> bool:
-        m = len(grid)
-        n = len(grid[0])
+        
+        M = len(grid)
+        N = len(grid[0])
+        v= set()
 
-        seen = [[False] * n for _ in range(m)]
-        diagonals = [0] * (m + n - 1)
-        q = deque()
-        q.append((0, 0))
-        while q:
-            i, j = q.popleft()
-            if grid[i][j] == 0 or seen[i][j]:
-                continue
-            seen[i][j] = True
-            diagonals[i + j] += 1
-            if i < m - 1:
-                q.append((i + 1, j))
-            if j < n - 1:
-                q.append((i, j + 1))
-        return any([x < 2 for x in diagonals[1:-1]])
+        dirs=[[0,1],[1,0],[-1,0],[0,-1]]
+
+        path = set()
+        
+        v.add((0,0))
+        path.add((0,0))
+        
+        def dfs(x,y):
+            
+            if x == M -1 and y == N -1:
+                return True
+            
+            for d in dirs:
+                nx = x + d[0]
+                ny = y + d[1]
+                
+                if 0 <= nx < M and 0 <=ny < N and (nx,ny) not in v and grid[nx][ny] == 1:
+                    v.add((nx,ny))
+                    path.add((nx,ny))
+                    if dfs(nx,ny):
+                        return True
+                    path.remove((nx,ny))
+            return False
+
+        #1 do a dfs
+        res =  dfs(0,0)
+        
+        if not res:
+            return True
+        
+        
+        path.remove((0,0))
+        
+        path.remove((M-1,N-1))
+        
+        #2 remove the path
+        for p in path:
+            grid[p[0]][p[1]] = 0
+        
+        v= set()
+        v.add((0,0))
+
+        #2 try again DFS
+        return not dfs(0,0)
+        

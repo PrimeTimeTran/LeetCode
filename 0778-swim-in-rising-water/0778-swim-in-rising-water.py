d65dccb0c7f9820eeb5012d8a86e8cbb8a5074cd
Loi Tran <datloiboi@gmail.com>
2023-02-04 08:15:20 -0500

    Time: 101 ms (90.40%), Space: 14.7 MB (25.64%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,3 +1,9 @@
+'''
+BFS
+BFS with PQ on value of cell/row/column. On each cell update res to max of current res and value.
+Flow 4 cardinal directions to unseen inbound cells and add them to PQ and seen set.
+'''
+
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
@@ -9,6 +15,6 @@ class Solution:
             res = max(res, V)
             if r == c == n-1: return res
             for nr, nc in [[r+1,c],[r,c+1], [r-1,c], [r,c-1]]:
-                if (nr,nc) not in seen and (0 <= nr < n and 0 <= nc < n):
+                if (nr,nc) not in seen and 0 <= nr < n and 0 <= nc < n:
                     seen.add((nr,nc))
                     heappush(pq, [grid[nr][nc], nr, nc])

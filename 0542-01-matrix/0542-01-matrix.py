7f14ac043c51dc2c53af0e6d3f686c787cacb54b
Loi Tran <datloiboi@gmail.com>
2023-02-04 02:17:42 -0500

    Time: 776 ms (49.91%), Space: 17.4 MB (41.92%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -7,22 +7,24 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m,n = len(mat), len(mat[0])
         q = deque([])
-        
         for r in range(m):
             for c in range(n):
                 if mat[r][c] == 0:
                     q.append((r,c))
                 else:
                     mat[r][c] = -1
+                    
         d = [0,-1,0,1,0]
+        seen = set()
         while q:
-            r, c  = q.popleft()
+            r,c = q.popleft()
+            
             for i in range(4):
-                nr, nc = r+d[i],c+d[i+1]
-                o = nr < 0 or nc < 0 or nr == m or nc == n
-                if o or mat[nr][nc] != -1:
-                    continue
-                mat[nr][nc] = mat[r][c] + 1
-                q.append((nr,nc))
+                nr,nc = r+d[i], c+d[i+1]
+                inbounds = 0<=nr<m and 0<=nc<n
+                if inbounds and (nr,nc) not in seen and mat[nr][nc] != 0:
+                    seen.add((nr,nc))            
+                    mat[nr][nc] = mat[r][c]+1
+                    q.append((nr,nc))
+                    
         return mat
-                

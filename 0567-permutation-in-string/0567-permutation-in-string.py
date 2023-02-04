d579a6c7c32ce8b5632a811c6c496fc54f5d575c
Loi Tran <datloiboi@gmail.com>
2023-02-04 00:59:29 -0500

    Time: 77 ms (72.81%), Space: 13.9 MB (64.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,14 +1,14 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        cntr, w = Counter(s1), len(s1)   
-
-        for i in range(len(s2)):
-            if s2[i] in cntr: 
-                cntr[s2[i]] -= 1
-            if i >= w and s2[i-w] in cntr: 
-                cntr[s2[i-w]] += 1
-
-            if all([cntr[i] == 0 for i in cntr]): # see optimized code below
+    def checkInclusion(self, p: str, s: str) -> bool:
+        cnt = Counter(p)
+        
+        l = 0
+        for r, c in enumerate(s):
+            cnt[c] -= 1
+            while cnt[c] < 0:  # If number of characters `c` is more than our expectation
+                cnt[s[l]] += 1  # Slide left until cnt[c] == 0
+                l += 1
+            if r - l + 1 == len(p):  # If we already filled enough `p.length()` chars
                 return True
-
+            
         return False

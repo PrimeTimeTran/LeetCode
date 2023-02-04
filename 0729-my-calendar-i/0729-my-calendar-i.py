4edfeb713af7477581ea36249accd644846bfd93
Loi Tran <datloiboi@gmail.com>
2023-02-04 05:23:16 -0500

    Time: 223 ms (92.10%), Space: 14.6 MB (92.41%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,14 +1,32 @@
-from sortedcontainers import SortedList
-
 class MyCalendar:
     def __init__(self):
-        self.arr = SortedList()
-        
-    def book(self, start, end):
-        q1 = SortedList.bisect_right(self.arr, start)
-        q2 = SortedList.bisect_left(self.arr, end)
-        if q1 == q2 and q1 % 2 == 0:
-            self.arr.add(start)
-            self.arr.add(end)
+        self.calendar = []
+
+    def book(self, start: int, end: int) -> bool:
+        right = len(self.calendar)
+        if right == 0:
+            self.calendar.append((start, end))
             return True
-        return False
+
+        left = 0
+        while left < right:
+            mid = (left+right) // 2
+            if self.calendar[mid][1] <= start:
+                left = mid + 1
+            else:
+                right = mid
+
+        if left == len(self.calendar):
+            self.calendar.append((start, end))
+            return True
+                
+        if self.calendar[left][0] >= end:
+            self.calendar.insert(left, (start, end))
+            return True
+            
+        return False        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)

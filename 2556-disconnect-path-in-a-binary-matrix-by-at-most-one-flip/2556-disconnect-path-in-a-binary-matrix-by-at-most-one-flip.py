020b1e0e3a3ac20d4fc464ade0d766e89b1e8339
Loi Tran <datloiboi@gmail.com>
2023-02-04 11:27:04 -0500

    Time: 875 ms (50.00%), Space: 21.4 MB (37.50%) - LeetHub

--- 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
@@ -1,22 +1,13 @@
 class Solution:
     def isPossibleToCutPath(self, grid: List[List[int]]) -> bool:
-        
-        M = len(grid)
-        N = len(grid[0])
+        M, N = len(grid), len(grid[0])
         v= set()
-
         dirs=[[0,1],[1,0],[-1,0],[0,-1]]
-
         path = set()
-        
         v.add((0,0))
         path.add((0,0))
-        
         def dfs(x,y):
-            
-            if x == M -1 and y == N -1:
-                return True
-            
+            if x == M -1 and y == N -1: return True
             for d in dirs:
                 nx = x + d[0]
                 ny = y + d[1]
@@ -28,19 +19,10 @@ class Solution:
                         return True
                     path.remove((nx,ny))
             return False
-
-        #1 do a dfs
         res =  dfs(0,0)
-        
-        if not res:
-            return True
-        
-        
+        if not res: return True
         path.remove((0,0))
-        
         path.remove((M-1,N-1))
-        
-        #2 remove the path
         for p in path:
             grid[p[0]][p[1]] = 0
         

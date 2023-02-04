3e9b3a2c2bf46f3aae9a7cd21f42aa3ef3792dcd
Loi Tran <datloiboi@gmail.com>
2023-02-04 02:26:23 -0500

    Time: 56 ms (64.66%), Space: 13.9 MB (88.28%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,27 +6,29 @@ If at the end fresh is 0, return time, else -1
 
 class Solution:
     def orangesRotting(self, g: List[List[int]]) -> int:
-        f,q = 0, deque([])
         m,n = len(g), len(g[0])
+        f,t = 0, 0
+        q = deque([])
         for r in range(m):
             for c in range(n):
                 if g[r][c] == 2:
                     q.append((r,c))
                 elif g[r][c] == 1:
                     f+=1
-        res = 0 
-        d = [0,-1,0,1,0]
+        d = [0,-1,0,1,0]    
+        seen = set()
         while q and f > 0:
             for _ in range(len(q)):
                 r,c = q.popleft()
+
                 for i in range(4):
-                    nr,nc = r+d[i],c+d[i+1]
-                    o = nr < 0 or nc < 0 or nr == m or nc == n
-                    if o or g[nr][nc] != 1:
-                        continue
-                    g[nr][nc] = 2
-                    q.append((nr,nc))
-                    f -= 1
-            res+=1
-        print(f)
-        return res if f == 0 else-1
+                    nr,nc = r+d[i], c+d[i+1]
+                    inbounds = 0<= nr<m and 0<= nc<n
+                    if inbounds and (nr,nc) not in seen and g[nr][nc] == 1:
+                        f-=1
+                        seen.add((nr,nc))
+                        g[nr][nc] = 2
+                        q.append((nr,nc))
+            
+            t+=1
+        return t if f == 0 else -1

e72fe1becaa09b51cd836bd8f459b17c0a768e32
Loi Tran <datloiboi@gmail.com>
2023-02-05 14:01:39 -0500

    Time: 37 ms (88.76%), Space: 13.8 MB (94.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,4 +1,5 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        return x if (x:=isqrt(x2:=n*(n+1)//2))**2 == x2 else -1 
-      
+        # return x if (x:=isqrt(x2:=n*(n+1)//2))**2 == x2 else -1 
+        x = sqrt(n * (n + 1) // 2)
+        return -1 if x % 1 else int(x)

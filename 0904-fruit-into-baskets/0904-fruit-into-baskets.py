bee189d59c986db958afec5e6b40a0b48c461042
Loi Tran <datloiboi@gmail.com>
2023-02-07 05:40:25 -0500

    Time: 875 ms (86.98%), Space: 20.1 MB (51.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,10 +1,10 @@
 class Solution:
-    def totalFruit(self, tree: List[int]) -> int:
-        count, L = {}, 0
-        for R, v in enumerate(tree):
+    def totalFruit(self, A: List[int]) -> int:
+        count, l = {}, 0
+        for r, v in enumerate(A):
             count[v] = count.get(v, 0) + 1
             if len(count) > 2:
-                count[tree[L]] -= 1
-                if count[tree[L]] == 0: del count[tree[L]]
-                L += 1
-        return R - L + 1
+                count[A[l]] -= 1
+                if count[A[l]] == 0: del count[A[l]]
+                l += 1
+        return r - l + 1

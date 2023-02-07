e714f1e915b79a8c69344aac98743cfca9c20429
Loi Tran <datloiboi@gmail.com>
2023-02-07 17:15:59 -0500

    Time: 109 ms (87.46%), Space: 14.3 MB (28.97%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,22 @@
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], N: int) -> int:
+        # uf = {x: x for x in range(N)}
+        uf = [i for i in range(N)]
+        self.groups = N
+
+        def merge(x, y):
+            x, y = find(x), find(y)
+            if x != y:
+                self.groups -= 1
+                uf[x] = y
+
+        def find(x):
+            if uf[x] != x:
+                uf[x] = find(uf[x])
+            return uf[x]
+
+        for t, x, y in sorted(logs):
+            merge(x, y)
+            if self.groups == 1:
+                return t
+        return -1

5e0181d74ab8b9f359f73b1b0417bc4fed4749af
Loi Tran <datloiboi@gmail.com>
2023-02-08 08:09:45 -0500

    Time: 98 ms (66.05%), Space: 14.9 MB (86.32%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.py
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -1,4 +1,10 @@
 class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
+        nums.sort()
+        for i in range(2, len(nums), 2):
+            nums[i], nums[i-1] = nums[i-1], nums[i]
+        
+        return nums
+
         for i in range(len(nums)):
             nums[i:i+2] = sorted(nums[i:i+2], reverse=i%2)

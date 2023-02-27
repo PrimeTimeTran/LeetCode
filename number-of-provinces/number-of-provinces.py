40227527a82b5ea9c4fc1fd942d3ae37b295b9f5
Loi Tran <datloiboi@gmail.com>
2023-02-27 10:29:11 -0500

    Time: 186 ms, Memory: 15 MB - LeetHub

---
+++ number-of-provinces/number-of-provinces.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCircleNum(self, A: List[List[int]]) -> int:
+        n = len(A)
+        seen = set()
+        res = 0
+        
+        def dfs(i):
+            for j, connected in enumerate(A[i]):
+                if connected and j not in seen:
+                    seen.add(j)
+                    dfs(j)
+        
+        for i in range(n):
+            if i not in seen:
+                dfs(i)
+                res+=1
+        return res

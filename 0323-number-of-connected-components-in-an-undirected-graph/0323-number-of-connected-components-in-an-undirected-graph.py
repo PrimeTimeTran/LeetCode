c3791affea55f13ce57fcab618f4c6bc1edeb331
Loi Tran <datloiboi@gmail.com>
2023-02-27 10:01:01 -0500

    Time: 107 ms (56.17%), Space: 16.4 MB (44.57%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -21,20 +21,18 @@ return res
 '''
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        g = defaultdict(list)
-        for a,b in edges:
-            g[a].append(b)
-            g[b].append(a)
-        seen = set()
-        def dfs(n):
-            for nei in g[n]:
-                if nei not in seen:
-                    seen.add(nei)
-                    dfs(nei)
-        res = 0
-        for i in range(n):
-            if i not in seen:
-                dfs(i)
-                res+=1
-        return res
+        p = [i for i in range(n)]
+        def find(x):
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
+        def union(x,y):
+            nx, ny = find(x), find(y)
+            if nx != ny:
+                p[nx] = ny
         
+        
+        for x, y in edges:
+            union(x,y)
+            
+        return len({find(x) for x in range(n)})

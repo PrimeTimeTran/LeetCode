a529d2acadcd406a9828a22ff4f4fa4f1c1c4173
Loi Tran <datloiboi@gmail.com>
2023-03-05 16:39:22 -0500

    Time: 948 ms (22.58%), Space: 32.4 MB (18.98%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        d, t = defaultdict(list), len(arr) - 1
+        for i, n in enumerate(arr):
+            d[n] += i,
+            
+        q, seen = [(0, 0)], defaultdict(lambda:inf)
+        seen[0] = 0
+        for i, h in q:
+            if i == t: return h
+            for j in [i+1, i-1, *d[arr[i]]]:
+                if i != j and 0 <= j <= t and seen[j] > h+1:
+                    seen[j] = h+1
+                    q += (j, h+1),
+            d.pop(arr[i])

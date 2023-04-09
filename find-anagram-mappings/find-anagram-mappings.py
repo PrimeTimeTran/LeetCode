f7275ca4bd7aee912395f64e18d5568295ea5ba8
Loi Tran <datloiboi@gmail.com>
2023-04-08 21:19:48 -0400

    Time: 23 ms (99.42%) | Memory: 13.9 MB (58.79%) - LeetSync

--- find-anagram-mappings/find-anagram-mappings.py
+++ find-anagram-mappings/find-anagram-mappings.py
@@ -2,7 +2,5 @@ class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
         mapping = []
         for c in nums1:
-            print(c)
-            # print(nums2.index(12))
             mapping.append(nums2.index(c))
         return mapping

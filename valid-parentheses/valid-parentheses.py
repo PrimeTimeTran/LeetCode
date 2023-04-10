057209fdf0aa5ddd0e0d4e0aa14c39e233655c71
Loi Tran <datloiboi@gmail.com>
2023-04-10 17:07:03 -0400

    Time: 36 ms (39.95%) | Memory: 13.9 MB (16.17%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hm = {
+            ")": "(",
+            "}": "{",
+            "]": "[",
+        }
+        
+        for c in s:
+            if c in hm:
+                if stack and stack[-1] == hm[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        
+        return len(stack) == 0

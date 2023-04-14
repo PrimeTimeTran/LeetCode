522d69ada2d1706a2f4f75c167c16ef6dfcdeb80
Loi Tran <datloiboi@gmail.com>
2023-04-14 13:42:06 -0400

    Time: 36 ms (67.17%) | Memory: 14 MB (13.82%) - LeetSync

--- unique-morse-code-words/unique-morse-code-words.py
+++ unique-morse-code-words/unique-morse-code-words.py
@@ -5,14 +5,10 @@ class Solution:
 		"-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         alpha = string.ascii_lowercase
         hashmap = {alpha[i]: morse[i] for i in range(len(alpha))}
-        
-		# we make a set for collecting unique/different transformations
         hashset = set()
         for word in set(words):
             t = "" # transform
             for ch in word:
                 t += hashmap[ch]
             hashset.add(t)
-        
-		# in the end we just return the length of hashset, which tells us how many different transformations it has seen
         return len(hashset)

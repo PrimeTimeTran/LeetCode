8b758bd8b856ec6af517e00c7f7e96505425520d
Loi Tran <datloiboi@gmail.com>
2023-04-18 03:31:43 -0400

    Time: 31 ms (72.43%) | Memory: 13.8 MB (55.13%) - LeetSync

--- merge-strings-alternately/merge-strings-alternately.py
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -1,8 +1,6 @@
 class Solution:
     def mergeAlternately(self, w1: str, w2: str) -> str:
         a1, a2 = [c for c in w1], [c for c in w2]
-
-        print(a1, a2)
         res = []
         while a1 or a2:
             if a1:

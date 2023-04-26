1bbf9e6ce13d2e86bbd862bc4ac5ac5b682c8c60
Loi Tran <datloiboi@gmail.com>
2023-04-25 22:47:59 -0400

    Time: 41 ms (12.50%) | Memory: 13.9 MB (39.79%) - LeetSync

--- add-digits/add-digits.py
+++ add-digits/add-digits.py
@@ -1,5 +1,9 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while num >= 10:
-            num = sum(int(i) for i in str(num))
+        if num < 10: return num
+        while num > 9:
+            res = 0
+            for c in str(num):
+                res += int(c)
+            num = res
         return num

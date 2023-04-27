7dfe440430e1ab6c10026676e9fca924a067c6aa
Loi Tran <datloiboi@gmail.com>
2023-04-27 02:23:32 -0400

    Time: 117 ms (5.04%) | Memory: 19.2 MB (6.45%) - LeetSync

---
+++ graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        g = defaultdict(list)
+        
+        for a,b in edges:
+            g[a].append(b); g[b].append(a)
+        
+        seen = set()
+        def dfs(n, p):
+            if n in seen:
+                return False
+            seen.add(n)
+            
+            for nei in g[n]:
+                if nei == p: continue
+                if not dfs(nei, n): return False
+                    
+            return True
+        
+        return dfs(0,-1) and len(seen) == n

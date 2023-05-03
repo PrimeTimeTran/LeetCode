91434c0166c2d5e720b7e7f18061c47f6239ccee
Loi Tran <datloiboi@gmail.com>
2023-05-03 00:02:58 -0400

    Time: 818 ms (21.57%) | Memory: 16.8 MB (9.53%) - LeetSync

--- find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -3,6 +3,4 @@ class Solution:
         answer = []
         answer.append(set([n for n in nums1 if n not in nums2]))
         answer.append(set([n for n in nums2 if n not in nums1]))
-
-        print(answer)
         return answer

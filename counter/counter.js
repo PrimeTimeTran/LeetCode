59b4fc06d9e51953e4cef7cac96f7e324c6389fa
Loi Tran <datloiboi@gmail.com>
2023-05-08 02:10:48 -0400

    Time: 65 ms (13.71%) | Memory: 41.8 MB (66.66%) - LeetSync

--- counter/counter.js
+++ counter/counter.js
@@ -3,10 +3,3 @@
  * @return {Function} counter
  */
 var createCounter = n => () => n++;
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */

8ad7816f21c3eb6f58ac96376ce58bc3683da162
Loi Tran <datloiboi@gmail.com>
2023-05-08 03:29:09 -0400

    Time: 345 ms (84.58%) | Memory: 109.4 MB (5.41%) - LeetSync

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -2,15 +2,11 @@
  * @param {Function} fn
  */
 function memoize(fn) {
-    var cache = [];
+    const cache = [];
 
     return function(...args) {       
-        var key = args.join("-").toString();
-
-        if(cache[key] != undefined ){
-            return cache[key];
-        }
-
+        const key = args.join("-").toString();
+        if(cache[key] != undefined )return cache[key];
         return cache[key] = Number(fn(...args));
     }
 }

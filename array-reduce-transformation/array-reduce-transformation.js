d68b17bd54c15ab3ff9bdbc7a2381b12f609d86d
Loi Tran <datloiboi@gmail.com>
2023-05-10 01:20:03 -0400

    Time: 59 ms (69.57%) | Memory: 42.8 MB (36.45%) - LeetSync

--- array-reduce-transformation/array-reduce-transformation.js
+++ array-reduce-transformation/array-reduce-transformation.js
@@ -1,9 +1,7 @@
-/**
- * @param {number[]} nums
- * @param {Function} fn
- * @param {number} init
- * @return {number}
- */
 var reduce = function(nums, fn, init) {
-      return nums.reduce((acc, val) => fn(acc, val), init)  
+  let acc = init;
+  for (let i = 0; i < nums.length; i++) {
+    acc = fn(acc, nums[i]);
+  }
+  return acc;
 };

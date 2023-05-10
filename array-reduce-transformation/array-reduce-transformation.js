ca3153693be909eb0bc77321d8cf6379783b7c8c
Loi Tran <datloiboi@gmail.com>
2023-05-10 01:23:13 -0400

    Time: 58 ms (74.71%) | Memory: 44.3 MB (16.27%) - LeetSync

--- array-reduce-transformation/array-reduce-transformation.js
+++ array-reduce-transformation/array-reduce-transformation.js
@@ -1,7 +1,15 @@
+// var reduce = function(nums, fn, init) {
+//   let acc = init;
+//   for (let i = 0; i < nums.length; i++) {
+//     acc = fn(acc, nums[i]);
+//   }
+//   return acc;
+// };
+
 var reduce = function(nums, fn, init) {
-  let acc = init;
-  for (let i = 0; i < nums.length; i++) {
-    acc = fn(acc, nums[i]);
+  let acc = init
+  for (let n of nums) {
+    acc = fn(acc, n)
   }
-  return acc;
+  return acc
 };

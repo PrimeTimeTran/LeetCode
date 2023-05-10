29f35b52ec6d6e57650db262f15a900c9169f9b3
Loi Tran <datloiboi@gmail.com>
2023-05-10 01:18:12 -0400

    Time: 43 ms (16.32%) | Memory: 16.3 MB (5.62%) - LeetSync

--- spiral-matrix-ii/spiral-matrix-ii.py
+++ spiral-matrix-ii/spiral-matrix-ii.py
@@ -2,46 +2,31 @@ class Solution:
     def generateMatrix(self, n):
         matrix = []
         if not n: return matrix
-
-        # construct a matrix of zeros
         for row in range(n):
             rowArray = []
             for col in range(n):
                 rowArray.append(0)
             matrix.append(rowArray)
-
-        # layer by layer strategy
         num = 1
-        top = 0 # top layer: top row index
-        right = n - 1 # right layer: right col index
-        down = n - 1 # bottom layer: bottom row index
-        left = 0 # left layer: left col index
-		 
-		 # while layers closing inward but not overlapping. if overlap = reached end of spiral matrix
+        top = 0
+        right = n - 1
+        down = n - 1
+        left = 0
         while left <= right and top <= down: 
-            # top row
-            for i in range(left, right+1): # from left to right. right + 1 to reach the last position in a row
-                matrix[top][i] = num # to fill top row, fix the top row index and increment the col position
-                num += 1 # update num
-            top += 1 # after traversing top row, move top row index inward(downward) by one unit
-
-            # right col
-            for i in range(top, down+1): # from top to bottom. bottom + 1 to reach the last positin in a col
-                matrix[i][right] = num # to fill the right col, fix the right col index and increment the row position
-                num += 1 # update num
-            right -= 1 # after traversing right col, move right col index inward(towards the left) by one unit
-
-            # bottom row
-            for i in range(right, left-1, -1): # from left to right, in reverse order. left-1 to reach the leftmost position in a row
-                matrix[down][i] = num
-                num += 1 # update num
-            down -= 1 # after traversing bottom row, move bottom row index inward(upward) by one unit
-
-            # left col
-            for i in range(down, top-1, -1): # from bottom to top, in reverse order. top-1 to reach the topmost position in a col
-                matrix[i][left] = num # to fill the left col, fix the left col index and increment the row position
+            for i in range(left, right+1):
+                matrix[top][i] = num
                 num += 1
-            left += 1 # after traversing left col, move left col index inward(towards the right) by one unit
-
-            # repeat until top-bottom or left-right indices collide (ie. have completed all layers)
+            top += 1
+            for i in range(top, down+1):
+                matrix[i][right] = num
+                num += 1
+            right -= 1
+            for i in range(right, left-1, -1):
+                matrix[down][i] = num
+                num += 1 
+            down -= 1
+            for i in range(down, top-1, -1):
+                matrix[i][left] = num 
+                num += 1
+            left += 1
         return matrix

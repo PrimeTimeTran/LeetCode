f3ab594ccb5722b93d7802548aa42150a9189b93
Loi Tran <datloiboi@gmail.com>
2023-06-06 19:51:40 -0400

    Time: 1012 ms (11.77%) | Memory: 0B (100.00%) - LeetSync

--- article-views-i/article-views-i.sql
+++ article-views-i/article-views-i.sql
@@ -1,8 +1,4 @@
-# Write your MySQL query statement below
-
-select distinct author_id as id
-from views as v
-where v.author_id = v.viewer_id
-order by 
-v.author_id
-;
+select distinct v.author_id as id 
+from views v
+where v.viewer_id = v.author_id
+order by v.author_id  ASC

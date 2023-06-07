1c5039cd9f087340f19b9776dc59ab8bd8f66d50
Loi Tran <datloiboi@gmail.com>
2023-06-06 21:27:03 -0400

    Time: 2815 ms (14.88%) | Memory: 0B (100.00%) - LeetSync

--- replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,9 +1,4 @@
-# select unique_id, name
-# from employees as e
-# LEFT OUTER JOIN employeeuni as u
-# on e.id = u.id;
-
-select unique_id, name
-from employees
-left join employeeuni
-on employees.id = employeeuni.id
+select unique_id, name 
+from employees e
+left outer join employeeuni u
+on e.id = u.id

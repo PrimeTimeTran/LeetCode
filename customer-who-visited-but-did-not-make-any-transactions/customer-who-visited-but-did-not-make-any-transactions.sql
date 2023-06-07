56ac1ec5f6932b58ef401ec768eb89b4e02f5707
Loi Tran <datloiboi@gmail.com>
2023-06-06 20:05:48 -0400

    Time: 4250 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,13 @@
-SELECT customer_id, COUNT(v.visit_id) as count_no_trans 
-FROM Visits v
-LEFT JOIN Transactions t ON v.visit_id = t.visit_id
-WHERE transaction_id IS NULL
-GROUP BY customer_id;
+# SELECT customer_id, COUNT(v.visit_id) as count_no_trans 
+# FROM Visits v
+# LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+# WHERE transaction_id IS NULL
+# GROUP BY customer_id;
+
+select customer_id, count(visits.visit_id) as count_no_trans
+
+from visits
+LEFT join transactions
+ON visits.visit_id = transactions.visit_id
+where transaction_id IS NULL
+group by customer_id

088ced7f5d49a0a65571db594c9568a4cf283e5e
Loi Tran <datloiboi@gmail.com>
2023-06-12 02:53:03 -0400

    Time: 676 ms (62.94%) | Memory: 45.6 MB (45.42%) - LeetSync

---
+++ snapshot-array/snapshot-array.py
@@ -0,0 +1,17 @@
+class SnapshotArray(object):
+    def __init__(self, n):
+        self.A = [[[-1, 0]] for _ in range(n)]
+        self.snap_id = 0
+
+    def set(self, index, val):
+        if self.A[index][-1][0] == self.snap_id: # if set(0, 4), set(0, 5), then delete previous to save space
+            self.A[index].pop()
+        self.A[index].append([self.snap_id, val])
+
+    def snap(self):
+        self.snap_id += 1
+        return self.snap_id - 1
+
+    def get(self, index, snap_id):
+        i = bisect.bisect(self.A[index], [snap_id + 1]) - 1
+        return self.A[index][i][1]

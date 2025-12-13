0cbb980c7e5ef1d35563bf20c253637a548e7b48
Loi Tran <primetimetran@gmail.com>
2025-12-14 02:33:36 +0700

    Time: 5 ms (64.65%), Space: 18 MB (77.79%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,21 @@
+'''
+1. Understand
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
+class Solution:
+    def permuteUnique(self, nums):
+        res = []
+        nums.sort()
+        def back(rem, path):
+            if not rem:
+                res.append(path)
+            for i in range(len(rem)):
+                if i > 0 and rem[i] == rem[i-1]: continue
+                back(rem[:i] + rem[i+1:], path+[rem[i]])
+        back(nums, [])
+        return res

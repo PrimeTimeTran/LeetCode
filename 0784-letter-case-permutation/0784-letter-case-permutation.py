f2e84e2ae9bada0ea9d728245c24393f445feded
Loi Tran <primetimetran@gmail.com>
2025-12-14 02:55:17 +0700

    Time: 2 ms (93.13%), Space: 18.4 MB (94.17%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,6 +1,11 @@
+'''
+For each character of the string create a lower & uppercase set.
+For both items, combine them with candidate res you have so far(extending it).
+Return the result from doing this with all chars of the string.
+'''
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        ans = [""]
-        for c in s: 
-            ans = [x + cc for x in ans for cc in {c, c.swapcase()}]
-        return ans 
+        res = ['']
+        for c in s:
+            res = [x + cc for x in res for cc in {c, c.swapcase()}]
+        return res

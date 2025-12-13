0683cd42683d542ae4ba44e3a56ef747734469a4
Loi Tran <primetimetran@gmail.com>
2025-12-14 06:08:56 +0700

    Time: 326 ms (6.22%), Space: 38.2 MB (5.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,18 +1,10 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        R, C = len(grid), len(grid[0])
-
-        parent = {}
-        rank = {}
-
+        if not grid: return 0
+        rank, parent, R, C = {}, {}, len(grid), len(grid[0])
         def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])  # path compression
+            if parent[x] != x: parent[x] = find(parent[x])
             return parent[x]
-
         def union(x, y):
             rx, ry = find(x), find(y)
             if rx == ry:
@@ -24,21 +16,18 @@ class Solution:
             else:
                 parent[ry] = rx
                 rank[rx] += 1
-
-        # Initialize DSU for land cells
         for r in range(R):
             for c in range(C):
                 if grid[r][c] == "1":
                     parent[(r, c)] = (r, c)
                     rank[(r, c)] = 0
-
         for r in range(R):
             for c in range(C):
                 if grid[r][c] == "1":
                     for dr, dc in [(1, 0), (0, 1)]:
                         nr, nc = r + dr, c + dc
-                        if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == "1":
+                        inbounds = 0 <= nr < R and 0 <= nc < C
+                        if inbounds and grid[nr][nc] == "1":
                             union((r, c), (nr, nc))
-
         roots = set(find(x) for x in parent)
         return len(roots)

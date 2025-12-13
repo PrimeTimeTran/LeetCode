47f526af1858f9df9a1ea06a392238297a1edfed
Loi Tran <primetimetran@gmail.com>
2025-12-14 06:07:38 +0700

    Time: 328 ms (5.66%), Space: 38 MB (5.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,44 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.ans, seen = 0, set()
+        if not grid:
+            return 0
+
         R, C = len(grid), len(grid[0])
-        def dfs(r,c):
-            inbounds = 0 <= r < R and 0 <= c < C and (r,c) not in seen
-            if inbounds and grid[r][c] == "1":
-                seen.add((r,c)) 
-                for dr, dc in [r+1,c],[r-1,c],[r,c+1],[r,c-1]:
-                    dfs(dr, dc)
-                return True
+
+        parent = {}
+        rank = {}
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])  # path compression
+            return parent[x]
+
+        def union(x, y):
+            rx, ry = find(x), find(y)
+            if rx == ry:
+                return
+            if rank[rx] < rank[ry]:
+                parent[rx] = ry
+            elif rank[rx] > rank[ry]:
+                parent[ry] = rx
+            else:
+                parent[ry] = rx
+                rank[rx] += 1
+
+        # Initialize DSU for land cells
         for r in range(R):
             for c in range(C):
-                if grid[r][c] == "1" and dfs(r,c):
-                    self.ans+=1
-        return self.ans
+                if grid[r][c] == "1":
+                    parent[(r, c)] = (r, c)
+                    rank[(r, c)] = 0
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == "1":
+                    for dr, dc in [(1, 0), (0, 1)]:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == "1":
+                            union((r, c), (nr, nc))
+
+        roots = set(find(x) for x in parent)
+        return len(roots)

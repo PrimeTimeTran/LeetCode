1fd630a41bbbb2d6edc1de34ddcd6bf615e15ef5
Loi Tran <primetimetran@gmail.com>
2025-12-14 04:26:39 +0700

    Time: 0 ms (100%), Space: 17.7 MB (70.6%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,25 @@
-'''
-Iterate grid adding rotten oranges to Q and counting fresh oranges.
-BFS Q incremenet time by 1 for each layer. Change fresh oranges to rotten, add them to q, and decrementing fresh count.
-If at the end fresh is 0, return time, else -1
-'''
-
 class Solution:
     def orangesRotting(self, g: List[List[int]]) -> int:
-        m,n = len(g), len(g[0])
-        f, q = 0, deque([])
-        for r in range(m):
-            for c in range(n):
+        R, C = len(g), len(g[0])
+        q, t, fresh, seen = [], 0, 0, set()
+
+        for r in range(R):
+            for c in range(C):
                 if g[r][c] == 2:
-                    q.append((r,c))
+                    heappush(q, (0, r, c))
                 elif g[r][c] == 1:
-                    f+=1
-        d = [0,-1,0,1,0]
-        t, seen = 0, set()
-        while q and f > 0:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                for i in range(4):
-                    nr,nc = r+d[i],c+d[i+1]
-                    inbounds = 0<=nr<m and 0<=nc<n
-                    if inbounds and g[nr][nc] == 1 and (nr,nc) not in seen:
-                        seen.add((nr,nc))
-                        q.append((nr,nc))
-                        g[nr][nc] = 2
-                        f-=1
-            t+=1
-        return t if f == 0 else -1
+                    fresh += 1
+        while q:
+            time, r, c = heappop(q)
+            t = max(t, time)
+            for dr, dc in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
+                if (
+                    0 <= dr < R and 0 <= dc < C and
+                    g[dr][dc] == 1 and
+                    (dr, dc) not in seen
+                ):
+                    seen.add((dr, dc))
+                    g[dr][dc] = 2
+                    fresh -= 1
+                    heappush(q, (time + 1, dr, dc))
+        return t if fresh == 0 else -1

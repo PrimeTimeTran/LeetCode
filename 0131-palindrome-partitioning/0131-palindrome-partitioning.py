afe4af25a3fcad89d16b972bd1402f1a6564bb08
Loi Tran <primetimetran@gmail.com>
2025-12-14 02:36:12 +0700

    Time: 61 ms (18.31%), Space: 34.5 MB (73.89%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,24 +1,20 @@
-# class Solution:
-#     def partition(self, s: str) -> List[List[str]]:
-#         res = []
-#         def back(rem, strings):
-#             if not rem:
-#                 res.append(strings)
-#             for i in range(1, len(rem)+1):
-#                 if rem[:i] == rem[:i][::-1]:
-#                     back(rem[i:], strings+[rem[:i]])
-#         back(s, [])
-#         return res
-
+'''
+1. Understand
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-
-        def back(cur, path):
-            if not cur:
-                res.append(path)
-            for i in range(1, len(cur)+1):
-                if cur[:i] == cur[:i][::-1]:
-                    back(cur[i:], path+[cur[:i]])
+        def back(rem, strings):
+            if not rem:
+                res.append(strings)
+            for i in range(1, len(rem)+1):
+                if rem[:i] == rem[:i][::-1]:
+                    back(rem[i:], strings+[rem[:i]])
         back(s, [])
         return res

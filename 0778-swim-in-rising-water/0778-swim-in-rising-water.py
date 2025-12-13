5273abe38f3bb77f55a8358038387c3721018ff4
Loi Tran <primetimetran@gmail.com>
2025-12-14 03:36:34 +0700

    Time: 39 ms (39.26%), Space: 18.5 MB (40.25%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -31,28 +31,15 @@ Space:  O()
 '''
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # BFS or DFS
-
-        # 1. Create variables.
         ans, seen, q, n = 0, set(), [[grid[0][0], 0, 0]], len(grid)
-        
-        # 2. Do work on queue.
         while q:
             elevation, r, c = heappop(q)                            # O(logn)
-            
-            # 3. Short circuit if appropriate
             if (r,c) in seen: continue
             seen.add((r,c))
-
-            # 4. Update our max elevation we've had to see so far.
             ans = max(ans, elevation)
             if r == c == n-1:
                 return ans
-            
-            # 5. Explore the neighbors of the cell which we're currently processing.
             for dx, dy in [r+1, c], [r-1, c], [r, c+1], [r, c-1]:
                 inbounds = 0 <= dx < n and 0 <= dy < n
                 if inbounds:
-                    
-                    # 6. Add neighbor to list of candidates to explore next
                     heappush(q, [grid[dx][dy], dx, dy])             # O(logn)

2a6e0569c0e473026ffebe294843604314213fe4
Loi Tran <primetimetran@gmail.com>
2025-12-14 02:47:24 +0700

    Time: 2 ms (26.45%), Space: 18 MB (16.56%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,14 +1,21 @@
+'''
+1. Understand
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
         def back(start, path):
-            if len(path) > k or sum(path) > n:
-                return
+            if len(path) > k or sum(path) > n: return
             if len(path) == k and sum(path) == n:
-                return res.append(path[:])
+                res.append(path)
+                return
             for i in range(start, 10):
-                path.append(i)
-                back(i+1, path)
-                path.pop()
+                back(i+1, path+[i])
         back(1, [])
         return res

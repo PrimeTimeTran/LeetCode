32cacbbd3527a55e2ae425997e6593c54cc10d58
Loi Tran <primetimetran@gmail.com>
2025-12-14 02:15:03 +0700

    Time: 137 ms (51.82%), Space: 59.8 MB (26.87%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,16 +1,23 @@
+'''
+1. Understand
+We want to return a list of lists. The nested lists should be of length k and contain numbers in the range 1 - n.
+
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time: O()
+Space: O()
+'''
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        def backtrack(first = 1, curr = []):
-            # if the combination is done
-            if len(curr) == k:  
-                res.append(curr[:])
-            for i in range(first, n + 1):
-                # add i into the current combination
-                curr.append(i)
-                # use next integers to complete the combination
-                backtrack(i + 1, curr)
-                # backtrack
-                curr.pop()
-        backtrack()
+        def back(start, path):
+            if len(path) == k:
+                return res.append(path[:])
+            for i in range(start, n+1):
+                path.append(i)
+                back(i+1, path)
+                path.pop()
+        back(1, [])
         return res

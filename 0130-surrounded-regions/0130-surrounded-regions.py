d008480a0bfda54833493c945d7ff0f1b6e31ffa
Loi Tran <primetimetran@gmail.com>
2025-12-14 04:43:06 +0700

    Time: 15 ms (20.07%), Space: 22.1 MB (24.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,41 +1,23 @@
-'''
-Iterate mat DFSing on border O cells. In DFS change O cell and neighbor O cells to T.
-
-Iterate mat and turn all O cell to X.
-Iterate mat and turn all T cell to O.
-'''
-
 class Solution:
     def solve(self, b: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        m,n = len(b), len(b[0])
-        
-        seen = set()
-        def dfs(r,c):
-            o = r < 0 or c < 0 or r == m or c == n
-            if o or b[r][c] == 'X' or (r,c) in seen:
-                return
+        R, C, seen = len(b), len(b[0]), set()
+        def explore(r,c):
+            explored = (r,c) in seen
+            inbounds = 0 <= r < R and 0 <= c < C
+            if not inbounds or explored or b[r][c] == 'X': return
             seen.add((r,c))
             b[r][c] = 'T'
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        
-        for r in range(m):
-            for c in range(n):
-                if b[r][c] == 'O' and (r in [0, m-1] or c in [0, n-1]):
-                    dfs(r,c)
-        
-        for r in range(m):
-            for c in range(n):
+            for dr,dc in [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]:
+                explore(dr,dc)
+        for r in range(R):
+            for c in range(C):
+                if b[r][c] == 'O' and (r in [0, R-1] or c in [0, C-1]):
+                    explore(r,c)
+        for r in range(R):
+            for c in range(C):
                 if b[r][c] == 'O':
                     b[r][c] = 'X'
-                    
-        for r in range(m):
-            for c in range(n):
+        for r in range(R):
+            for c in range(C):
                 if b[r][c] == 'T':
                     b[r][c] = 'O'
-                    

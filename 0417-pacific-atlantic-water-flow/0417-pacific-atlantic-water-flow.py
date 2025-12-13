d5624f7613430b08bd76431e5bcf88f5927ef562
Loi Tran <primetimetran@gmail.com>
2025-12-14 04:37:48 +0700

    Time: 39 ms (38.15%), Space: 19.6 MB (14.7%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,39 +1,27 @@
-'''
-Iterate mat borders and DFS each cell if prev value is less than upcoming value.
-Add cells reachable by both oceans to sets representing both oceans.
-Iterate mat and add cells reachable by both oceans to res list and return it.
-'''
-
 class Solution:
-    def pacificAtlantic(self, h: List[List[int]]) -> List[List[int]]:
-        pac,atl = set(), set()
-        m,n = len(h), len(h[0])
-        
-        def dfs(seen, r,c, prev):
-            o = r < 0 or c < 0 or r == m or c == n
-            if o or (r,c) in seen:
-                return 
-            cur = h[r][c]
-            if prev > cur: return
-            seen.add((r,c))
-            
-            dfs(seen, r+1,c,cur)
-            dfs(seen, r-1,c,cur)
-            dfs(seen, r,c+1,cur)
-            dfs(seen, r,c-1,cur)
-        
-        
-        for r in range(m):
-            dfs(pac,r, 0, h[r][0])
-            dfs(atl,r, n-1, h[r][n-1])
-            
-        for c in range(n):
-            dfs(pac,0, c, h[0][c])
-            dfs(atl,m-1, c, h[m-1][c])
+    def pacificAtlantic(self, g: List[List[int]]) -> List[List[int]]:
+        res, R, C = [], len(g), len(g[0])
+        pac, atl = set(), set()
 
-        res = []
-        for r in range(m):
-            for c in range(n):
-                if (r,c) in atl and (r,c) in pac:
-                    res.append((r,c))
+        def dfs(r, c, ocean):
+            if (r, c) in ocean:
+                return
+            ocean.add((r, c))
+            for dr, dc in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
+                inbounds = 0 <= dr < R and 0 <= dc < C
+                if inbounds and g[dr][dc] >= g[r][c]:
+                    dfs(dr, dc, ocean)
+
+        for r in range(R):
+            dfs(r, 0, pac)
+            dfs(r, C-1, atl)
+        for c in range(C):
+            dfs(0, c, pac)
+            dfs(R-1, c, atl)
+
+        for r in range(R):
+            for c in range(C):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+
         return res

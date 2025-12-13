95ef1527b1833e407effaaf8c7ccf79adf4f3a57
Loi Tran <primetimetran@gmail.com>
2025-12-14 05:29:12 +0700

    Time: 11 ms (8.66%), Space: 21.1 MB (6.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,17 +1,12 @@
 class Solution:
     def findOrder(self, N: int, P: List[List[int]]) -> List[int]:
-        seen, g, res = {}, defaultdict(list), []
-        for a, b in P: g[a].append(b)
-        def dfs(n):
-            if n in seen: return seen[n]
-            seen[n] = False
-            for pre in g[n]:
-                if not dfs(pre):
-                    return False
-            seen[n] = True
-            res.append(n)
-            return seen[n]
-        for i in range(N):
-            if not dfs(i):
-                return []
-        return res
+        res, g, seen = [], defaultdict(list), {}
+        for u, v in P: g[u].append(v)
+        def dfs(c):
+            if c in seen: return seen[c]
+            seen[c] = False
+            if not all(dfs(p) for p in g[c]): return False
+            seen[c] = True
+            res.append(c)
+            return True
+        return [] if not all(dfs(c) for c in range(N)) else res 

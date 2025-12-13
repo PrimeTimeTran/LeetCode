4c681fd6c44394ede5dec549fe67db532b816890
Loi Tran <primetimetran@gmail.com>
2025-12-14 03:36:05 +0700

    Time: 43 ms (31%), Space: 18.5 MB (56.41%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,22 +1,58 @@
 '''
-BFS
-BFS with PQ on value of cell/row/column. On each cell update res to max of current res and value.
-Flow 4 cardinal directions to unseen inbound cells and add them to PQ and seen set.
-'''
+1. Understand
+BFS solution. 
+Heap
+We want to create a heap which is a list of cells to check, and then check if we've reached the bottom right cell. If that's case, then we jsut need to return the maximum elevation/time in our path to the bottom right cell.
 
+2. Diagram
+
+
+[0, 5]
+[1, 4]
+
+[0, 5]
+[1, 1]
+
+3. Pseudocode
+    1. Define an answer variable and update during our BFS
+    2. Iterate our queue while we havbe it. 
+    3. Check if we've reached the bottom right cell, if so return the max we've encountered so far.
+    4. For each cell we traverse we also need to traverse neighbors if htey're inbound.
+    5. Ensure we don't backtrack over previously visited cells.
+4. Code
+5. Big O 
+
+Peter's
+O(n^2 log n)
+O(n^2 * log (n^2)) 
+
+Time:   O(logn^2 * ) where n is the length of the row or column
+Space:  O()
+'''
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        res = 0 
-        n = len(grid)
-        seen = set((0,0))
-        pq = [[grid[0][0], 0, 0]]
-        while pq:
-            V, r, c = heappop(pq)
-            res = max(res, V)
-            if r == c == n-1: return res
-            for nr, nc in [[r+1,c],[r,c+1], [r-1,c], [r,c-1]]:
-                inbounds = 0 <= nr < n and 0 <= nc < n
-                unseen = (nr,nc) not in seen
-                if inbounds and unseen:
-                    seen.add((nr,nc))
-                    heappush(pq, [grid[nr][nc], nr, nc])
+        # BFS or DFS
+
+        # 1. Create variables.
+        ans, seen, q, n = 0, set(), [[grid[0][0], 0, 0]], len(grid)
+        
+        # 2. Do work on queue.
+        while q:
+            elevation, r, c = heappop(q)                            # O(logn)
+            
+            # 3. Short circuit if appropriate
+            if (r,c) in seen: continue
+            seen.add((r,c))
+
+            # 4. Update our max elevation we've had to see so far.
+            ans = max(ans, elevation)
+            if r == c == n-1:
+                return ans
+            
+            # 5. Explore the neighbors of the cell which we're currently processing.
+            for dx, dy in [r+1, c], [r-1, c], [r, c+1], [r, c-1]:
+                inbounds = 0 <= dx < n and 0 <= dy < n
+                if inbounds:
+                    
+                    # 6. Add neighbor to list of candidates to explore next
+                    heappush(q, [grid[dx][dy], dx, dy])             # O(logn)

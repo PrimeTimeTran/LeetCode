e101631f43f9354fc4565697f433e8c75be8f736
Loi Tran <primetimetran@gmail.com>
2025-12-14 22:21:15 +0700

    Time: 4 ms (96.62%), Space: 31.4 MB (82.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,3 @@
-# 1. Understand
-# - Take an input array and return true if element appears more than once in the array.
-# - 
-# 2. Diagram
-# nums = [1,2,3,4,5,5] -> nums = [1,2,3,4,5]
-# 
-# 3. Pseudocode
-# 3. Code
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)

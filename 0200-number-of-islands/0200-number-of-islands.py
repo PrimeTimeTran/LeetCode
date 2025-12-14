a483056ac614f968e6ebeaf13a9b3f0266473f60
Loi Tran <primetimetran@gmail.com>
2025-12-14 19:46:30 +0700

    Time: 327 ms (5.94%), Space: 38.1 MB (5.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,9 +1,10 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-        rank, parent, R, C = {}, {}, len(grid), len(grid[0])
+    def numIslands(self, g: List[List[str]]) -> int:
+        if not g: return 0
+        rank, parent, m, n = {}, {}, len(g), len(g[0])
         def find(x):
-            if parent[x] != x: parent[x] = find(parent[x])
+            if parent[x] != x:
+                parent[x] = find(parent[x])
             return parent[x]
         def union(x, y):
             rx, ry = find(x), find(y)
@@ -15,18 +16,17 @@ class Solution:
             else:
                 parent[ry] = rx
                 rank[rx] += 1
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == "1":
-                    parent[(r, c)] = (r, c)
+        for r in range(m):
+            for c in range(n):
+                if g[r][c] == "1":
+                    parent[(r, c)] = (r,c)
                     rank[(r, c)] = 0
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == "1":
-                    for dr, dc in [(1, 0), (0, 1)]:
+        for r in range(m):
+            for c in range(n):
+                if g[r][c] == "1":
+                    for dr, dc in [(1,0), (0,1)]:
                         nr, nc = r + dr, c + dc
-                        inbounds = 0 <= nr < R and 0 <= nc < C
-                        if inbounds and grid[nr][nc] == "1":
+                        inbounds = 0 <= nr < m and 0 <= nc < n
+                        if inbounds and g[nr][nc] == "1":
                             union((r, c), (nr, nc))
-        roots = set(find(x) for x in parent)
-        return len(roots)
+        return len({find(x) for x in parent})

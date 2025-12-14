98c1ddcc9fd8e0f26f957551f46dd806526f7a9b
Loi Tran <primetimetran@gmail.com>
2025-12-14 19:58:40 +0700

    Time: 308 ms (10.33%), Space: 29.9 MB (5.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,20 @@
 class Solution:
     def numIslands(self, g: List[List[str]]) -> int:
         if not g: return 0
-        rank, parent, m, n = {}, {}, len(g), len(g[0])
+        parent, rank, m, n = {}, {}, len(g), len(g[0])
+
         def find(x):
-            if parent[x] != x:
+            if x != parent[x]:
                 parent[x] = find(parent[x])
             return parent[x]
         def union(x, y):
             rx, ry = find(x), find(y)
-            if rx == ry: return
-            if rank[rx] < rank[ry]:
-                parent[rx] = ry
-            elif rank[rx] > rank[ry]:
-                parent[ry] = rx
-            else:
-                parent[ry] = rx
-                rank[rx] += 1
+            parent[rx] = ry
+        
         for r in range(m):
             for c in range(n):
                 if g[r][c] == "1":
-                    parent[(r, c)] = (r,c)
-                    rank[(r, c)] = 0
+                    parent[(r,c)] = (r,c)
         for r in range(m):
             for c in range(n):
                 if g[r][c] == "1":
@@ -29,4 +23,5 @@ class Solution:
                         inbounds = 0 <= nr < m and 0 <= nc < n
                         if inbounds and g[nr][nc] == "1":
                             union((r, c), (nr, nc))
+        
         return len({find(x) for x in parent})

0c2cc4f05a441b9e52634c7619271e36af1b8212
Loi Tran <primetimetran@gmail.com>
2025-12-14 23:06:53 +0700

    Time: 0 ms (100%), Space: 17.7 MB (91.96%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,22 @@
+# 1. Understand
+# Check that each row, column and block has unique numbers. If they do it's solvable otherwise it's not.
+# 2. Diagram
+# Iterate the matrix and add to a seen set for rows, cols and blocks. If the number has already been seen in that container return false
+# 3. Pseudocode
+# 4. Code
+# 5. Analyze
+# O(M*N) where M * N are the lengths of the rows and columns respectively
 class Solution:
-    def isValidSudoku(self, b: List[List[str]]) -> bool:
-        m,n = len(b), len(b[0])
-        rowSets = defaultdict(set)
-        colSets = defaultdict(set)
-        squareSets = defaultdict(set)
-        
-        for r in range(m):
-            for c in range(n):
-                num = b[r][c]
-                if num == '.':
-                    continue
-                if num in rowSets[r] or num in colSets[c] or num in squareSets[(r//3, c//3)]:
-                    return False
-                rowSets[r].add(num)
-                colSets[c].add(num)
-                squareSets[(r//3, c//3)].add(num)
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows, cols, blocks = defaultdict(set), defaultdict(set), defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num == '.': continue
+                key = (r//3, c//3)
+                seen = num in rows[r] or num in cols[c] or num in blocks[key]
+                if seen: return False
+                rows[r].add(num)
+                cols[c].add(num)
+                blocks[key].add(num)
         return True

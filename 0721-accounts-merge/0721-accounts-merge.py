f58f7b0d3ca21fc6b04b3615927cd74d4c714236
Loi Tran <primetimetran@gmail.com>
2025-12-14 20:13:32 +0700

    Time: 43 ms (28.1%), Space: 20.7 MB (73.08%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,29 +1,29 @@
-class UF:
-    def __init__(self, N):
-        self.parents = list(range(N))
-    def union(self, child, parent):
-        self.parents[self.find(child)] = self.find(parent)
-    def find(self, x):
-        if x != self.parents[x]:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-    
 class Solution:
-    # 196 ms, 82.09%. 
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        uf = UF(len(accounts))
+        res, parent, email_to_name, components = [], {}, {}, defaultdict(list)
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        def find(x):
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
         
-        # Creat unions between indexes
-        ownership = {}
-        for i, (_, *emails) in enumerate(accounts):
+        # Build the parent list using the accounts
+        for account in accounts:
+            name, emails = account[0], account[1:]
+            
+            # If the email isn't in the parent list, then set it as it's own parent.
             for email in emails:
-                if email in ownership:
-                    uf.union(i, ownership[email])
-                ownership[email] = i
-        
-        # Append emails to correct index
-        ans = collections.defaultdict(list)
-        for email, owner in ownership.items():
-            ans[uf.find(owner)].append(email)
-        
-        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
+                if email not in parent:
+                    parent[email] = email
+                    email_to_name[email] = name
+            for i in range(1, len(emails)):
+                union(emails[0], emails[i])
+
+        for email in parent:
+            root = find(email)
+            components[root].append(email)
+        for root, emails in components.items():
+            res.append([email_to_name[root]] + sorted(emails))
+        return res

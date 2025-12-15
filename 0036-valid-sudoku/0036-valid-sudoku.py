8d3c548a225259a57b200f50c85407c55c5c1f93
Loi Tran <primetimetran@gmail.com>
2025-12-15 23:06:10 +0700

    Time: 7 ms (36.46%), Space: 17.6 MB (91.92%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,11 +1,3 @@
-# 1. Understand
-# Check that each row, column and block has unique numbers. If they do it's solvable otherwise it's not.
-# 2. Diagram
-# Iterate the matrix and add to a seen set for rows, cols and blocks. If the number has already been seen in that container return false
-# 3. Pseudocode
-# 4. Code
-# 5. Analyze
-# O(M*N) where M * N are the lengths of the rows and columns respectively
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         R, C, B = defaultdict(set), defaultdict(set), defaultdict(set)

c16954b52722beb63b38ac130f642f64fefde664
Loi Tran <primetimetran@gmail.com>
2025-12-16 03:07:32 +0700

    Time: 7 ms (82.68%), Space: 18.1 MB (67.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s)-1
+        l, r = 0, len(s) -1
         while l < r:
             while l < r and not s[l].isalnum():
                 l+=1

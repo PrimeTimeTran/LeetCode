ee98d431e5d78569841f14523b99d9e203148d4c
Loi Tran <primetimetran@gmail.com>
2025-12-15 22:54:16 +0700

    Time: 11 ms (83.04%), Space: 21.3 MB (42.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
-        for w in strs:
-            groups[tuple(sorted(w))].append(w)
-        return list(groups.values())
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    res = defaultdict(list)
+    for w in strs:
+        res[tuple(sorted(w))].append(w)
+    return list(res.values())

a181196453b3812be5d0e3b3276f8b6f701c736c
Loi Tran <primetimetran@gmail.com>
2025-12-15 23:58:40 +0700

    Time: 7 ms (82.62%), Space: 17.8 MB (94.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,7 +6,7 @@ class Solution:
                 l+=1
             while l < r and not s[r].isalnum():
                 r-=1
-            if not s[l].lower() == s[r].lower():
+            if s[l].lower() != s[r].lower():
                 return False
             l+=1
             r-=1

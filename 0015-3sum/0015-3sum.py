c3b0796460f4ffdf28ff0b06870dd7f967df6a66
Loi Tran <primetimetran@gmail.com>
2025-12-15 23:29:52 +0700

    Time: 607 ms (38.64%), Space: 20.7 MB (70.58%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,3 @@
-'''
-1. Understand
-    - We've given nums which is a list of nums.
-    - We want to return a list of lists where each sublist is the numbers which sum to 0.
-2. Pseudocode
-    1. Sort nums
-    2. Iterate numbers, setting l pointer to current index + 1 and r pointer to len(nums).
-    3. Close window until we're out of bounds
-    4. On each iteration check that the sum of the numbers <, >, or == 0. If 0 then append the list of current of nums.
-    5. Check if we can close our window more within the current iteration because we don't want duplicates.
-
-    - Make sure to check for equality between indexes.
-
-3. Diagram
-nums = [-1, 0, 1, 2,-1,-4]
-nums = [-4,-1,-1, 0, 1, 2]
-               c  l     r
-0: -4,
-    l = -1
-    r = 2
-    ... []
-1: -1
-    l = -1
-    r = 2
-    append [-1,-1,2]
-
-
-
-4. Code
-5. Big O
-'''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []

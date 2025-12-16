9c61efa5c28b89fc45606e47e4540d7504d15d30
Loi Tran <primetimetran@gmail.com>
2025-12-16 21:48:10 +0700

    Time: 0 ms (100%), Space: 17.7 MB (82.4%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, n1: int, n2: int) -> int:
+        x = n1 ^ n2
+        res = 0
+        while x:
+            res += 1
+            x &= x-1
+        return res

aa8a08aede10ecdd5419c6d77ab7353e48cd1178
Loi Tran <primetimetran@gmail.com>
2025-12-16 21:47:58 +0700

    Time: 35 ms (22.55%), Space: 18.6 MB (57.87%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,15 +4,10 @@ class Solution:
             res = 0
             while v != 0:
                 res+=1
-                v = v & v-1
+                v &= v-1
             return res
-            
         res = []
         for i in range(n+1):
             val = countOnes(i)
             res.append(val)
-            
         return res
-
-# 0000000        
-# 128,64,32,16,8,4,2,1

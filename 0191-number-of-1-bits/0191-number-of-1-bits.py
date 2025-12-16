741a98b656bd8a6bcc0016e492686599b96c29cf
Loi Tran <primetimetran@gmail.com>
2025-12-17 00:46:01 +0700

    Time: 0 ms (100%), Space: 17.8 MB (30.88%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,8 +1,3 @@
-'''
-1. 
-n - 1 will flip the least significant 1 to 0. All other 0 bits to the right of this bit become a 1
-n & n-1 "persists" the 0's after switching the least significant set bit to 0(maintains 0s right of it).
-'''
 class Solution:
   def hammingWeight(self, n: int) -> int:
     res = 0

a6d74ea2a634254460b9c3b1729504a098aa7ae8
Loi Tran <primetimetran@gmail.com>
2025-12-17 01:39:30 +0700

    Time: 0 ms (100%), Space: 17.9 MB (41.47%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num <= 0:
+            return False
+
+        while num & 1 == 0:
+            num >>= 1
+        for p in [3, 5]:
+            while num % p == 0:
+                num //= p
+        return num == 1

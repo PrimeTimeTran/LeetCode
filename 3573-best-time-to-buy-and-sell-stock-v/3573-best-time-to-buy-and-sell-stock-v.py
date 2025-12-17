6d166335a80981a419e751cbc9f1da3387f0fe78
Loi Tran <primetimetran@gmail.com>
2025-12-17 21:59:24 +0700

    Time: 4220 ms (17.7%), Space: 42 MB (40.76%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -1,25 +1,33 @@
+'''
+1. Understand
+Return an int representing the maximum profit you can make making at most k transactions.
+DP because it's asking to find the best given the constraint k from a list of subproblems.
+
+2. Diagram
+3. Pseudocode
+4. Code
+5. Big O
+Time:   O()
+Space:  O()
+'''
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
+        # i         =   day/price
+        # remaining =   how many transactions remaining
+        # carry      =   action of carryious state. 
+        #               -1  = free
+        #               0   = sold
+        #               1   = bought
         @lru_cache(maxsize=1000*4)
-        def dps(i, lf, kd):
-            # array or capacity is over
-            if i >= len(prices) or lf == 0:
-                # if we finished with a pending operation return -inf
-                return 0 if kd==-1 else -inf
-            
+        def dp(i, rem, carry):
+            if i >= len(prices) or rem == 0:
+                return 0 if carry == -1 else -inf
             mx = -inf
-            if kd > -1:
-                if kd == 0:
-                    # buy, because we sold
-                    mx = dps(i+1, lf-1, -1)  - prices[i]
-                else:
-                    # sell, because we bought
-                    mx = dps(i+1, lf-1, -1)  + prices[i]
+            p = prices[i]
+            if carry > -1:
+                mx = dp(i+1, rem-1, -1) + (-p if carry == 0 else p)
             else:
-                # sell or buy
-                mx = max(dps(i+1, lf, 0)  + prices[i],
-                            dps(i+1, lf, 1)  - prices[i])
-            # do nothing
-            return max(dps(i+1, lf, kd), mx)
-        
-        return dps(0, k, -1)
+                mx = max(dp(i+1, rem, 0)  + p,
+                            dp(i+1, rem, 1)  - p)
+            return max(dp(i+1, rem, carry), mx)
+        return dp(0, k, -1)

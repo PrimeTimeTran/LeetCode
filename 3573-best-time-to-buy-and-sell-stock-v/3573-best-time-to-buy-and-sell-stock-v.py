1f88a3cc3367a7c7cd351b7909010f38f65c5307
Loi Tran <primetimetran@gmail.com>
2025-12-17 22:31:00 +0700

    Time: 4041 ms (19.99%), Space: 42.1 MB (40.76%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -12,25 +12,28 @@ Space:  O()
 '''
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
-        # i         =   day/price
-        # remaining =   how many transactions remaining
-        # carry      =   action of carryious state. 
-        #               -1  = free
-        #               0   = sold
-        #               1   = bought
+        # i             =   day/price
+        # remaining     =   how many transactions remaining
+        # carry         =   action of carries forward prev state. 
+        #                   -1      =   free
+        #                   0       =   sold
+        #                   1       =   bought
         @lru_cache(maxsize=1000*4)
         def dp(i, rem, carry):
-            if i >= len(prices) or rem == 0:
+            if i == len(prices) or rem == 0:
                 return 0 if carry == -1 else -inf
-            mx = -inf
-            p = prices[i]
+            p, mx = prices[i], -inf
             if carry > -1:
                 if carry == 0:
-                    mx = dp(i+1, rem-1, -1) - p
+                    mx = dp(i+1, rem - 1, -1) - p
                 else:
-                    mx = dp(i+1, rem-1, -1) + p
+                    mx = dp(i+1, rem - 1, -1) + p
             else:
-                mx = max(dp(i+1, rem, 0)  + p,
-                            dp(i+1, rem, 1)  - p)
+                mx = max(
+                    # Sell
+                    dp(i+1, rem, 0) + p,
+                    # Buy
+                    dp(i+1, rem, 1) - p,
+                )
             return max(dp(i+1, rem, carry), mx)
         return dp(0, k, -1)

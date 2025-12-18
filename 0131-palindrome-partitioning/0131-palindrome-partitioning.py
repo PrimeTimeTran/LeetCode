f4caab9076dba60cc19dcaaf50831115f8738782
Loi Tran <primetimetran@gmail.com>
2025-12-19 01:43:43 +0700

    Time: 61 ms (18.47%), Space: 34.5 MB (74.29%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -10,11 +10,11 @@ Space:   O()
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        def back(cur, path):
-            if not cur:
-                res.append(path)
-            for i in range(1, len(cur)+1):
-                if cur[:i] == cur[:i][::-1]:
-                    back(cur[i:], path + [cur[:i]])
+        def back(rem, strings):
+            if not rem:
+                res.append(strings)
+            for i in range(1, len(rem)+1):
+                if rem[:i] == rem[:i][::-1]:
+                    back(rem[i:], strings+[rem[:i]])
         back(s, [])
         return res

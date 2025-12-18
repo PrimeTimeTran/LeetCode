a33cd664383852f1613e23be35c6ae7a8e80aab1
Loi Tran <primetimetran@gmail.com>
2025-12-19 01:48:09 +0700

    Time: 11 ms (49.07%), Space: 17.5 MB (97.93%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,32 @@
+'''
+1. Understand
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = []
+        board = [['.'] * n for _ in range(n)]
+        pDiag, nDiag, C = set(), set(), set()
+        def back(r):
+            if r == n:
+                res.append([''.join(r) for r in board])
+                return 
+            for c in range(n):
+                clear = not ((r+c) in pDiag or (r-c) in nDiag or c in C)
+                if clear:
+                    pDiag.add(r+c)
+                    nDiag.add(r-c)
+                    C.add(c)
+                    board[r][c] = 'Q'
+                    back(r+1)
+                    pDiag.remove(r+c)
+                    nDiag.remove(r-c)
+                    C.remove(c)
+                    board[r][c] = '.'
+        back(0)
+        return len(res)

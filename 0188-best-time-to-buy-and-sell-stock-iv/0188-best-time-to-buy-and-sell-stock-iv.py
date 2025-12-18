b6d02684d9adf725a1d037456f01873c454b6242
Loi Tran <primetimetran@gmail.com>
2025-12-18 13:18:51 +0700

    Time: 254 ms (11.94%), Space: 84.7 MB (6.11%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,35 +1,37 @@
+'''
+1. Understand
+We should be returning an integer representing the sum profit we might receive
+If we buy/sell optimally.
+
+2. Diagram
+3. Pseudocode
+- Define a top memo function which expands from day 0 to the end.
+    - Identify base cases which prevent Stack Overflows. 
+    - Perform a "reduction" of the problem space such that it gets closer to the base case....
+    - "Do business logic..."
+- Subproblem
+- Base Case
+- Top Down Memo
+    - Recursive
+- Bottom Up Tab
+    - Create a store and build on it.
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         @lru_cache(None)
-        def dp(i,rem):
-            if i == len(prices) or rem == 0:
-                return 0
-            skip = dp(i+1, rem)
-            future = dp(i+1, rem-1)
-            # Apply buy/sell sign
-            # Even number transactions means debit(-)
-            # Odd  number transactions means credit(+)
-            sign = -1 if rem % 2 == 0 else 1
-            take = future + sign * prices[i]
-            return max(skip, take)
-        return dp(0, k * 2)
-
-# class Solution:
-#     def maxProfit(self, k: int, prices: List[int]) -> int:
-#         # bottom up 
-#         dp = [0]*((k*2)+1)
-
-#         for i in range(len(prices)-1,-1,-1):
-#             next_dp = [0]*((k*2)+1)
-
-#             for j in range(1,(k*2)+1):
-#                 col = (k*2) - j
-#                 if j%2 == 1:
-#                     # sell the stock or skip
-#                     next_dp[col] = max(dp[col],prices[i]+dp[col+1])
-#                     continue
-#                 next_dp[col] = max(dp[col],-prices[i]+dp[col+1])
-#             dp = next_dp
-#         print(dp)
-#         return dp[0]
-        
+        def dp(i, count):
+            # If we've previously bought, our count should be odd
+            opened = count % 2 == 0
+            if i == len(prices) or count == 0:
+                return 0 if opened else -inf
+            price = prices[i]
+            # If we've opened a position we debit, credit otherwise
+            sign = -1 if opened else 1
+            take = dp(i+1, count-1) + (price * sign)
+            skip = dp(i+1, count)
+            return max(take, skip)
+        return dp(0, k*2)

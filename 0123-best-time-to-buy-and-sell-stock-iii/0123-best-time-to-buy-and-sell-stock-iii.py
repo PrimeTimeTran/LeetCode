8821ccd85c2476aa2f37ba84850e0ba17c11f80c
Loi Tran <primetimetran@gmail.com>
2025-12-18 08:25:31 +0700

    Time: 1522 ms (16.44%), Space: 581.2 MB (5.14%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -10,13 +10,12 @@ class Solution:
                 return -inf if open_position else 0
             price = prices[i]
             i+=1
-            pnl_today_skipped = dp(i, completed_count, open_position)
+            pnl_today_excluded = dp(i, completed_count, open_position)
             if open_position:
-                sell = price + dp(i, completed_count + 1, False)
-                return max(pnl_today_skipped, sell)
+                pnl_today_included = price + dp(i, completed_count + 1, False)
             else:
-                buy = -price + dp(i, completed_count, True)
-                return max(pnl_today_skipped, buy)
+                pnl_today_included = -price + dp(i, completed_count, True)
+            return max(pnl_today_excluded, pnl_today_included)
 
         return dp(0, 0, False)
 

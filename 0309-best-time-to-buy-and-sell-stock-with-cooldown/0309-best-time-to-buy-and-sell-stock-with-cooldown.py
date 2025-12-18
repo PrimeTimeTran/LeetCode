a5811fead39edd114d2a36159b98636e4b40de2f
Loi Tran <primetimetran@gmail.com>
2025-12-18 09:43:04 +0700

    Time: 7 ms (41.83%), Space: 22.7 MB (5.14%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -13,11 +13,11 @@ class Solution:
             if cooldown:
                 return dp(i, open_position, False)
             if open_position == "buy_to_open":
-                pnl_from_close = price + dp(i, False, True)
+                pnl_from_close = price + dp(i, None, True)
                 pnl_from_holding = dp(i, open_position, False)
                 return max(pnl_from_close, pnl_from_holding)
             else:
                 pnl_from_opening  = -price + dp(i, "buy_to_open", False)
-                pnl_from_waiting = dp(i, False, False)
+                pnl_from_waiting = dp(i, None, False)
                 return max(pnl_from_opening, pnl_from_waiting)
         return dp(0, False, False)

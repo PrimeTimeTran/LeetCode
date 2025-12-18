9d5f4865fac651e1305d288fa512c574766cf0e2
Loi Tran <primetimetran@gmail.com>
2025-12-18 09:47:05 +0700

    Time: 11 ms (14.34%), Space: 22.5 MB (5.14%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from math import inf
-from typing import List
+from typing import List, Union
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
@@ -10,14 +10,16 @@ class Solution:
                 return 0 if open_position != "buy_to_open" else -inf
             price = prices[i]
             i+=1
-            if cooldown:
+
+            if cooldown: 
                 return dp(i, open_position, False)
+
             if open_position == "buy_to_open":
-                pnl_from_close = price + dp(i, None, True)
-                pnl_from_holding = dp(i, open_position, False)
-                return max(pnl_from_close, pnl_from_holding)
+                pnl_today_included = price + dp(i, None, True)
+                pnl_today_skipped = dp(i, open_position, False)
             else:
-                pnl_from_opening  = -price + dp(i, "buy_to_open", False)
-                pnl_from_waiting = dp(i, None, False)
-                return max(pnl_from_opening, pnl_from_waiting)
-        return dp(0, False, False)
+                pnl_today_included  = -price + dp(i, "buy_to_open", False)
+                pnl_today_skipped = dp(i, None, False)
+            return max(pnl_today_included, pnl_today_skipped)
+
+        return dp(0, None, False)

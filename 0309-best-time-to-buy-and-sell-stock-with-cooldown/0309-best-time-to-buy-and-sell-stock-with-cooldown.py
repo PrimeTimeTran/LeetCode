bc5ced8d8a736f35379d83e444ae75c922423e4d
Loi Tran <primetimetran@gmail.com>
2025-12-18 09:48:41 +0700

    Time: 11 ms (14.34%), Space: 22.6 MB (5.14%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -7,18 +7,18 @@ class Solution:
         @lru_cache(None)
         def dp(i: int, open_position: Union[str, bool], cooldown):
             if i == len(prices):
-                return 0 if open_position != "buy_to_open" else -inf
+                return 0 if not open_position else -inf
             price = prices[i]
             i+=1
 
-            if cooldown: 
+            if cooldown:
                 return dp(i, open_position, False)
 
-            if open_position == "buy_to_open":
+            if open_position:
                 pnl_today_included = price + dp(i, None, True)
                 pnl_today_skipped = dp(i, open_position, False)
             else:
-                pnl_today_included  = -price + dp(i, "buy_to_open", False)
+                pnl_today_included  = -price + dp(i, True, False)
                 pnl_today_skipped = dp(i, None, False)
             return max(pnl_today_included, pnl_today_skipped)
 

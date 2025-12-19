396944b38c1cee326f38f281b44158ab1059d047
Loi Tran <primetimetran@gmail.com>
2025-12-20 01:25:34 +0700

    Time: 12 ms (43.72%), Space: 18 MB (99.2%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -9,24 +9,22 @@ Space:   O()
 '''
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res = []
-        board = [['.'] * n for _ in range(n)]
-        pDiag, nDiag, C = set(), set(), set()
+        res, board = [], [['.']*n for _ in range(n)]
+        C, pDiag, nDiag = set(), set(), set()
         def back(r):
             if r == n:
                 res.append([''.join(r) for r in board])
-                return 
             for c in range(n):
-                clear = not ((r+c) in pDiag or (r-c) in nDiag or c in C)
-                if clear:
-                    pDiag.add(r+c)
-                    nDiag.add(r-c)
+                clear = not (c in C or r+c in pDiag or r-c in nDiag)
+                if clear: 
+                    pDiag.add((r+c))
+                    nDiag.add((r-c))
                     C.add(c)
                     board[r][c] = 'Q'
                     back(r+1)
-                    pDiag.remove(r+c)
-                    nDiag.remove(r-c)
+                    pDiag.remove((r+c))
+                    nDiag.remove((r-c))
                     C.remove(c)
                     board[r][c] = '.'
-        back(0)
-        return res
+            return res
+        return back(0)

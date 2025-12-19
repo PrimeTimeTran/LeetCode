55ac27a3bb1a6cfb84ae14f016f59130d8f73974
Loi Tran <primetimetran@gmail.com>
2025-12-20 01:35:56 +0700

    Time: 11 ms (49.09%), Space: 17.4 MB (99.93%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -9,24 +9,22 @@ Space:   O()
 '''
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        res = []
-        board = [['.'] * n for _ in range(n)]
-        pDiag, nDiag, C = set(), set(), set()
+        res, board = [], [['.']*n for _ in range(n)]
+        C, pDiag, nDiag = set(), set(), set()
         def back(r):
             if r == n:
                 res.append([''.join(r) for r in board])
-                return 
             for c in range(n):
-                clear = not ((r+c) in pDiag or (r-c) in nDiag or c in C)
-                if clear:
-                    pDiag.add(r+c)
-                    nDiag.add(r-c)
+                clear = not (c in C or r+c in pDiag or r-c in nDiag)
+                if clear: 
+                    pDiag.add((r+c))
+                    nDiag.add((r-c))
                     C.add(c)
                     board[r][c] = 'Q'
                     back(r+1)
-                    pDiag.remove(r+c)
-                    nDiag.remove(r-c)
+                    pDiag.remove((r+c))
+                    nDiag.remove((r-c))
                     C.remove(c)
                     board[r][c] = '.'
-        back(0)
-        return len(res)
+            return res
+        return len(back(0))

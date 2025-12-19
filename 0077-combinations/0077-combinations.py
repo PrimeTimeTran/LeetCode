42db2718bbb954ea1b5b1edfe9085e61fa6684c3
Loi Tran <primetimetran@gmail.com>
2025-12-20 01:43:34 +0700

    Time: 250 ms (8%), Space: 59.4 MB (96.08%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,10 @@
-'''
-1. Understand
-2. Diagram
-3. Pseudocode
-4. Code
-5. BigO
-Time:    O()
-Space:   O()
-'''
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         def back(start, path):
             if len(path) == k:
-                return res.append(path)
+                res.append(path)
             for i in range(start, n+1):
                 back(i+1, path+[i])
-        back(1, [])
-        return res
+            return res
+        return back(1, [])

148757bc6472261d4051b5178688d750144f424d
Loi Tran <primetimetran@gmail.com>
2025-12-20 01:39:16 +0700

    Time: 0 ms (100%), Space: 17.3 MB (99.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,8 +1,20 @@
+'''
+1. Understand
+2. Diagram
+3. Pseudocode
+4. Code
+5. BigO
+Time:    O()
+Space:   O()
+'''
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dict = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7': "pqrs", 
-            '8':"tuv", '9':"wxyz"}
-        comb = [''] if digits else []
+        store = {
+            '2':"abc", '3':"def", '4':"ghi", '5':"jkl", 
+            '6':"mno", '7': "pqrs", '8':"tuv", '9':"wxyz"
+        }
+        res = ['']
         for d in digits:
-            comb = [p+char for p in comb for char in dict[d]]
-        return comb
+            res = [prefix + c for prefix in res for c in store[d]]
+
+        return res

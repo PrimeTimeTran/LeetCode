de3b140bb17286823bc4b1e3846669d6dcc23eee
Loi Tran <primetimetran@gmail.com>
2025-12-20 04:57:33 +0700

    Time: 0 ms (100%), Space: 17.4 MB (99.94%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,10 +1,17 @@
+'''
+1. Understand
+Key mental insight
+- In a rotated sorted array, the minimum element is the only element where the previous element is greater than it (or equivalently, it’s the “pivot point”).
+- Another way to think: the minimum is always in the unsorted half of the current search range.
+
+'''
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
         while l < r:
-            m = (l + r) // 2
-            if nums[m] < nums[r]:
-                r = m
-            else:
+            m = (l+r) // 2
+            if nums[m] >= nums[r]:
                 l = m + 1
+            else:
+                r = m
         return nums[l]

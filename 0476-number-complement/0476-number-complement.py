c197e2c8584f2db908990c1c5d94313ec3dd19b3
Loi Tran <primetimetran@gmail.com>
2025-12-20 00:08:14 +0700

    Time: 0 ms (100%), Space: 17.3 MB (99.93%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,6 +1,4 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        if num == 0:
-            return 1
         mask = (1 << num.bit_length()) - 1
-        return num ^ mask
+        return mask ^ num

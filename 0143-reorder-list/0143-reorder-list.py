c053f185f96ad4cfffce29c491f6b91fe54986ff
Loi Tran <primetimetran@gmail.com>
2025-12-20 12:04:48 +0700

    Time: 1 ms (57.76%), Space: 21.9 MB (99.89%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,9 +1,7 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        d = ListNode(0, head)
-        s, f = d, d
-        # Find the middle of the list
-        while f and f.next:
+        s, f = head, head
+        while f.next and f.next.next:
             s = s.next
             f = f.next.next
         

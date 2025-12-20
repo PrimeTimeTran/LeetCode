5e8d5ed204155667e2fe72fbe97185dc46d2d9ce
Loi Tran <primetimetran@gmail.com>
2025-12-20 07:10:38 +0700

    Time: 0 ms (100%), Space: 17.3 MB (99.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,14 +3,16 @@
 l = 0, r = 3
 (l+r)//2 = 1        # mid leans left
 l + (r-l+1)//2 = 2  # mid leans right
+
+2025-12-20 - 162. Find Peak Element
 '''
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums)-1
         while l < r:
-            m = l + (r - l + 1) // 2
+            m = l + (r-l+1) // 2
             if nums[m-1] > nums[m]:
                 r = m - 1
             else:
                 l = m
-        return l
+        return r

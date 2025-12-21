0c1749697ecdb4a5baec90be9381c8e60e1c7e26
Loi Tran <primetimetran@gmail.com>
2025-12-22 02:58:42 +0700

    Time: 559 ms (13.05%), Space: 230.4 MB (9.55%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -13,13 +13,13 @@ class Solution:
             
             if holding:
                 # Option 1: sell today
-                sell = price - fee + dp(i+1, False)
+                sell = price  + dp(i+1, False)
                 # Option 2: skip / hold
                 hold = dp(i+1, True)
                 return max(sell, hold)
             else:
                 # Option 1: buy today
-                buy = -price + dp(i+1, True)
+                buy = -price - fee + dp(i+1, True)
                 # Option 2: skip
                 skip = dp(i+1, False)
                 return max(buy, skip)

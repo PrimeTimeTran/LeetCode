def646df6d5afc64fcb5efe3c5de7d8dc4d80213
Loi Tran <primetimetran@gmail.com>
2025-12-21 15:29:05 +0700

    Time: 91 ms (54.41%), Space: 26.4 MB (98.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,7 @@
-# class Solution:
-#     def maxProfit(self, P: List[int]) -> int:
-#         ans,l = 0, 0
-#         for r, n in enumerate(P):
-#             ans = max(ans, n - P[l])
-#             if n < P[l]: l = r
-#         return ans
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         ans = l = 0
-        for r, n in enumerate(prices):
-            ans = max(ans, n - prices[l])
-            if n < prices[l]: l = r
+        for r, p in enumerate(prices):
+            ans = max(ans, p - prices[l])
+            if p < prices[l]: l = r
         return ans

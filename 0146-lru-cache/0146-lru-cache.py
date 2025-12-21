110274fe5ada9a4f9e70c830e096381933b67ec2
Loi Tran <primetimetran@gmail.com>
2025-12-21 09:50:12 +0700

    Time: 119 ms (76.85%), Space: 77.8 MB (90.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,69 +1,28 @@
-'''
-1. Constraints
-We'll receive a stream of data and we need to return values depending on the stream's messages.
-
-2. Diagram
-
-["LRUCache", "put",  "put",  "get", "put",  "get", "put",  "get", "get", "get"]
-[[2],        [1, 1], [2, 2], [1],   [3, 3], [2],   [4, 4], [1],   [3],   [4]]
-                                                                          ^                                                                          
-CAP 2
-CACHE
-
-{
-    3:3,
-    4:4
-}              
-
-LL:
-left > 3 > 4 > right
-
-return 1
-return -1
-return -1
-return 3
-return 4
-
-3. Pseudocode
-
-We define a ListNode class which has keyy, value, next, and prev properties.
-When we init LRUCache we define cap, cache, left, and right.
-
-We define a get method which looks at our cache and updates it to the end of the linkedList if 
-
-4. Code
-
-'''
-
 class ListNode:
     def __init__(self, key, val):
-        self.key,self.val = key, val
+        self.key, self.val = key, val
         self.next = None
         self.prev = None
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = {}
         self.cap = capacity
+        self.cache = {}
         self.left, self.right = ListNode(0,0), ListNode(0,0)
         self.left.next, self.right.prev = self.right, self.left
-
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-        
-    def remove(self, n) -> None:
-        prev, nxt = n.prev, n.next
-        prev.next, nxt.prev = nxt, prev
-
-    def insert(self, n) -> None:
+    def insert(self, n) -> int:
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = n
         n.prev, n.next = prev, nxt
-
+    def remove(self, n) -> int:
+        prev, nxt = n.prev, n.next
+        prev.next, nxt.prev = nxt, prev
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
@@ -73,3 +32,9 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)

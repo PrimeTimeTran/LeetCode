99b1f461aee732d129a533cd44ef405dce927951
Loi Tran <primetimetran@gmail.com>
2025-12-21 15:14:47 +0700

    Time: 214 ms (5.05%), Space: 26.5 MB (98.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = -prices[0]   # best profit after buying once
-        sell = 0           # best profit after selling once
-
+        NEG_INF = float('-inf')
+        dp = [0, -prices[0]]
         for price in prices[1:]:
-            buy = max(buy, -price)        # buy ONCE
-            sell = max(sell, buy + price) # sell ONCE
-
-        return sell
+            new_dp = [NEG_INF, NEG_INF]
+            new_dp[0] = dp[0]
+            new_dp[1] = dp[1]
+            new_dp[1] = max(new_dp[1], -price)
+            new_dp[0] = max(new_dp[0], dp[1] + price)
+            dp = new_dp
+        return dp[0]

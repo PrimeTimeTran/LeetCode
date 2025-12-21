f5cdfb142e290ad058bc95919d5284e2b9ab5eee
Loi Tran <primetimetran@gmail.com>
2025-12-22 03:41:39 +0700

    Time: 864 ms (5.03%), Space: 342.8 MB (8.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,27 @@
+'''
+1. Understand
+Return the best profit you can make given a list of prices of stocks on a range of days.
+You can make at most one transaction and can only profit when you buy & then sell a stock.
+
+State Machine:
+- Day purchaseed and remaining.
+- Whether or not holding a stock(previously purchased).
+
+Pseudocode:
+From the first day skip and take the day until you've reached the end of the list.
+Inside of recursion, compare which was more profitable. Taking or selling.
+When moving forward track whether or not you bought/sold so you can know when you need to do on subsequent days.
+'''
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
         @lru_cache(None)
-        def dp(i: int, holding: bool) -> int:
-            if i == n:
+        def dp(i, holding):
+            if i == len(prices):
                 return 0
-            skip = dp(i + 1, holding)
-            pnl = prices[i]
+            skip = dp(i+1, holding)
+            take = prices[i]
             if not holding:
-                pnl = -pnl + dp(i + 1, True)
-            return max(skip, pnl)
+                take = -take + dp(i+1, True)
+            return max(skip, take)
 
         return dp(0, False)

7e0d93b9c8cf85099b0b45a3114234d4b031f73d
Loi Tran <primetimetran@gmail.com>
2025-12-22 02:00:02 +0700

    Time: 4177 ms (17.64%), Space: 42.6 MB (43.88%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -8,21 +8,24 @@ Can be both short and long positions.
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
         n = len(prices)
-
+        # 0 = None
+        # 1 = Buy to open
+        # 2 = Sell to open
         @lru_cache(maxsize=1000*4)
         def dp(i, kount, holding):
             if i == n or kount == k:
-                return 0 if holding is None else -inf
+                return 0 if holding is None else -10**18
             price = prices[i]
             i+=1
             skip = dp(i, kount, holding)
             if holding:
-                sign = 1 if holding == "sell_to_open" else -1
-                pnl_today_included = (sign * price) + dp(i, kount+1, None)
+                sign = 1 if holding == 1 else -1
+                pnl_from_close = sign * price
+                pnl_today_included = pnl_from_close + dp(i, kount+1, None)
             else:
                 pnl_today_included = max(
-                    dp(i, kount, "buy_to_open") + price,
-                    dp(i, kount, "sell_to_open") - price,
+                    price + dp(i, kount, 2),
+                    dp(i, kount, 1) - price,
                 )
             return max(skip, pnl_today_included)
         return dp(0, 0, False)

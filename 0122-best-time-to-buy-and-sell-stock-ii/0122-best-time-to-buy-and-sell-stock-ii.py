025967b4a026a7c288b8167df35b3165ba129991
Loi Tran <primetimetran@gmail.com>
2025-12-21 15:18:01 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,9 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        pro = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                pro += prices[i] - prices[i-1]
-        return pro
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         l = res = 0
+#         for r, p in enumerate(prices):
+#             if prices[l] < p:
+#                 l = r
+#             else:
+#                 res += p - prices[l]
+#         return res

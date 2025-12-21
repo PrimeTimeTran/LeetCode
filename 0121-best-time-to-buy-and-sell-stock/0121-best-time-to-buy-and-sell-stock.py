6db04c2ab2dd11d3e98167bfa1b100488b1b19ee
Loi Tran <primetimetran@gmail.com>
2025-12-21 23:23:25 +0700

    Time: 899 ms (5.05%), Space: 350.5 MB (8.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,3 @@
-from functools import lru_cache
-from math import inf
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
@@ -8,17 +5,18 @@ class Solution:
         @lru_cache(None)
         def dp(i: int, holding: bool) -> int:
             if i == n:
-                return -inf if holding else 0
-
-            price = prices[i]
+                return 0  # always return 0 at the end
 
             if holding:
+                # Either sell today or skip
+                sell = prices[i]  # profit from selling today
                 skip = dp(i + 1, True)
-                sell = price
+                # If we sell, we cannot sell again, so the recursion for future is 0
                 return max(skip, sell)
             else:
+                # Either buy today or skip
+                buy = -prices[i] + dp(i + 1, True)
                 skip = dp(i + 1, False)
-                buy  = -price + dp(i + 1, True)
                 return max(skip, buy)
 
         return dp(0, False)

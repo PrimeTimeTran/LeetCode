78b8211718258e4d0c6d782c157bb0f0f28d1817
Loi Tran <primetimetran@gmail.com>
2025-12-21 15:30:04 +0700

    Time: 216 ms (5.05%), Space: 26.3 MB (99.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans = l = 0
-        for r, p in enumerate(prices):
-            ans = max(ans, p - prices[l])
-            if p < prices[l]: l = r
-        return ans
+        dp = [0, -prices[0]]
+        for price in prices[1:]:
+            new_dp = [-inf, -inf]
+            new_dp[0] = dp[0]
+            new_dp[1] = dp[1]
+            new_dp[1] = max(new_dp[1], -price)
+            new_dp[0] = max(new_dp[0], dp[1] + price)
+            dp = new_dp
+        return dp[0]

88982d51f91f7fe8daced00ac0e7f8d563008695
Loi Tran <primetimetran@gmail.com>
2025-12-22 04:17:58 +0700

    Time: 4076 ms (24.78%), Space: 42.4 MB (44.02%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -20,24 +20,24 @@ Return the maximum of the potential pnl from acting today and skipped.
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
         n = len(prices)
-        # 1 = buy to open
-        # 2 = sell to open
         @lru_cache(1000*4)
-        def dp(i, completed_count, position):
+        # 0 = None
+        # 1 = buy_to_open
+        # 2 = sell_to_open
+        def dp(i, position, completed_count):
             if i == n or completed_count == k:
                 return 0 if position is None else -inf
             price = prices[i]
             i+=1
-            skip = dp(i, completed_count, position)
+            skip = dp(i, position, completed_count)
+            cashflow_realized = price if position == 1 else -price
             if position:
-                sign = 1 if position == 1 else -1
-                buy_or_sell = sign * price
-                pnl_today_included = buy_or_sell + dp(i, completed_count+1, None)
+                realized = price if position == 1 else -price
+                act = realized + dp(i, None, completed_count+1)
             else:
-                pnl_today_included = max(
-                    dp(i, completed_count, 1) - price,
-                    dp(i, completed_count, 2) + price
+                act = max(
+                    dp(i, 1, completed_count) - price,
+                    dp(i, 2, completed_count) + price
                 )
-
-            return max(skip, pnl_today_included)
-        return dp(0, 0, 0)
+            return max(skip, act)
+        return dp(0, None, 0)

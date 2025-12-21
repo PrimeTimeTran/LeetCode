10357bda871b6537ae66b96a751e5e553084219a
Loi Tran <primetimetran@gmail.com>
2025-12-22 01:03:51 +0700

    Time: 31 ms (5.34%), Space: 43.6 MB (5.06%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,28 @@
+'''
+1. Understand
+Given a list of stock prices return the total profit you can make buying and selling.
+You must first buy a stock to sell it.
+
+Pseudocode:
+From first day skip and take that day and then proceed to further days.
+Taking is a relative term, you must sell after buying and buy before selling.
+
+Recurse by moving forward in the list of days and exiting when you've reached the base case(last day).
+Depending on if you're selling or buying, you will ± price.
+'''
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         @lru_cache(None)
-        def dp(i: int, holding: int) -> int:
-            if i == n: return 0
+        def dp(i, holding):
+            if i == n:
+                return 0
+            skip = dp(i+1, holding)
+            price = prices[i]
             if holding:
-                return max(prices[i] + dp(i+1, 0), dp(i+1, 1))
+                sell = price + dp(i+1, False)
+                return max(skip, sell)
             else:
-                return max(-prices[i] + dp(i+1, 1), dp(i+1, 0))
-        return dp(0, 0)
+                buy = -price + dp(i+1, True)
+                return max(skip, buy)
+        return dp(0, False)

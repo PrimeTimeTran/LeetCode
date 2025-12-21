0cf36a9128bb7f4ac33bb0086b9eb09ba0f1499b
Loi Tran <primetimetran@gmail.com>
2025-12-22 00:13:01 +0700

    Time: 945 ms (5.05%), Space: 344.5 MB (8.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-
         @lru_cache(None)
         def dp(i: int, holding: bool) -> int:
             if i == n:
                 return 0
-            if holding:
-                sell = prices[i]        # profit from selling today
-                skip = dp(i + 1, True)
-                return max(skip, sell)
-            else:
-                buy  = -prices[i] + dp(i + 1, True)
-                skip = dp(i + 1, False)
-                return max(skip, buy)
+            skip = dp(i + 1, holding)
+            sign = 1 if holding else -1
+            price = prices[i]
+            pnl = sign * price
+            if not holding:
+                pnl = pnl + dp(i + 1, True)
+            return max(skip, pnl)
 
         return dp(0, False)

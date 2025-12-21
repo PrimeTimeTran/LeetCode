8817af868bac3c7ad27c3a6bdf99ffa476503d33
Loi Tran <primetimetran@gmail.com>
2025-12-22 03:32:38 +0700

    Time: 11 ms (14.43%), Space: 22.1 MB (5.73%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,25 +1,17 @@
-from functools import lru_cache
-from math import inf
-from typing import List, Union
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         @lru_cache(None)
-        def dp(i: int, open_position: Union[str, bool], cooldown):
-            if i == len(prices):
-                return 0 if not open_position else -inf
+        def dp(i: int, holding: bool, cooldown: bool) -> int:
+            if i == len(prices): return 0
             price = prices[i]
-            i+=1
-
+            i += 1
+            skip = dp(i, holding, False)
             if cooldown:
-                return dp(i, open_position, False)
-
-            if open_position:
-                pnl_today_included = price + dp(i, None, True)
-                pnl_today_skipped = dp(i, open_position, False)
+                return skip
+            realized = price if holding else -price
+            if holding:
+                pnl = realized + dp(i, not holding, True)
             else:
-                pnl_today_included  = -price + dp(i, True, False)
-                pnl_today_skipped = dp(i, None, False)
-            return max(pnl_today_included, pnl_today_skipped)
-
-        return dp(0, None, False)
+                pnl = realized + dp(i, not holding, False)
+            return max(skip, pnl)
+        return dp(0, False, False)

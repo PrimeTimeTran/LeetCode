4fe752d9c499b35af2ebd11fe1dceb3f1396157e
Loi Tran <primetimetran@gmail.com>
2025-12-22 01:36:32 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -1,21 +1,30 @@
+'''
+1. Understand
+Use k transactions max to maximum a realized profit.
+Must open and close to make a profit.
+Can be both short and long positions.
+'''
+
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
-        @lru_cache(maxsize=1000*4)
-        def dp(i: int, completed_transactions: int, position: str):
-            if i == len(prices) or completed_transactions == k:
-                return 0 if position == None else -inf
-            p, pnl_today_included = prices[i], -inf
-            i+=1
-            if position:
-                sign = -1 if position == "sell_to_open" else 1
-                pnl_today = sign * p
-                pnl_unrealized = dp(i, completed_transactions + 1, None)
-                pnl_today_included = pnl_today + pnl_unrealized
+        n = len(prices)
+        @lru_cache(None)
+        def dp(i, num, holding):
+            if i == n or num == k:
+                return 0 if holding is None else -inf
+            skip = dp(i+1, num, holding)
+            price = prices[i]
+            num += 1 if holding else 0
+            if holding:
+                sign = -1 if holding == "sell_to_open" else 1
+                pnl_today = sign * price
+                pnl = sign * price
+                pnl_unrealized = dp(i+1, num, None)
+                pnl_today_included = pnl + pnl_unrealized
             else:
                 pnl_today_included = max(
-                    dp(i, completed_transactions, "sell_to_open") + p,
-                    dp(i, completed_transactions, "buy_to_open") - p,
+                    dp(i+1, num, "sell_to_open") + price,
+                    dp(i+1, num, "buy_to_open") - price
                 )
-            pnl_today_skipped = dp(i, completed_transactions, position)
-            return max(pnl_today_skipped, pnl_today_included)
-        return dp(0, 0, None)
+            return max(skip, pnl_today_included)
+        return dp(0, 0, False)

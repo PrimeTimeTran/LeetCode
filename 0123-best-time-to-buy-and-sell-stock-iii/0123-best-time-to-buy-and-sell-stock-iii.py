0c960047a3bcc789df43df269f6601a82ca079b5
Loi Tran <primetimetran@gmail.com>
2025-12-22 01:08:30 +0700

    Time: 1702 ms (13.64%), Space: 600.5 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,43 +1,49 @@
-from functools import lru_cache
-from math import inf
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # @lru_cache(None)
-        # def dp(i: int, completed_count: int, open_position: bool) -> int:
-        #     if i == len(prices) or completed_count == 2:
-        #         return -inf if open_position else 0
-        #     price = prices[i]
-        #     i+=1
-        #     pnl_today_excluded = dp(i, completed_count, open_position)
-        #     if open_position:
-        #         pnl_today_included = price + dp(i, completed_count + 1, False)
-        #     else:
-        #         pnl_today_included = -price + dp(i, completed_count, True)
-        #     return max(pnl_today_excluded, pnl_today_included)
+        n = len(prices)
+        @lru_cache(None)
+        def dp(i: int, count_transactions: int, position_open: bool) -> int:
+            if i == n or count_transactions == 2:
+                return 0
 
-        # return dp(0, 0, False)
+            price = prices[i]
 
-        # if not prices: return 0
-        # # forward traversal, profits record the max profit 
-        # # by the ith day, this is the first transaction
-        profits = []
-        max_profit = 0
-        current_min = prices[0]
-        for price in prices:
-            current_min = min(current_min, price)
-            max_profit = max(max_profit, price - current_min)
-            profits.append(max_profit)
-        
-        # backward traversal, max_profit records the max profit
-        # after the ith day, this is the second transaction 
-        total_max = 0    
-        max_profit = 0
-        current_max = prices[-1]
-        for i in range(len(prices) - 1, -1, -1):
-            current_max = max(current_max, prices[i])
-            max_profit = max(max_profit, current_max - prices[i])
-            total_max = max(total_max, max_profit + profits[i])
-            
-        return total_max
+            # Skip today
+            pnl_today_skipped = dp(i + 1, count_transactions, position_open)
+
+            if position_open:
+                # Option to sell today (if we can)
+                sell = price + dp(i + 1, count_transactions + 1, False)
+                return max(pnl_today_skipped, sell)
+            else:
+                # Option to buy today
+                buy = -price + dp(i + 1, count_transactions, True)
+                return max(pnl_today_skipped, buy)
+
+        return dp(0, 0, False)
+
+
+        # 2. Bottom Up
+        # dp[k][h]: max profit with k transactions, holding h
+        # dp = [[-inf, -inf] for _ in range(3)]
+        # dp[0][0] = 0
+        # dp[0][1] = -prices[0]
+
+        # for price in prices[1:]:
+        #     new_dp = [[-inf, -inf] for _ in range(3)]
+        #     for k in range(3):
+        #         # skip
+        #         new_dp[k][0] = max(new_dp[k][0], dp[k][0])
+        #         new_dp[k][1] = max(new_dp[k][1], dp[k][1])
+
+        #         # buy
+        #         if dp[k][0] != -inf:
+        #             new_dp[k][1] = max(new_dp[k][1], dp[k][0] - price)
+
+        #         # sell
+        #         if k > 0 and dp[k-1][1] != -inf:
+        #             new_dp[k][0] = max(new_dp[k][0], dp[k-1][1] + price)
+
+        #     dp = new_dp
+        # print(dp)
+        # return max(dp[k][0] for k in range(3))

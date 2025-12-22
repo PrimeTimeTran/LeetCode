a88a5272dccba3c43820e0393c7c5e81a666b75d
Loi Tran <primetimetran@gmail.com>
2025-12-22 07:47:59 +0700

    Time: 0 ms (100%), Space: 18.5 MB (98.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,13 +1,7 @@
-'''
-1. Understand
-We want to converge our pointers toward:
-1) The index where a number is strictly less and closest to the target.
-2) The index where a number is strictly more and closest to the target.
-'''
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if not nums: return [-1, -1]
-        l, r = 0, len(nums)-1
+        l, r = 0, len(nums) - 1
         while l < r:
             m = (l+r) // 2
             if nums[m] < target:
@@ -15,15 +9,13 @@ class Solution:
             else:
                 r = m
         firstIndex = l
-        if nums[firstIndex] != target: return [-1, -1]
-        l, r = 0, len(nums)-1
+        if nums[l] != target: return [-1,-1]
+        l, r = 0, len(nums) - 1
         while l < r:
-            m = l + ((r-l+1) // 2)
+            m = (l+r+1) // 2
             if nums[m] > target:
                 r = m - 1
             else:
                 l = m
-            m = (l+r) // 2
-        lastIndex = r
-        return [firstIndex, lastIndex]
-
+        secondIndex = l
+        return [firstIndex, secondIndex]

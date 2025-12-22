f864290203c8f744947025a327c6cb83fe380d2b
Loi Tran <primetimetran@gmail.com>
2025-12-22 12:53:05 +0700

    Time: 0 ms (100%), Space: 17.4 MB (98%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,15 +1,10 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        l, r = 0, len(nums)-1
+        l, r = 0, len(nums) - 1
         while l < r:
-            # M must be greater than l otherwise we'd enter an infinite loop.
             m = (l+r+1) // 2
-            # ✅ We know that there exists at least 1 peak left of 
-            # the current index/midpoint so we can discard the right half.
             if nums[m-1] > nums[m]:
                 r = m - 1
             else:
-                # 🔑 l does not move if l == m
-                # This line is why our algorithm for m is different.
                 l = m
         return l

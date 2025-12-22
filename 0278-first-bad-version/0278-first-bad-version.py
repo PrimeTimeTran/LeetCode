59b0775e74a7a8bb5262b582287c4a7b27f625a2
Loi Tran <primetimetran@gmail.com>
2025-12-22 16:44:51 +0700

    Time: 35 ms (71.64%), Space: 17.4 MB (97.25%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,4 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        return bisect_left(range(n), True, key=isBadVersion)
+        l, r = 0, n
+
+        while l <= r:
+            m = (l+r) // 2
+            if isBadVersion(m):
+                r = m-1
+            else:
+                l = m+1
+        return l

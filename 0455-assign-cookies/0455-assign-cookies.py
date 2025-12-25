8da617bc589078fafb59891c5a9e78541b291ca7
Loi Tran <primetimetran@gmail.com>
2025-12-25 09:34:20 +0700

    Time: 27 ms (24.94%), Space: 19.9 MB (67.52%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        c = i = j = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                c+=1
+                i+=1
+            j+=1
+        return c

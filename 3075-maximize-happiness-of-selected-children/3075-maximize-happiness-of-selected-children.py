9b46d743803ea47ebef232ea24652546a0f3400f
Loi Tran <primetimetran@gmail.com>
2025-12-26 04:58:16 +0700

    Time: 841 ms (39.19%), Space: 44.3 MB (57.43%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -1,9 +1,19 @@
+# class Solution:
+#     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+#         res, n = [], 0
+#         happiness.sort(reverse=True)
+#         for i, h in enumerate(happiness):
+#             if i < k:
+#                 res.append(h - n if h - n > 0 else 0)
+#                 n+=1
+#         return sum(res)
+
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        res, n = [], 0
         happiness.sort(reverse=True)
-        for i, h in enumerate(happiness):
-            if i < k:
-                res.append(h - n if h - n > 0 else 0)
-                n+=1
-        return sum(res)
+        res = 0
+
+        for i in range(k):
+            res += max(happiness[i] - i, 0)
+
+        return res

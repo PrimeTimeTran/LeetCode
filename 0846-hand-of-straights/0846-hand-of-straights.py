0aeaf7cd368fe9512f9a43000503fb7b4d62bf36
Loi Tran <primetimetran@gmail.com>
2025-12-26 18:59:35 +0700

    Time: 31 ms (77.58%), Space: 19.5 MB (17.92%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,19 +1,16 @@
+'''
+This is a greedy algorithm that always forms sequences starting from the smallest available card, which is a forced and safe choice.
+Each copy of the smallest card starts a straight, so I must consume the same number of copies from every consecutive value to complete those straights.
+'''
 class Solution(object):
     def isNStraightHand(self, hand, groupSize):
-        if len(hand) % groupSize != 0:
-            return False
-
-        # Frequency map using Counter
+        if len(hand) % groupSize != 0: return False
         count = Counter(hand)
-
-        # Sorted keys
         sorted_keys = sorted(count.keys())
-
         for key in sorted_keys:
             if count[key] > 0:
                 for i in range(1, groupSize):
-                    if count[key + i] < count[key]:
+                    if count[key+i] < count[key]:
                         return False
-                    count[key + i] -= count[key]
+                    count[key+i] -= count[key]
         return True
-        

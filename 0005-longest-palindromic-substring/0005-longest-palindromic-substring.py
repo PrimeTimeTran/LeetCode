f7a54792f51d3e6b7a4abb99210d027ff32b6be0
Loi Tran <primetimetran@gmail.com>
2025-12-26 18:05:16 +0700

    Time: 9476 ms (5.01%), Space: 17.3 MB (98.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Step 1: Transform the string
-        T = '#'.join('^{}$'.format(s))  # add sentinels
-        n = len(T)
-        P = [0] * n  # radius array
-        C = R = 0    # center and right edge of current rightmost palindrome
-
-        for i in range(1, n-1):
-            mirr = 2*C - i  # mirror of i around C
-
-            if i < R:
-                P[i] = min(R - i, P[mirr])
-
-            # expand around i
-            while T[i + P[i] + 1] == T[i - P[i] - 1]:
-                P[i] += 1
-
-            # update center and right boundary
-            if i + P[i] > R:
-                C, R = i, i + P[i]
-
-        # Find the maximum element in P
-        max_len, center_index = max((n, i) for i, n in enumerate(P))
-        start = (center_index - max_len)//2  # map back to original string indices
-        return s[start: start + max_len]
+        n, res = len(s), s[0]
+        for l in range(n-1):
+            for r in range(l+1, n):
+                substr, len_substr = s[l:r+1], r - l + 1
+                is_palindrome = substr == substr[::-1]
+                is_new_max = len_substr > len(res)
+                if is_palindrome and is_new_max:
+                    res = substr
+        return res

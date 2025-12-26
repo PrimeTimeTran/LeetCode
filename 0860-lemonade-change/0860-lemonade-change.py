2621447e5b66858e093d487f0c3a3b5bca7ccd30
Loi Tran <primetimetran@gmail.com>
2025-12-27 06:16:21 +0700

    Time: 7 ms (29.7%), Space: 21.4 MB (90.03%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,21 +1,22 @@
 class Solution(object):
     def lemonadeChange(self, bills):
-        c5 = c10 = 0
+        n5 = n10 = 0
         for b in bills:
             if b == 5:
-                c5 += 1
+                n5 += 1
             elif b == 10:
-                if c5 > 0:
-                    c5 -= 1
+                if n5 > 0:
+                    n10 += 1
+                    n5 -= 1
                 else:
                     return False
-                c10 += 1
             else:
-                if c10 > 0 and c5 > 0:
-                    c10 -= 1
-                    c5 -= 1
-                elif c5 > 2:
-                    c5 -= 3
+                if n10 > 0 and n5 > 0:
+                    n10 -= 1
+                    n5 -= 1
+                elif n5 > 2:
+                    n5 -= 3
                 else:
                     return False
         return True
+

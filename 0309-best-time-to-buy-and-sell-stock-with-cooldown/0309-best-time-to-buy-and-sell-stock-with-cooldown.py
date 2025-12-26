1d4c171829c2dcd3586de3730be73a5e9ae48ad0
Loi Tran <primetimetran@gmail.com>
2025-12-26 10:41:41 +0700

    Time: 7 ms (41.72%), Space: 22 MB (6.02%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,17 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
         @lru_cache(None)
-        def dp(i: int, holding: bool, cooldown: bool) -> int:
-            if i == len(prices): return 0
-            price = prices[i]
-            i += 1
-            skip = dp(i, holding, False)
-            if cooldown:
-                return skip
-            # If holding, sell, credit
-            # If not holding, buy, debiting
-            realized = price if holding else -price
-            cooldown = holding
-            pnl = realized + dp(i, not holding, cooldown)
-            return max(skip, pnl)
+        def dp(i: int, is_holding: bool, is_cooldown: bool) -> int:
+            if i == n: return 0
+            i, price = i + 1, prices[i]
+            skip = dp(i, is_holding, False)
+            if is_cooldown: return skip
+            pnl = price if is_holding else -price
+            # is_cooldown = is_holding
+            realized = pnl + dp(i, not is_holding, is_holding)
+            return max(skip, realized)
         return dp(0, False, False)

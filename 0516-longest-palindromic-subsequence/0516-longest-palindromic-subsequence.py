8ef00c5e676dd9fa456836f1e983215e3cb14336
Loi Tran <primetimetran@gmail.com>
2025-12-26 09:20:10 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -11,8 +11,10 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         @lru_cache(None)
         def dp(l, r):
-            if l == len(s) or r < 0:
+            if l > r:
                 return 0
+            if l == r:
+                return 1
             if s[l] == s[r]:
                 return 1 + dp(l+1, r-1)
             return max(dp(l+1, r), dp(l, r-1))

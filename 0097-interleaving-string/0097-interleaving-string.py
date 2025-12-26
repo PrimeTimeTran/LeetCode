551b667510eadbf05610c5fa9be73e12b379874d
Loi Tran <primetimetran@gmail.com>
2025-12-26 09:23:23 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -10,17 +10,14 @@ Space: O()
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         m, n, o = len(s1), len(s2), len(s3)
-        if m+n != o:
+        if m + n != o:
             return False
-        @lru_cache(None)
         def dp(i, j):
-            if i == m and j == n:
-                return True
-            k = i + j
-            ans = False
+            if i == m and j == n: return True
+            res, k = False, i + j
             if i < m and s1[i] == s3[k]:
-                ans = ans or dp(i+1, j)
+                res = res or dp(i+1, j)
             if j < n and s2[j] == s3[k]:
-                ans = ans or dp(i, j+1)
-            return ans
+                res = res or dp(i, j+1)
+            return res
         return dp(0, 0)

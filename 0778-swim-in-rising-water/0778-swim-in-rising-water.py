4916ee6217ad7bdc6ea46685bdb0d44c9dffbba1
Loi Tran <primetimetran@gmail.com>
2025-12-27 13:04:10 +0700

    Time: 47 ms (25.49%), Space: 18.1 MB (92.49%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,45 +1,14 @@
-'''
-1. Understand
-BFS solution. 
-Heap
-We want to create a heap which is a list of cells to check, and then check if we've reached the bottom right cell. If that's case, then we jsut need to return the maximum elevation/time in our path to the bottom right cell.
-
-2. Diagram
-
-
-[0, 5]
-[1, 4]
-
-[0, 5]
-[1, 1]
-
-3. Pseudocode
-    1. Define an answer variable and update during our BFS
-    2. Iterate our queue while we havbe it. 
-    3. Check if we've reached the bottom right cell, if so return the max we've encountered so far.
-    4. For each cell we traverse we also need to traverse neighbors if htey're inbound.
-    5. Ensure we don't backtrack over previously visited cells.
-4. Code
-5. Big O 
-
-Peter's
-O(n^2 log n)
-O(n^2 * log (n^2)) 
-
-Time:   O(logn^2 * ) where n is the length of the row or column
-Space:  O()
-'''
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        ans, seen, q, n = 0, set(), [[grid[0][0], 0, 0]], len(grid)
+        seen, t, n, q = set(), 0, len(grid), [[grid[0][0], 0, 0]]
         while q:
-            elevation, r, c = heappop(q)                            # O(logn)
+            elevation, r, c = heappop(q)
             if (r,c) in seen: continue
             seen.add((r,c))
-            ans = max(ans, elevation)
+            t = max(t, elevation)
             if r == c == n-1:
-                return ans
-            for dx, dy in [r+1, c], [r-1, c], [r, c+1], [r, c-1]:
-                inbounds = 0 <= dx < n and 0 <= dy < n
+                return t
+            for dr, dc in [r+1, c], [r-1, c], [r,c+1], [r,c-1]:
+                inbounds = 0 <= dr < n and 0 <= dc < n
                 if inbounds:
-                    heappush(q, [grid[dx][dy], dx, dy])             # O(logn)
+                    heappush(q, [grid[dr][dc], dr, dc])

dd3d394cdd56ce44f8d30500d521fda77338bdf6
Loi Tran <primetimetran@gmail.com>
2025-12-28 04:32:40 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,15 +1,16 @@
+'''
+“A natural alternative is greedy — for example, always picking the highest-value event first. However, that fails because the highest-value event may span a long time range and prevent us from selecting a second event, even when two smaller-value non-overlapping events together yield a higher total. Since local choices affect future feasibility, the problem lacks the greedy-choice property, which is why a DP approach is appropriate.”
+'''
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        n = len(events)
         events.sort()
-        starts = [s for s, _, _ in events]
-        @lru_cache(None)
-        def dp(i, taken):
-            if taken == 2 or i == n:
+        n, starts = len(events), [s for s, _, _ in events]
+        def dp(i, pluck):
+            if i == n or pluck == 2:
                 return 0
-            _, e, v = events[i]
+            s, e, v = events[i]
             j = bisect_right(starts, e)
-            skip = dp(i + 1, taken)
-            take = v + dp(j, taken + 1)
+            skip = dp(i+1, pluck)
+            take = v + dp(j, pluck+1)
             return max(skip, take)
         return dp(0, 0)

4ca1392cdfd8d0e090f2803fb4b8ce50f9ab2266
Loi Tran <primetimetran@gmail.com>
2025-12-27 09:41:36 +0700

    Time: 187 ms (17.52%), Space: 51.4 MB (19.76%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,23 +1,14 @@
-'''
-
-Sort intervals by start time.
-Iterate through them comparing previous end with current start.
-If start > prev
-
-
-'''
-
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort()
         res = 0 
-        p = intervals[0][1]
+        prev_end = intervals[0][1]
         for i in range(1, len(intervals)):
-            s, e = intervals[i]
-            if s >= p:
-                p = e
+            start, new_end = intervals[i]
+            if start >= prev_end:
+                prev_end = new_end
             else:
-                res+=1
-                p = min(p, e)
+                res += 1
+                prev_end = min(prev_end, new_end)
         
         return res

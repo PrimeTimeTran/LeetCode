8b643f9822e163fdcd9e1a19d129f685bb189789
Loi Tran <primetimetran@gmail.com>
2025-12-27 14:20:56 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,28 +1,17 @@
 '''
 1. Understand
 Greedy.
-
 5. Big O
-Time:   O()
-Space:  O()
+
+Time:   O(nlogn + n) where n is the length of points
+Space:  O(1) for two pointers
 '''
-# class Solution:
-#     def findMinArrowShots(self, points: List[List[int]]) -> int:
-#         n, c = len(points), 1
-#         if n == 0: return 0
-#         points.sort(key = lambda x: x[1])
-#         cur_end = points[0][1]
-#         for p in points:
-#             if cur_end < p[0]:
-#                 c += 1
-#                 cur_end = p[1]
-#         return c
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key = lambda x: x[1])
+        points.sort()
         c, cur_end = 1, points[0][1]
-        for p in points:
-            if cur_end < p[0]:
-                cur_end = p[1]
+        for start, end in points:
+            if cur_end < start:
                 c+=1
+                cur_end = end
         return c

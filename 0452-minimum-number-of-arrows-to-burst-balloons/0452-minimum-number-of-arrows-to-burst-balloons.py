af653ae9fa42e8362a91c9d41e28504570d66c7f
Loi Tran <primetimetran@gmail.com>
2025-12-28 04:25:01 +0700

    Time: 68 ms (77.71%), Space: 50 MB (90.13%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -8,7 +8,7 @@ Space:  O(1) for two pointers
 '''
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()
+        points.sort(key = lambda x: x[1])
         c, cur_end = 1, points[0][1]
         for start, end in points:
             if cur_end < start:

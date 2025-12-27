bd6220cd5cb9b07208a03f2c8eabb6cad3a487ed
Loi Tran <primetimetran@gmail.com>
2025-12-27 09:21:09 +0700

    Time: 71 ms (73.35%), Space: 49.9 MB (92.76%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,13 +1,29 @@
+'''
+1. Understand
+Greedy.
+
+5. Big O
+Time:   O()
+Space:  O()
+'''
+# class Solution:
+#     def findMinArrowShots(self, points: List[List[int]]) -> int:
+#         n, c = len(points), 1
+#         if n == 0: return 0
+#         points.sort(key = lambda x: x[1])
+#         cur_end = points[0][1]
+#         for p in points:
+#             if cur_end < p[0]:
+#                 c += 1
+#                 cur_end = p[1]
+#         return c
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        c, n = 1, len(points)
         points.sort(key = lambda x: x[1])
-        n, count = len(points), 1
-        if n == 0: return 0
-        curr = points[0]
-        
-        for i in range(n):
-            if curr[1] < points[i][0]:
-                count += 1
-                curr = points[i]
-                
-        return count  
+        cur_end = points[0][1]
+        for p in points:
+            if cur_end < p[0]:
+                cur_end = p[1]
+                c+=1
+        return c

09f1d1c4f263a82357d45bd6da05c2ef45937d1d
Loi Tran <primetimetran@gmail.com>
2025-12-27 09:33:56 +0700

    Time: 50 ms (17.1%), Space: 51.9 MB (91.15%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,7 +1,15 @@
+'''
+1. Understand
+Greedy.
+
+5. Big O
+Time:   O(n)
+Space:  O(1)
+'''
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         a = b = c = 0
-        for x, y, z in triplets:
-            if x <= target[0] and y <= target[1] and z <= target[2]:
-                a, b, c = max(a, x), max(b, y), max(c, z)
-        return [a, b, c] == target
+        for x in triplets:
+            if x[0] <= target[0] and x[1] <= target[1] and x[2] <= target[2]:
+                a, b, c = max(a, x[0]), max(b, x[1]), max(c, x[2])
+        return [a,b,c] == target

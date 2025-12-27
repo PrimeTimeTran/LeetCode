daecc41b8a79c9eb43cf0710102e825b2b645bd9
Loi Tran <primetimetran@gmail.com>
2025-12-27 14:24:46 +0700

    Time: 3 ms (65.28%), Space: 18.1 MB (85.35%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,10 +1,9 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         events = []
-        for p, start, end in trips:
-            events.append((start, p))   # pick up passengers
-            events.append((end, -p))    # drop off passengers
-        # Sort by time; drop-offs happen before pick-ups at same time
+        for n, f, t in trips:
+            events.append([f, n])
+            events.append([t, -n])
         events.sort()
         cur = 0
         for _, delta in events:

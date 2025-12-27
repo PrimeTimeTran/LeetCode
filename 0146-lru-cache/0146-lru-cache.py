38464369962ed585f780c775fef09f6fdd7b2f26
Loi Tran <primetimetran@gmail.com>
2025-12-27 15:53:05 +0700

    Time: 141 ms (51.95%), Space: 77.3 MB (99.4%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,40 +1,18 @@
-class ListNode:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.next = None
-        self.prev = None
-
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {}
-        self.left, self.right = ListNode(0,0), ListNode(0,0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.cache = OrderedDict()
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
-    def insert(self, n) -> int:
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = n
-        n.prev, n.next = prev, nxt
-    def remove(self, n) -> int:
-        prev, nxt = n.prev, n.next
-        prev.next, nxt.prev = nxt, prev
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)   # mark as most recently used
+        return self.cache[key]
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = ListNode(key, value)
-        self.insert(self.cache[key])
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
         if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            self.cache.popitem(last=False)  # remove LRU

d1b65d4a1614e5df57bbdfe61f3193fe606fa749
Loi Tran <primetimetran@gmail.com>
2025-12-27 12:33:17 +0700

    Time: 500 ms (24.07%), Space: 157.8 MB (7.06%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,17 +1,14 @@
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        events.sort()
         n = len(events)
-        # extract start times for binary search
+        events.sort()
         starts = [s for s, e, v in events]
         @lru_cache(None)
-        def dp(i: int, taken: int) -> int:
-            if i == n or taken == 2: return 0
-            # option 1: skip event i
-            best = dp(i + 1, taken)
-            # option 2: take event i
+        def dp(i, total):
+            if total == 2 or i == n: return 0
             s, e, v = events[i]
-            j = bisect_right(starts, e)  # first event with start > e
-            best = max(best, v + dp(j, taken + 1))
-            return best
+            j = bisect_right(starts, e)
+            skip = dp(i+1, total)
+            take = v + dp(j, total+1)
+            return max(skip, take)
         return dp(0, 0)

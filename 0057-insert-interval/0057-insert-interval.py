7ccaf2de61acdab796070d65588dee2dc8f9e204
Loi Tran <primetimetran@gmail.com>
2025-12-28 04:29:29 +0700

    Time: 3 ms (51.45%), Space: 19.2 MB (90.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -29,6 +29,7 @@ Space:  O(n)
 class Solution:
     def insert(self, intervals: List[List[int]], n: List[int]) -> List[List[int]]:
         res = []
+        intervals.sort()
         for i, cur in enumerate(intervals):
             if n[1] < cur[0]:
                 res.append(n)
@@ -36,6 +37,6 @@ class Solution:
             elif n[0] > cur[1]:
                 res.append(cur)
             else:
-                n = [min(cur[0], n[0]), max(cur[1], n[1])]
+                n = [min(n[0], cur[0]), max(n[1], cur[1])]
         res.append(n)
         return res

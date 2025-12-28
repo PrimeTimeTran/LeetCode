a05151c219b2f5a0c73341b4f4b0a040c4fb476f
Loi Tran <primetimetran@gmail.com>
2025-12-28 07:08:34 +0700

    Time: 8 ms (63.75%), Space: 19.3 MB (39.11%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -2,24 +2,20 @@
 Create G and BFS on PQ with cost, city, num of remaining stops.
 Guard cycle using a hashmap which contains number of stops remaining.
 If the number of stops in the hashmap is greater than stops, continue; skipping this pq item's neighbors.
+
+seen, q, g = {}, [[0,src,k+1]], defaultdict(list)
 '''
+
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        g = defaultdict(list)
-        
-        for u,v,w in flights:
-            g[u].append((v,w))
-            
-        pq = [[0, src, k+1]]
-        seen = {}
-        while pq:
-            c, u, stops = heappop(pq)
-            if u == dst: return c
-            
+        g, q, seen = defaultdict(list), [[0,src,k+1]], {}
+        for u,v,w in flights: g[u].append([v,w])
+        while q:
+            accum_cost, u, stops = heappop(q)
+            if u == dst: return accum_cost
             if u in seen and seen[u] >= stops: continue
-            seen[u] = stops-1
-            
+            seen[u] = stops - 1
             if stops:
-                for v, w in g[u]:
-                    heappush(pq, (c+w, v, stops-1))
+                for des, cost in g[u]:
+                    heappush(q, [cost + accum_cost, des, seen[u]])
         return -1

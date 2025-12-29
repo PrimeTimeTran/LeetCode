10f0c9eb033a03a169616289caf8510e5d9cfa9d
Loi Tran <primetimetran@gmail.com>
2025-12-29 10:28:52 +0700

    Time: 7 ms (17.24%), Space: 18 MB (89.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,11 +1,10 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        events = []
-        for n, f, t in trips:
-            events.append([f, n])
-            events.append([t, -n])
+        cur, events = 0, []
+        for n, start, end in trips:
+            events.append([start, n])
+            events.append([end, -n])
         events.sort()
-        cur = 0
         for _, delta in events:
             cur += delta
             if cur > capacity:

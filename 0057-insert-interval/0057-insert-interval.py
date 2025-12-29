5cec658f19139e26f486133087b9516cba992b8c
Loi Tran <primetimetran@gmail.com>
2025-12-29 10:23:34 +0700

    Time: 8 ms (7.36%), Space: 19.1 MB (92.54%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -28,13 +28,13 @@ Space:  O(n)
 '''
 class Solution:
     def insert(self, intervals: List[List[int]], n: List[int]) -> List[List[int]]:
-        res = []
         intervals.sort()
+        res = []
         for i, cur in enumerate(intervals):
             if n[1] < cur[0]:
                 res.append(n)
                 return res + intervals[i:]
-            elif n[0] > cur[1]:
+            elif cur[1] < n[0]:
                 res.append(cur)
             else:
                 n = [min(n[0], cur[0]), max(n[1], cur[1])]

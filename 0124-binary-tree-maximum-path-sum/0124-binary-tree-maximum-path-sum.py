7c9477680a382be37cac3e55e88dad2da5385de9
Loi Tran <primetimetran@gmail.com>
2025-12-30 14:55:45 +0700

    Time: 14 ms (66.58%), Space: 21.9 MB (91.82%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -3,7 +3,7 @@ class Solution:
         self.res = root.val
         def dfs(n):
             if not n: return 0
-            l, r = max(dfs(n.left), 0), max(dfs(n.right), 0)
+            l, r = max(0, dfs(n.left)), max(0, dfs(n.right))
             self.res = max(self.res, n.val + l + r)
             return n.val + max(l, r)
         dfs(root)

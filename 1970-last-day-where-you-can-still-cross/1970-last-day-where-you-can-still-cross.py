e7559c7921d9bba1d043118768b18f11e693e0aa
Loi Tran <primetimetran@gmail.com>
2026-01-01 06:47:17 +0700

    Time: 718 ms (67.58%), Space: 31.5 MB (5.49%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -25,19 +25,16 @@ class Solution:
             for i in range(day):
                 r, c = cells[i]
                 board[r-1][c-1] = 1
-
             def dfs(r, c):
                 if r == m - 1: return True
                 board[r][c] = 1
                 for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
                     nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == 0:
-                        if dfs(nr, nc):
-                            return True
-                return False
+                    inbounds = 0 <= nr < m and 0 <= nc < n
+                    if inbounds and board[nr][nc] == 0:
+                        if dfs(nr, nc): return True
             for c in range(n):
-                if board[0][c] == 0 and dfs(0, c):
-                    return True
+                if board[0][c] == 0 and dfs(0, c): return True
             return False
         l, r = 0, len(cells)
         while l < r:

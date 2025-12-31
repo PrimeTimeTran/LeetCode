af7452c65c23ee706ed3d6ba010c73ae53c88bdd
Loi Tran <primetimetran@gmail.com>
2025-12-31 19:02:31 +0700

    Time: 235 ms (5.08%), Space: 23 MB (18.16%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,22 +1,17 @@
-'''
-DFS land cells. In recursive calls return 1 for border and water cells. If seen return 0.
-Return sumed value of first land cell found.
-'''
 class Solution:
-    def islandPerimeter(self, g: List[List[int]]) -> int:
-        m,n = len(g), len(g[0])
-        seen = set()        
-        def dfs(r,c):
-            o = r < 0 or c < 0 or r == m or c == n or g[r][c] == 0
-            if o: return 1
-            if (r,c) in seen: return 0
-            seen.add((r,c))
-            
-            return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
-        
-        for r in range(m):
-            for c in range(n):
-                if g[r][c]:
-                    return dfs(r,c)
-        
-        
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        seen, self.ans, R, C = set(), 0, len(grid), len(grid[0])
+        def dfs(r, c):
+            if (r,c) in seen: return
+            safe = 0 <= r < R and 0 <= c < C
+            if not safe or grid[r][c] == 0:
+                self.ans += 1
+            else:
+                seen.add((r,c))
+                for dr, dc in [r+1,c], [r-1,c], [r,c+1], [r,c-1]:
+                    dfs(dr, dc)
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 1 and (r,c) not in seen:
+                    dfs(r, c)
+        return self.ans

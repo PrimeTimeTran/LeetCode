1e73cca8bde70c8b6f55bb0576d3d76757706a3d
Loi Tran <primetimetran@gmail.com>
2025-12-31 09:34:22 +0700

    Time: 36 ms (88.54%), Space: 18.1 MB (7.83%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,16 @@
+# class Solution:
+#     def combinationSum4(self, nums: List[int], target: int) -> int:
+#         dp = {0: 1}
+#         for t in range(1, target+1):
+#             dp[t] = 0
+#             for n in nums:
+#                 dp[t] += dp.get(t-n, 0)
+#         return dp[target]
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        @lru_cache(None)
+        def dfs(rem):
+            if rem < 0: return 0
+            if rem == 0: return 1
+            return sum(dfs(rem - n) for n in nums)
+        return dfs(target)

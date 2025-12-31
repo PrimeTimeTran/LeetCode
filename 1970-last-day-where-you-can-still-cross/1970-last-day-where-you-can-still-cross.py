a672c4513be62c5e0723cd8507d75392b4cbf7b1
Loi Tran <primetimetran@gmail.com>
2026-01-01 06:49:31 +0700

    Time: 695 ms (69.23%), Space: 32.7 MB (5.49%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -1,23 +1,3 @@
-# class Solution:
-#     def latestDayToCross(self, m: int, n: int, cells: List[List[int]]) -> int:
-#         res, board = 0, [[0] * n for _ in range(m)]
-#         def dfs(r, c):
-#             if r == m-1: return True
-#             reached = False
-#             for dr, dc in [[r+1,c], [r-1,c], [r,c+1],[r,c-1]]:
-#                 inbounds = 0 <= dr < m and 0 <= dc < n
-#                 if inbounds and board[dr][dc] == 0:
-#                     tmp = board[dr][dc]
-#                     board[dr][dc] = '.'
-#                     reached = reached or dfs(dr, dc)
-#                     board[dr][dc] = tmp
-#             return reached
-#         for i, cur in enumerate(cells):
-#             for c in range(n):
-#                 if board[0][c] == 0 and dfs(0, c):
-#                     res = max(res, i)
-#             board[cur[0]-1][cur[1]-1] = 1
-#         return res
 class Solution:
     def latestDayToCross(self, m: int, n: int, cells: List[List[int]]) -> int:
         def can_cross(day):
@@ -36,6 +16,7 @@ class Solution:
             for c in range(n):
                 if board[0][c] == 0 and dfs(0, c): return True
             return False
+        
         l, r = 0, len(cells)
         while l < r:
             mid = (l + r + 1) // 2

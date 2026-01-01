efa375fce7db37a50b22a79ba36a41aa8a912af2
Loi Tran <primetimetran@gmail.com>
2026-01-01 21:59:59 +0700

    Time: 47 ms (27.03%), Space: 17.3 MB (99.99%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,8 +1,10 @@
 class Solution:
-    def cloneGraph(self, n: Optional['Node'], seen = {}) -> Optional['Node']:
+    def cloneGraph(self, n: Optional['Node'], seen=None) -> Optional['Node']:
         if not n: return None
+        if seen is None:
+            seen = {}
         if n in seen: return seen[n]
         seen[n] = Node(n.val)
         for nei in n.neighbors:
-            seen[n].neighbors.append(self.cloneGraph(nei))
+            seen[n].neighbors.append(self.cloneGraph(nei, seen))
         return seen[n]

c1df4010b0337e580d9826385bb158161a82e0c4
Loi Tran <primetimetran@gmail.com>
2026-01-01 20:26:48 +0700

    Time: 43 ms (14.14%), Space: 17.1 MB (94.67%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -4,11 +4,10 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         l, r = 0, n
-
-        while l <= r:
-            m = (l+r) // 2
+        while l < r:
+            m = (l+r)//2
             if isBadVersion(m):
-                r = m-1
+                r = m
             else:
-                l = m+1
+                l = m + 1
         return l

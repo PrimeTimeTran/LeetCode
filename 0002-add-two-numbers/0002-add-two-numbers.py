044c6d573cf59b66651368d5b2c61415b91b8269
Loi Tran <primetimetran@gmail.com>
2026-01-01 19:12:43 +0700

    Time: 11 ms (10.39%), Space: 17.4 MB (86.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -11,8 +11,7 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         def tolist(num):
             n = ListNode(num % 10)
-            if num > 9:
-                n.next = tolist(num // 10)
+            if num > 9: n.next = tolist(num // 10)
             return n
         def toint(num):
             return num.val + 10 * toint(num.next) if num else 0

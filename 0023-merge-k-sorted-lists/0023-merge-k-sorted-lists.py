e8f9b899c509fe7bddabd242956081ed45a700e7
Loi Tran <primetimetran@gmail.com>
2026-01-01 20:23:02 +0700

    Time: 7 ms (90.58%), Space: 19.3 MB (98.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,28 +1,24 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
-            return None
+        if not lists: return None
         while len(lists) > 1:
             tomerge = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                tomerge.append(self.merge(l1, l2))
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                tomerge.append(self.mergeTwoLists(l1, l2))
             lists = tomerge
         return lists[0]
 
-    def merge(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        head = ListNode(0, None)
-        tail = head
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        tail = head = ListNode(0, None)
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                head.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                head.next = l2
                 l2 = l2.next
-            tail = tail.next
-        tail.next = l1 or l2
-        return head.next
+            head = head.next
+        head.next = l1 or l2
+        return tail.next

ff415f9bd936fcfc673e7dc23ec29906416ffa06
Loi Tran <primetimetran@gmail.com>
2026-01-01 18:53:36 +0700

    Time: 7 ms (22.11%), Space: 21.9 MB (93.64%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -6,12 +6,11 @@ class Solution:
             f = f.next.next
         prev = None
         while s:
-            nxt = s.next
-            s.next = prev
-            prev = s
-            s = nxt
+            nxt, s.next = s.next, prev
+            prev, s = s, nxt
         l1, reversed_list = head, prev
         while l1 and reversed_list:
             tmp1, tmp2 = l1.next, reversed_list.next
             l1.next, reversed_list.next = reversed_list, tmp1
             l1, reversed_list = tmp1, tmp2
+

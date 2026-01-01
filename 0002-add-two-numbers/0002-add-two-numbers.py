b74726393fd5aca5bd078bbc30528fc837a1a11c
Loi Tran <primetimetran@gmail.com>
2026-01-01 19:11:15 +0700

    Time: 7 ms (32.78%), Space: 17.7 MB (78.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,16 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 '''
-1. Understand
-
+243+564 = 807
+342+465 = 807
 '''
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def toint(n):
-            return n.val + 10 * toint(n.next) if n else 0
-        def tolist(n):
-            node = ListNode(n % 10)
-            if n > 9:
-                node.next = tolist(n // 10)
-            return node
-
-        return tolist(toint(l1)+toint(l2))
+        def tolist(num):
+            n = ListNode(num % 10)
+            if num > 9:
+                n.next = tolist(num // 10)
+            return n
+        def toint(num):
+            return num.val + 10 * toint(num.next) if num else 0
+        return tolist(toint(l1) + toint(l2))

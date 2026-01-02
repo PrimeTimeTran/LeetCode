af8e17eba6415c00cbab7a8b7c69313a54003813
Loi Tran <primetimetran@gmail.com>
2026-01-02 16:20:38 +0700

    Time: 37 ms (20.73%), Space: 17.9 MB (90.92%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -1,30 +1,20 @@
-'''
-1. Understand
-We want the k-th smallest absolute difference among all pairs:
-
-Question:
-“What is the k-th smallest distance?”
-Better Question:
-“How many pairs have distance ≤ D?”
-'''
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
         nums.sort()
         n = len(nums)
-
         def count_pairs(D):
             j = count = 0
             for i in range(n):
                 while j < n and nums[j] - nums[i] <= D:
-                    j += 1
+                    j+=1
                 count += j - i - 1
             return count
 
         l, r = 0, nums[-1] - nums[0]
         while l < r:
-            m = (l + r) // 2
+            m = (l+r) // 2
             if count_pairs(m) >= k:
-                r = m
+                r = m 
             else:
                 l = m + 1
-        return r
+        return l

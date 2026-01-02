7b76e75942fe5c7ffaa73861fb990ce690a35afc
Loi Tran <primetimetran@gmail.com>
2026-01-02 16:13:19 +0700

    Time: 0 ms (100%), Space: 17.6 MB (93.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,11 +1,11 @@
 class Solution:
-    def smaller_first(self, n1, n2):
+    def setShorterLengthInvariant(self, n1, n2):
         m, n = len(n1), len(n2)
         if m > n:
             n1, n2, m, n = n2, n1, n, m
         return (n1, n2, m, n)
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        l1, l2, m, n = self.smaller_first(nums1, nums2)
+        l1, l2, m, n = self.setShorterLengthInvariant(nums1, nums2)
         l, r = 0, m
         midex = (m+n+1) // 2
         while l <= r:
@@ -15,12 +15,12 @@ class Solution:
             l2_max = l2[p2-1] if p2 > 0 else -inf
             r1_min = l1[p1] if p1 < m else inf
             r2_min = l2[p2] if p2 < n else inf
-            if l1_max <= r2_min and l2_max < r1_min:
+            if l1_max <= r2_min and l2_max <= r1_min:
                 lefts_max = max(l1_max, l2_max)
                 rights_min = min(r1_min, r2_min)
-                if (m + n) % 2:
+                if (m+n) % 2:
                     return lefts_max
-                return (lefts_max+rights_min) / 2
+                return (lefts_max + rights_min) / 2
             elif l1_max > r2_min:
                 r = p1 - 1
             else:

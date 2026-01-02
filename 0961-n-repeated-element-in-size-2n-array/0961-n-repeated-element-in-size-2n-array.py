f92c8c2949e6930b510280f7e2b5e83a4863b028
Loi Tran <primetimetran@gmail.com>
2026-01-02 12:49:12 +0700

    Time: 5 ms (50.23%), Space: 18.9 MB (23.01%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -3,5 +3,4 @@ class Solution:
         n = len(nums) // 2
         c = Counter(nums)
         for key, val in c.items():
-            if val == n:
-                return key
+            if val == n: return key

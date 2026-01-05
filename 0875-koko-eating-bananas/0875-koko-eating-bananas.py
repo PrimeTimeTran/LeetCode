81f0df005bc1e06fc7d6ca0be64679e9d43e988d
Loi Tran <primetimetran@gmail.com>
2026-01-05 09:46:18 +0700

    Time: 163 ms (42.67%), Space: 18.8 MB (79.74%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -2,7 +2,7 @@ class Solution:
     def minEatingSpeed(self, piles, H):
         l, r = 0, max(piles)
         while l + 1 < r:
-            mid = (l + r)//2
+            mid = (l + r) // 2
             if sum(ceil(i/mid) for i in piles) > H:
                 l = mid
             else:

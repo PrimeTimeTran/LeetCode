145cf9925e840b2734b7e93bbc422d4ff67946b2
Loi Tran <primetimetran@gmail.com>
2026-01-06 14:20:51 +0700

    Time: 207 ms (32.5%), Space: 21.9 MB (91.22%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,17 +1,15 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], day_limit: int) -> int:
-        l, r = max(weights), sum(weights)
         def is_feasible(cap, load = 0, days = 1):
             for pkg_weight in weights:
-                if load + pkg_weight > cap:
-                    days+=1
-                    load = 0
+                if load + pkg_weight > cap: days+=1; load = 0
                 load += pkg_weight
             return days <= day_limit
+        l, r = max(weights), sum(weights)
         while l < r:
             m = (l+r) // 2
             if is_feasible(m):
                 r = m
             else:
                 l = m + 1
-        return l
+        return r

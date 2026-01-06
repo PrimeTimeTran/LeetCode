55221cc44bb74841d1bacb922ad52e57f9a15510
Loi Tran <primetimetran@gmail.com>
2026-01-06 23:38:13 +0700

    Time: 401 ms (12.97%), Space: 19.3 MB (7.92%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -2,15 +2,18 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         items = list(range(1, n + 1))
-        vals = itertools.permutations(items)
+        vals = permutations(items)
         for i, perm in enumerate(vals):
             if i+1 == k:
                 return ''.join(map(str, perm))
-        # res = []
-        # def back(cur, path):
-        #     if not cur:
-        #         return res.append(path)
-        #     for i in range(n+1):
-        #         back(cur[:i] + cur[i+1:], [cur[i]] + path)
-        # back(list(range(1, n+1)), [])
-        # print(res)
+
+# class Solution:
+#     def getPermutation(self, n: int, k: int) -> str:
+#         res = []
+#         def back(cur, path):
+#             if not cur:
+#                 return res.append(path)
+#             for i in range(len(cur)):
+#                 back(cur[:i] + cur[i+1:], path + [cur[i]])
+#         back(list(range(1, n+1)), [])
+#         return ''.join(map(str, res[k-1]))

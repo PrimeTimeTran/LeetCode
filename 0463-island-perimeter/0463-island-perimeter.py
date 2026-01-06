c5764963fbf6bd52039528498d6b5ed5a25515c6
Loi Tran <primetimetran@gmail.com>
2026-01-06 14:43:30 +0700

    Time: 197 ms (5.83%), Space: 25.7 MB (12.18%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,13 +2,12 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         R, C = len(grid), len(grid[0])
         self.res, seen = 0, set()
-        def dfs(r,c):
+        def dfs(r, c, val = 0):
             if (r,c) in seen: return 0
             inbounds = 0 <= r < R and 0 <= c < C
             if not inbounds or grid[r][c] == 0:
                 return 1
             seen.add((r,c))
-            val = 0
             for dr, dc in [r+1,c],[r-1, c],[r,c+1],[r,c-1]:
                 val += dfs(dr, dc)
             self.res = max(self.res, val)

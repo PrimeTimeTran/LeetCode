0c6688987857c4722a2cade15c2c6de64e1c9bcc
Loi Tran <primetimetran@gmail.com>
2026-01-06 19:38:13 +0700

    Time: 0 ms (100%), Space: 20.2 MB (6.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,13 +1,11 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        n = len(rooms)
         seen = set()
         def dfs(r):
-            if r in seen:
-                return
+            if r in seen: return
             seen.add(r)
             for room in rooms[r]:
-                if room not in seen:
-                    dfs(room)
-        
+                dfs(room)
         dfs(0)
-        return len(seen) == len(rooms)
+        return len(seen) == n

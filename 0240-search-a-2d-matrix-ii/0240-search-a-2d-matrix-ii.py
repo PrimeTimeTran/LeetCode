3aa1e1640449fdb1abb0c91b2b450a306e983e5f
Loi Tran <primetimetran@gmail.com>
2026-01-11 13:58:34 +0700

    Time: 139 ms (77.63%), Space: 25.7 MB (6.38%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -3,25 +3,11 @@ class Solution:
         m, n = len(mat), len(mat[0])
         r, c = 0, n-1
         while r < m and c >= 0:
-            val = mat[r][c]
-            if val == target: return True
-            elif val > target:
-                c -= 1
-            else:
+            cell = mat[r][c]
+            if cell == target:
+                return True
+            elif cell < target:
                 r += 1
+            else:
+                c -= 1
         return False
-
-# class Solution:
-#     def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
-#         rows, cols = len(mat), len(mat[0])
-#         r, c = 0, cols - 1
-#         while r < rows and c >= 0:
-#             cur = mat[r][c]
-
-#             if cur == target:
-#                 return True
-#             elif cur > target:
-#                 c -= 1
-#             else:
-#                 r += 1
-#         return False

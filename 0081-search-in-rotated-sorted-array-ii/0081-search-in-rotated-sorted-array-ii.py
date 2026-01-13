832341f88695a23dd35a0be734a7a40762114c28
Loi Tran <primetimetran@gmail.com>
2026-01-13 14:30:47 +0700

    Time: 0 ms (100%), Space: 19.8 MB (6.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,28 +1,20 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        l, r = 0, len(nums)-1
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
         while l <= r:
             m = (l+r) // 2
             if nums[m] == target:
                 return True
-            elif nums[l] == nums[m]:
+            if nums[l] == nums[m]:
                 l += 1
-                continue
-            # If true, we know that left is completed sorted.
             elif nums[l] <= nums[m]:
-                # ✅ Left Partition sorted but does target fall inside it?
-                # If it does, then we can discard the rest of the search space.
                 if nums[l] <= target < nums[m]:
                     r = m - 1
-                # If not, then it's inside the unsorted partition
-                else: 
+                else:
                     l = m + 1
             else:
-                # ✅ Right Partition sorted but does target fall inside it?
-                # If it does, then we can discard the rest of the search space.
                 if nums[m] < target <= nums[r]:
                     l = m + 1
-                # If not, then it's inside the unsorted partition
-                else: 
+                else:
                     r = m - 1
         return False

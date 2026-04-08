cdd0e39796c58a9d85ba6bd5eed12201d814cf66
Loi Tran <primetimetran@gmail.com>
2026-04-08 00:29:02 -0500

    Time: 0 ms (100%), Space: 19.5 MB (53.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -16,3 +16,22 @@ class Solution:
                 else:
                     r = m - 1
         return -1
+
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         l, r = 0, len(nums)-1
+#         while l <= r:
+#             m = (l+r) // 2
+#             if nums[m] == target:
+#                 return m
+#             elif nums[l] <= nums[m]:                # The left partition is sorted
+#                 if nums[l] <= target < nums[m]:     # Does the target lie in the sorted half?
+#                     r = m - 1
+#                 else:
+#                     l = m + 1
+#             else:
+#                 if nums[m] < target <= nums[r]:
+#                     l = m + 1
+#                 else:
+#                     r = m - 1
+#         return -1

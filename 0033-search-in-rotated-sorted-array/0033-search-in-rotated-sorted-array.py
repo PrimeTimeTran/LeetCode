fbb314311f7bfda16f8bb0b5d481abb7f52b553f
Loi Tran <primetimetran@gmail.com>
2026-04-08 00:30:52 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,16 +5,16 @@ class Solution:
             m = (l+r) // 2
             if nums[m] == target:
                 return m
-            elif nums[l] <= nums[m]:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
+            elif nums[m] <= nums[r]:
+                if nums[m] <= target < nums[r]:
                     l = m + 1
+                else:
+                    r = m - 1
             else:
-                if nums[m] < target <= nums[r]:
-                    l = m + 1
-                else:
+                if nums[l] < target <= nums[m]:
                     r = m - 1
+                else:
+                    l = m + 1
         return -1
 
 # class Solution:

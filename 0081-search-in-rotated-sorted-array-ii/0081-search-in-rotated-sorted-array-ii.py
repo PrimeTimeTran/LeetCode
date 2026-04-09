c5833836fb115e5a551ae721f82d8b0409d03b06
Loi Tran <primetimetran@gmail.com>
2026-04-08 20:41:29 -0500

    Time: 0 ms (100%), Space: 19.6 MB (44.7%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -5,14 +5,14 @@ class Solution:
             m = (l+r) // 2
             if nums[m] == target:
                 return True
-            if nums[l] == nums[m]:
-                l += 1
+            if nums[m] == nums[l]:
+                l = l + 1
             elif nums[l] <= nums[m]:
                 if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            else:
+            elif nums[m] <= nums[r]:
                 if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:

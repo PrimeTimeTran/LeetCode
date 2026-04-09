207a555caeeb7590ee38eef979c78475316dbc83
Loi Tran <primetimetran@gmail.com>
2026-04-08 20:39:31 -0500

    Time: 0 ms (100%), Space: 19.3 MB (84.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,33 +5,14 @@ class Solution:
             m = (l+r) // 2
             if nums[m] == target:
                 return m
+            elif nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
             elif nums[m] <= nums[r]:
                 if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
-            else:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
         return -1
-
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         l, r = 0, len(nums)-1
-#         while l <= r:
-#             m = (l+r) // 2
-#             if nums[m] == target:
-#                 return m
-#             elif nums[l] <= nums[m]:                # The left partition is sorted
-#                 if nums[l] <= target < nums[m]:     # Does the target lie in the sorted half?
-#                     r = m - 1
-#                 else:
-#                     l = m + 1
-#             else:
-#                 if nums[m] < target <= nums[r]:
-#                     l = m + 1
-#                 else:
-#                     r = m - 1
-#         return -1

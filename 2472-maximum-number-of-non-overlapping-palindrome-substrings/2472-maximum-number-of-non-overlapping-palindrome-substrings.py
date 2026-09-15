a1dceb441d0da965c18272a0e56e2ca5b472460c
Loi Tran <primetimetran@gmail.com>
2026-09-15 16:53:00 -0400

    Time: 3 ms (95.96%), Space: 19.4 MB (72.73%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -7,8 +7,9 @@ class Solution:
     while i <= n - k:
       for d in (k, k + 1):
         inbounds = i + d <= n
-        reversed = s[i : i + d][::-1]
-        valid_palindrome = s[i : i + d] == reversed
+        substring = s[i : i + d]
+        reversed = substring[::-1]
+        valid_palindrome = substring == reversed
         if inbounds and valid_palindrome:
           res += 1  
           i += d

d8c2a88e267939c4df1398fdbb8cd66d8549b2a6
Loi Tran <primetimetran@gmail.com>
2026-09-20 00:29:02 -0400

    Time: 7 ms (70%), Space: 19.2 MB (55.57%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -8,11 +8,17 @@
 #     for i, c in enumerate(s, 1):
 #         total += values[c] * i
 #     return total
+# class Solution:
+#     def reverseDegree(self, s: str) -> int:
+#         total = 0
+#         for i, c in enumerate(s, 1):
+#             total += (123 - ord(c)) * i
+#         return total
 class Solution:
     def reverseDegree(self, s: str) -> int:
         total = 0
 
-        for i, c in enumerate(s, 1):
-            total += (123 - ord(c)) * i
+        for i, c in enumerate(s.encode(), 1):
+            total += (123 - c) * i
 
         return total
